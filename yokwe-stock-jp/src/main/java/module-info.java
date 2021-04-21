e5566d3ce25cb2514161ec12e9f062f021eab83e
hasegawa <hasegawa@localhost>
2021-04-21 17:45:19 +0900

    Make contents up to date.

--- yokwe-stock-jp/src/main/java/module-info.java
+++ yokwe-stock-jp/src/main/java/module-info.java
@@ -1,20 +1,22 @@
 module yokwe.stock.jp {
-	exports yokwe.stock.jp.edinet;
-	exports yokwe.stock.jp.xbrl.label;
-	exports yokwe.stock.jp.sony;
-	exports yokwe.stock.jp.sony.xml;
+	exports yokwe.stock.jp;
 	exports yokwe.stock.jp.data;
-	exports yokwe.stock.jp.release;
-	exports yokwe.stock.jp.tdnet;
-	exports yokwe.stock.jp.xbrl.taxonomy;
-	exports yokwe.stock.jp.jpx;
-	exports yokwe.stock.jp.sony.json;
+	exports yokwe.stock.jp.edinet;
 	exports yokwe.stock.jp.fsa;
+	exports yokwe.stock.jp.jasdec;
+	exports yokwe.stock.jp.jpx;
+	exports yokwe.stock.jp.release;
 	exports yokwe.stock.jp.smbctb;
 	exports yokwe.stock.jp.smbctb.json;
+	exports yokwe.stock.jp.sony;
+	exports yokwe.stock.jp.sony.json;
+	exports yokwe.stock.jp.sony.xml;
+	exports yokwe.stock.jp.tdnet;
 	exports yokwe.stock.jp.xbrl;
 	exports yokwe.stock.jp.xbrl.inline;
+	exports yokwe.stock.jp.xbrl.label;
 	exports yokwe.stock.jp.xbrl.report;
+	exports yokwe.stock.jp.xbrl.taxonomy;
 	exports yokwe.stock.jp.xsd;
 
 	// http
