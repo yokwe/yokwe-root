c91b227a544d295d11ce05b7583b66ae770ba5c8
hasegawa <hasegawa@localhost>
2021-04-23 14:34:07 +0900

    Use proper naming of field.

--- yokwe-stock-jp/src/main/java/yokwe/stock/jp/toushin/Price.java
+++ yokwe-stock-jp/src/main/java/yokwe/stock/jp/toushin/Price.java
@@ -3,13 +3,13 @@ package yokwe.stock.jp.toushin;
 import java.util.Collections;
 import java.util.List;
 
-import yokwe.stock.jp.Storage;
 import yokwe.util.CSVUtil;
+import yokwe.util.StringUtil;
 
 public class Price implements Comparable<Price> {
 	public static final String getPath(String isinCode) {
-		String name = String.format("seller/%s.csv", isinCode);
-		return Storage.getPath(Fund.PREFIX, name);
+		String path = String.format("price/%s.csv", isinCode);
+		return Fund.getPath(path);
 	}
 	public static void save(String isinCode, List<Price> list) {
 		Collections.sort(list);
@@ -23,14 +23,30 @@ public class Price implements Comparable<Price> {
 	// 2014年11月19日,12417,5688,,
 	// 2014年11月20日,12526,5756,0.00,1
 
-	public String date;           // 年月日
-	public String basePrice;      // 基準価額(円)
-	public String totalNetAssset; // 純資産総額（百万円）
-	public String dividend;       // 分配金
-	public String settlement;     // 決算期
+	public String date;          // 年月日
+	public String basePrice;     // 基準価額(円) = 純資産総額 / (総口数 * 10,000)
+	public String netAssetValue; // 純資産総額（百万円）
+	public String dividend;      // 分配金
+	public String period;        // 決算期
+	
+	public Price(String date, String price, String netAsset, String dividend, String period) {
+		this.date          = date;
+		this.basePrice     = price;
+		this.netAssetValue = netAsset;
+		this.dividend      = dividend;
+		this.period        = period;
+	}
+	public Price() {
+		this(null, null, null, null, null);
+	}
 	
 	@Override
 	public int compareTo(Price that) {
 		return this.date.compareTo(that.date);
+	}
+	
+	@Override
+	public String toString() {
+		return StringUtil.toString(this);
 	}
 }
