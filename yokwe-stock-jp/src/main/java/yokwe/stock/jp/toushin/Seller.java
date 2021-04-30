bcc6d00496efba624d144836cbae9b5b346c92c0
hasegawa <hasegawa@localhost>
2021-04-30 15:31:52 +0900

    Create UpdateSeller with StringTask.post()

--- yokwe-stock-jp/src/main/java/yokwe/stock/jp/toushin/Seller.java
+++ yokwe-stock-jp/src/main/java/yokwe/stock/jp/toushin/Seller.java
@@ -1,9 +1,11 @@
 package yokwe.stock.jp.toushin;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
 import yokwe.util.CSVUtil;
+import yokwe.util.StringUtil;
 
 public class Seller implements Comparable<Seller> {
 	public static final String getPath(String isinCode) {
@@ -18,14 +20,30 @@ public class Seller implements Comparable<Seller> {
 		return CSVUtil.read(Seller.class).file(getPath(isinCode));
 	}
 
-	public String isinCode;
-	public String name;
-	public String initialFee;
+	public String     isinCode;
+	public String     sellerCode;
+	public BigDecimal salesFee;
+	public String     sellerName;
 	
+	public Seller(String isinCode, String sellerCode, BigDecimal salesFee, String sellerName) {
+		this.isinCode   = isinCode;
+		this.sellerCode = sellerCode;
+		this.salesFee   = salesFee;
+		this.sellerName = sellerName;
+	}
+	public Seller() {
+		this(null, null, null, null);
+	}
+
 	@Override
 	public int compareTo(Seller that) {
 		int ret = this.isinCode.compareTo(that.isinCode);
-		if (ret == 0) ret = this.name.compareTo(that.name);
+		if (ret == 0) ret = this.sellerCode.compareTo(that.sellerCode);
 		return ret;
+	}
+	
+	@Override
+	public String toString() {
+		return StringUtil.toString(this);
 	}
 }
--- yokwe-stock-jp/src/main/java/yokwe/stock/jp/toushin/UpdateSeller.java
+++ yokwe-stock-jp/src/main/java/yokwe/stock/jp/toushin/UpdateSeller.java
@@ -1,48 +1,176 @@
 package yokwe.stock.jp.toushin;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
+import org.apache.hc.core5.http2.HttpVersionPolicy;
 import org.slf4j.LoggerFactory;
 
+import yokwe.util.StringUtil;
 import yokwe.util.UnexpectedException;
-import yokwe.util.http.HttpUtil;
+import yokwe.util.http.Download;
+import yokwe.util.http.DownloadAsync;
+import yokwe.util.http.DownloadSync;
+import yokwe.util.http.RequesterBuilder;
+import yokwe.util.http.StringTask;
+import yokwe.util.http.Task;
+import yokwe.util.json.JSON;
 
 public class UpdateSeller {
 	static final org.slf4j.Logger logger = LoggerFactory.getLogger(UpdateSeller.class);
 
 	// https://toushin-lib.fwg.ne.jp/FdsWeb/FDST030000/company-search
 	
-	public static void init() {
-//		System.setProperty("http.proxyHost", "192.168.86.40");
-//		System.setProperty("http.proxyPort", "9090");
-//		System.setProperty("https.proxyHost", "192.168.86.40");
-//		System.setProperty("https.proxyPort", "9090");
+	private static class Context {
+		private Integer buildCount;
 		
-        try {
-            TrustManager[] trustManager = new TrustManager[] {
-                	(TrustManager) new X509TrustManager() {
-        	            public java.security.cert.X509Certificate[] getAcceptedIssuers() { return null; }
-        	            public void checkClientTrusted(X509Certificate[] certs, String authType) { }
-        	            public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+		public Context() {
+			buildCount = 0;
+		}
+		
+		public void incrementBuildCount() {
+			synchronized(buildCount) {
+				buildCount = buildCount + 1;
+			}
+		}
+		public int getBuildCount() {
+			int ret;
+			synchronized(buildCount) {
+				ret = buildCount;
+			}
+			return ret;
+		}
+	}
+	
+	//{"fdsInstCd":"21168","salesFee":0.0,"salesInstDiv":"1","kanaName":"イマムラ","instName":"今村証券","associFundCd":null},
+	public static class SellerInfo {
+		public String     fdsInstCd;
+		public BigDecimal salesFee;
+		public String     salesInstDiv;
+		public String     kanaName;
+		public String     instName;
+		public String     associFundCd;
+		
+		public SellerInfo() {
+			fdsInstCd    = null;
+			salesFee     = null;
+			salesInstDiv = null;
+			kanaName     = null;
+			instName     = null;
+			associFundCd = null;
+		}
+		
+		@Override
+		public String toString() {
+			return StringUtil.toString(this);
+		}
+	}
+	
+	private static class MyConsumer implements Consumer<String> {
+		final Context context;
+		final String isinCode;
+		
+		MyConsumer(Context context, String isinCode) {
+			this.context = context;
+			this.isinCode = isinCode;
+		}
+		
+		@Override
+		public void accept(String string) {
+			context.incrementBuildCount();
+			
+			if (string.startsWith("[")) {
+				List<Seller> list = new ArrayList<>();
+				for(var e: JSON.getList(SellerInfo.class, string)) {
+					if (e.salesFee == null) {
+						e.salesFee = BigDecimal.ZERO;
+					}
+					
+					list.add(new Seller(isinCode, e.fdsInstCd, e.salesFee, e.instName));
+				}
+				
+				Seller.save(isinCode, list);
+			} else {
+				logger.warn("string is not JSON");
+				logger.warn("string - {}", string);
+			}
+		}
+	}
+	
+	
+	private static void buildContext(Context context) {
+		int threadCount = 5; // 50 is too high for this site
+		int maxPerRoute = 50;
+		int maxTotal    = 100;
+		int soTimeout   = 30;
+		logger.info("threadCount {}", threadCount);
+		logger.info("maxPerRoute {}", maxPerRoute);
+		logger.info("maxTotal    {}", maxTotal);
+		logger.info("soTimeout   {}", soTimeout);
+		
+		RequesterBuilder requesterBuilder = RequesterBuilder.custom()
+				.setVersionPolicy(HttpVersionPolicy.NEGOTIATE)
+				.setSoTimeout(soTimeout)
+				.setMaxTotal(maxTotal)
+				.setDefaultMaxPerRoute(maxPerRoute);
 
-                } };
+//		Download download = new DownloadAsync();
+		Download download = new DownloadSync();
+		
+		download.setRequesterBuilder(requesterBuilder);
+		
+		// Configure custom header
+		download.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit");
+		download.addHeader("Accept",           "*/*");
+		download.addHeader("X-Requested-With", "XMLHttpRequest");
+		download.addHeader("Origin",           "toushin-lib.fwg.ne.jp");
+		download.addHeader("Accept-Encoding",  "gzip, deflate");
+		download.addHeader("Accept-Language",  "ja");
 
-        	
-            SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustManager, new SecureRandom());
+		
+		// Configure thread count
+		download.setThreadCount(threadCount);
+		
+		//
+		List<Fund> list = Fund.load();
+		Collections.shuffle(list);
+		
+		final int listSize = list.size();
 
-            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
-        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+		for(var e: list) {
+			String isinCode  = e.isinCode;
+			String uriString = "https://toushin-lib.fwg.ne.jp/FdsWeb/FDST030000/company-search";
+			String content   = String.format("isinCd=%s", isinCode);
+			Task   task      = StringTask.post(uriString, new MyConsumer(context, isinCode), content);
+			download.addTask(task);
+		}
+		
+		logger.info("BEFORE RUN");
+		download.startAndWait();
+		logger.info("AFTER  RUN");
+//		download.showRunCount();
+		
+		try {
+			for(int i = 0; i < 10; i++) {
+				int buildCount = context.getBuildCount();
+				if (buildCount == listSize) break;
+				logger.info("buildCount {} / {}", buildCount, listSize);
+				Thread.sleep(1000);
+			}
+			{
+				int buildCount = context.getBuildCount();
+				if (buildCount != listSize) {
+					logger.error("Unexpected");
+					logger.error("  buildCount {}", buildCount);
+					logger.error("  listSize   {}", listSize);
+					throw new UnexpectedException("Unexpected");
+				}
+			}
+			logger.info("AFTER  WAIT");
+		} catch (InterruptedException e) {
 			String exceptionName = e.getClass().getSimpleName();
 			logger.error("{} {}", exceptionName, e);
 			throw new UnexpectedException(exceptionName, e);
@@ -53,38 +181,8 @@ public class UpdateSeller {
 	public static void main(String[] args) {
 		logger.info("START");
 		
-		init();
-		
-		String isinCode = "JP90C0009VE0";
-		
-		HttpUtil httpUtil = HttpUtil.getInstance();
-
-		// https://toushin-lib.fwg.ne.jp/FdsWeb/FDST030000?isinCd=JP90C0009VE0
-		String referer   = String.format("https://toushin-lib.fwg.ne.jp/FdsWeb/FDST030000?isinCd=%s", isinCode);
-		String urlString = "https://toushin-lib.fwg.ne.jp/FdsWeb/FDST030000/company-search";
-		URI uri;
-		try {
-			uri = new URI(urlString);
-		} catch (URISyntaxException e) {
-			String exceptionName = e.getClass().getSimpleName();
-			logger.error("{} {}", exceptionName, e);
-			throw new UnexpectedException(exceptionName, e);
-		}
-		
-		{
-			HttpUtil.Result result = httpUtil
-				.withHeader("Accept",           "*/*")
-				.withHeader("X-Requested-With", "XMLHttpRequest")
-				.withHeader("Origin",           uri.getHost())
-				.withHeader("Referer",          referer)
-				.withHeader("Accept-Encoding",  "gzip, deflate")
-				.withHeader("Accept-Language",  "ja")
-				.withPost("isinCd=JP90C0009VE0", "application/x-www-form-urlencoded; charset=UTF-8")
-				.download(urlString);
-			
-			logger.info("result {} {}", result.result.length(), result.response);
-			logger.info("  {}", result.result);
-		}
+		Context context = new Context();
+		buildContext(context);
 		
 		logger.info("STOP");
 	}
