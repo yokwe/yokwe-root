59a054e5c3f6c154c7173d4f5a299e8b85bf5ab0
hasegawa <hasegawa@localhost>
2021-04-30 15:23:43 +0900

    Use post/get for method name.

--- yokwe-util/src/main/java/yokwe/util/http/Task.java
+++ yokwe-util/src/main/java/yokwe/util/http/Task.java
@@ -3,16 +3,34 @@ package yokwe.util.http;
 import java.net.URI;
 import java.util.function.Consumer;
 
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.Method;
+
 public class Task {
-	public final URI uri;
+	public final URI    uri;
 	public final Consumer<Result> consumer;
 	
-	public Task(URI uri, Consumer<Result> consumer) {
-		this.uri      = uri;
-		this.consumer = consumer;
+	public final Method      method;
+	public final byte[]      entity;
+	public final ContentType contentType;
+	
+	
+	public static Task get(Consumer<Result> consumer, URI uri) {
+		return new Task(consumer, uri, Method.GET, null, null);
 	}
-	public Task(String uriString, Consumer<Result> consumer) {
-		this(URI.create(uriString), consumer);
+
+	public static Task post(Consumer<Result> consumer, URI uri, String content, ContentType contentType) {
+		return new Task(consumer, uri, Method.POST, content.getBytes(contentType.getCharset()), contentType);
+	}
+	public static Task post(Consumer<Result> consumer, URI uri, byte[] content, ContentType contentType) {
+		return new Task(consumer, uri, Method.POST, content, contentType);
+	}
+	private Task(Consumer<Result> consumer, URI uri, Method method, byte[] entity, ContentType contentType) {
+		this.consumer    = consumer;
+		this.uri         = uri;
+		this.method      = method;
+		this.entity      = entity;
+		this.contentType = contentType;
 	}
 	
 	public void process(Result result) {
