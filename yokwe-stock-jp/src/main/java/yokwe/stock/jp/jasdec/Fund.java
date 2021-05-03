08461fe802bd62fafae3c2f33c5e8a85acba3f79
hasegawa <hasegawa@localhost>
2021-05-03 20:32:51 +0900

    Add unitPrice and minimumUnit. Use proper type for fields.

--- yokwe-stock-jp/src/main/java/yokwe/stock/jp/jasdec/Fund.java
+++ yokwe-stock-jp/src/main/java/yokwe/stock/jp/jasdec/Fund.java
@@ -1,5 +1,7 @@
 package yokwe.stock.jp.jasdec;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,7 +10,7 @@ import yokwe.util.CSVUtil;
 import yokwe.util.StringUtil;
 
 public class Fund implements Comparable<Fund> {
-	public static final String NO_LIMIT = "9999-12-31";
+	public static final String INDEFINITE = "9999-12-31";
 	public static final String PREFIX = "jasdec";
 	
 	public static final String getPath() {
@@ -23,27 +25,33 @@ public class Fund implements Comparable<Fund> {
 		return CSVUtil.read(Fund.class).file(getPath());
 	}
 
-	public int    idno;           // IDNO
-	public String isinCode;       // ISINコード
-	public String issueDate;      // 設定日
-	public String redemptionDate; // 償還日
-	public String offerCategory;  // 募集区分
-	public String fundCategory;   // 投信区分
-	public String issuer;         // 発行者名	
-	public String name;           // 銘柄正式名称
+	public int        idno;           // IDNO
+	public String     isinCode;       // ISINコード
+	public LocalDate  issueDate;      // 設定日
+	public LocalDate  redemptionDate; // 償還日
+	public BigDecimal unitPrice;      // 当初1口当たり元本
+	public BigDecimal minimuUnit;     // 最低発行単位口数
+	public String     offerCategory;  // 募集区分
+	public String     fundCategory;   // 投信区分
+	public String     issuer;         // 発行者名	
+	public String     name;           // 銘柄正式名称
 	
-	public Fund(int idno, String isinCode, String issueDate, String redemptionDate, String offerCategory, String fundCategory, String issuer, String name) {
+	public Fund(
+		int idno, String isinCode, LocalDate issueDate, LocalDate redemptionDate, BigDecimal unitPrice, BigDecimal minimuUnit, 
+		String offerCategory, String fundCategory, String issuer, String name) {
 		this.idno           = idno;
 		this.isinCode       = isinCode;
 		this.issueDate      = issueDate;
 		this.redemptionDate = redemptionDate;
+		this.unitPrice      = unitPrice;
+		this.minimuUnit     = minimuUnit;
 		this.offerCategory  = offerCategory;
 		this.fundCategory   = fundCategory;
 		this.issuer         = issuer;
 		this.name           = name;
 	}
 	public Fund() {
-		this(0, null, null, null, null, null, null, null);
+		this(0, null, null, null, null, null, null, null, null, null);
 	}
 
 	@Override
