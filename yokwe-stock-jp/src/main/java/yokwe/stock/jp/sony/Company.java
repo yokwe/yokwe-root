84f3e61ff245fafc523cfe21cc8e9b5bc0f4e1ed
hasegawa <hasegawa@localhost>
2021-05-04 13:05:46 +0900

    LeggMason become Franklin Templeton

--- yokwe-stock-jp/src/main/java/yokwe/stock/jp/sony/Company.java
+++ yokwe-stock-jp/src/main/java/yokwe/stock/jp/sony/Company.java
@@ -2,6 +2,41 @@ package yokwe.stock.jp.sony;
 
 import yokwe.util.UnexpectedException;
 
+// https://moneykit.net/data/fund/SFBA1700F470.js
+//FundManagerMst= {
+//	"1":[{"KanaCode":"002","KaisyaMei":"朝日ライフ"}],
+//	"2":[{"KanaCode":"004","KaisyaMei":"アセマネOne"}],
+//	"3":[{"KanaCode":"006","KaisyaMei":"アムンディ"}],
+//	"4":[{"KanaCode":"009","KaisyaMei":"イーストS"}],
+//	"5":[{"KanaCode":"011","KaisyaMei":"インベスコ"}],
+//	"6":[{"KanaCode":"012","KaisyaMei":"HSBC"}],
+//	"7":[{"KanaCode":"014","KaisyaMei":"NN"}],
+//	"8":[{"KanaCode":"024","KaisyaMei":"コモンズ"}],
+//	"9":[{"KanaCode":"027","KaisyaMei":"シュローダー"}],
+//	"10":[{"KanaCode":"033","KaisyaMei":"JPモルガン"}],
+//	"11":[{"KanaCode":"034","KaisyaMei":"ジャナス・キャピタル"}],
+//	"12":[{"KanaCode":"039","KaisyaMei":"SOMPO"}],
+//	"13":[{"KanaCode":"041","KaisyaMei":"大和"}],
+//	"14":[{"KanaCode":"046","KaisyaMei":"T&D"}],
+//	"15":[{"KanaCode":"049","KaisyaMei":"ドイチェ"}],
+//	"16":[{"KanaCode":"050","KaisyaMei":"日興"}],
+//	"17":[{"KanaCode":"051","KaisyaMei":"日興AMヨーロッパ"}],
+//	"18":[{"KanaCode":"052","KaisyaMei":"ニッセイ"}],
+//	"19":[{"KanaCode":"055","KaisyaMei":"野村"}],
+//	"20":[{"KanaCode":"061","KaisyaMei":"BNPパリバ"}],
+//	"21":[{"KanaCode":"063","KaisyaMei":"ピクテ"}],
+//	"22":[{"KanaCode":"065","KaisyaMei":"ファイブスター"}],
+//	"23":[{"KanaCode":"067","KaisyaMei":"フィデリティ"}],
+//	"24":[{"KanaCode":"069","KaisyaMei":"フランクリン"}],
+//	"25":[{"KanaCode":"070","KaisyaMei":"ブラックロック"}],
+//	"26":[{"KanaCode":"077","KaisyaMei":"マニュライフ"}],
+//	"27":[{"KanaCode":"079","KaisyaMei":"三井住友DS"}],
+//	"28":[{"KanaCode":"080","KaisyaMei":"三井住友TAM"}],
+//	"29":[{"KanaCode":"081","KaisyaMei":"三菱UFJ国際"}],
+//	"30":[{"KanaCode":"087","KaisyaMei":"UBS"}],
+//	"31":[{"KanaCode":"089","KaisyaMei":"楽天"}],
+//	"32":[{"KanaCode":"093","KaisyaMei":"レオス"}]};
+
 public enum Company {
 	ASAHILIFE("002", "朝日ライフ"),
 	ASEMANE  ("004", "アセマネOne"),
@@ -26,6 +61,7 @@ public enum Company {
 	PICTE    ("063", "ピクテ"),
 	FIVESTAR ("065", "ファイブスター"),
 	FEDILITY ("067", "フィデリティ"),
+	FRANKLIN ("069", "フランクリン"),
 	BLACKROCK("070", "ブラックロック"),
 	MANULIFE ("077", "マニュライフ"),
 	SMDS     ("079", "三井住友DS"),
@@ -33,8 +69,7 @@ public enum Company {
 	UFJ      ("081", "三菱UFJ国際"),
 	UBS      ("087", "UBS"),
 	RAKUTEN  ("089", "楽天"),
-	RHEOS    ("093", "レオス"),
-	LM       ("094", "L・メイソン");
+	RHEOS    ("093", "レオス");
 	
 	public final String code;
 	public final String name;
