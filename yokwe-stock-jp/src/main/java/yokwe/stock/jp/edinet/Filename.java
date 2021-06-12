4d0a11138db30a933a77251ac9cf7a9e23119188
hasegawa <hasegawa@localhost>
2021-06-12 23:33:52 +0900

    Add Filename.equals(Instance, Honbun).

--- yokwe-stock-jp/src/main/java/yokwe/stock/jp/edinet/Filename.java
+++ yokwe-stock-jp/src/main/java/yokwe/stock/jp/edinet/Filename.java
@@ -26,6 +26,19 @@ public class Filename {
 			"_" +
 			"(?<submitDate>20[0-9]{2}-[01][0-9]-[0-3][0-9])";
 	
+	public static boolean equals(Instance instance, Honbun honbun) {
+		if (honbun == null) return false;
+		return
+			instance.form.equals(honbun.form) &&
+			instance.report.equals(honbun.report) &&
+			instance.reportNo.equals(honbun.reportNo) &&
+			instance.code.equals(honbun.code) &&
+			instance.codeNo.equals(honbun.codeNo) &&
+			instance.date.equals(honbun.date) &&
+			instance.submitNo.equals(honbun.submitNo) &&
+			instance.submitDate.equals(honbun.submitDate);
+	}
+
 	public static class Instance implements Comparable<Instance> {
 		// jp{府令様式番号}-{報告書略号}-{報告書連番(3 桁)}_{EDINET コード又はファンドコード}-{追番(3 桁)}_{報告対象期間期末日|報告義務発生日}_{報告書提出回数(2 桁)}_{報告書提出日}.xbrl 
 		// jpcrp040300-q3r-001_E01442-000_2020-01-31_01_2020-03-06.xbrl
@@ -85,7 +98,8 @@ public class Filename {
 
 		@Override
 		public int compareTo(Instance that) {
-			int ret = this.code.compareTo(that.code);
+			int ret = 0;
+			if (ret == 0) ret = this.code.compareTo(that.code);
 			if (ret == 0) ret = this.codeNo.compareTo(that.codeNo);
 			if (ret == 0) ret = this.submitDate.compareTo(that.submitDate);
 			if (ret == 0) ret = this.submitNo.compareTo(that.submitNo);
@@ -112,32 +126,32 @@ public class Filename {
 		public String    string;
 
 		public Honbun (
-				String    no,
-				String    title,
-				String    form,
-				Report    report,
-				String    reportNo,
-				String    code,
-				String    codeNo,
-				LocalDate date,
-				String    submitNo,
-				LocalDate submitDate
-				) {
-				this.no         = no;
-				this.title      = title;
-				this.form       = form;
-				this.report     = report;
-				this.reportNo   = reportNo;
-				this.code       = code;
-				this.codeNo     = codeNo;
-				this.date       = date;
-				this.submitNo   = submitNo;
-				this.submitDate = submitDate;
-				
-				// 0101010_honbun_jpcrp040300-q3r-001_E01442-000_2020-01-31_01_2020-03-06_ixbrl.htm
-				this.string     = String.format("%s_%s_%s-%s-%s_%s-%s_%s_%s_%s_ixbrl.htm", no, title, form, report, reportNo, code, codeNo, date, submitNo, submitDate);
-			}
-
+			String    no,
+			String    title,
+			String    form,
+			Report    report,
+			String    reportNo,
+			String    code,
+			String    codeNo,
+			LocalDate date,
+			String    submitNo,
+			LocalDate submitDate
+			) {
+			this.no         = no;
+			this.title      = title;
+			this.form       = form;
+			this.report     = report;
+			this.reportNo   = reportNo;
+			this.code       = code;
+			this.codeNo     = codeNo;
+			this.date       = date;
+			this.submitNo   = submitNo;
+			this.submitDate = submitDate;
+			
+			// 0101010_honbun_jpcrp040300-q3r-001_E01442-000_2020-01-31_01_2020-03-06_ixbrl.htm
+			this.string     = String.format("%s_%s_%s-%s-%s_%s-%s_%s_%s_%s_ixbrl.htm", no, title, form, report, reportNo, code, codeNo, date, submitNo, submitDate);
+		}
+		
 		public static Honbun getInstance(String string) {
 			Honbun ret = ScrapeUtil.get(Honbun.class, PAT, string);
 			// sanity check
@@ -157,7 +171,12 @@ public class Filename {
 		
 		@Override
 		public int compareTo(Honbun that) {
-			int ret = this.no.compareTo(that.no);
+			int ret = 0;
+			if (ret == 0) ret = this.code.compareTo(that.code);
+			if (ret == 0) ret = this.codeNo.compareTo(that.codeNo);
+			if (ret == 0) ret = this.submitDate.compareTo(that.submitDate);
+			if (ret == 0) ret = this.submitNo.compareTo(that.submitNo);
+			if (ret == 0) ret = this.no.compareTo(that.no);
 			return ret;
 		}
 	}
