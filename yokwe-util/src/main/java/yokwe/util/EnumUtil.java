9675b4c71085d3d4eb244d881bf54e5522ddd5cf
hasegawa <hasegawa@localhost>
2021-07-07 23:21:23 +0900

    Add GenericXmlAdapter and use HashMap for speed.

--- yokwe-util/src/main/java/yokwe/util/EnumUtil.java
+++ yokwe-util/src/main/java/yokwe/util/EnumUtil.java
@@ -1,15 +1,17 @@
 package yokwe.util;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
+
+import jakarta.xml.bind.annotation.adapters.XmlAdapter;
 
 public class EnumUtil {
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(EnumUtil.class);
 
 	private static class EnumInfo {
-		private static Map<String, EnumInfo> map = new TreeMap<>();
+		private static Map<String, EnumInfo> map = new HashMap<>();
 		
-		static EnumInfo get(Class<?> clazz) {
+		private static EnumInfo get(Class<?> clazz) {
 			String key = clazz.getName();
 			if (map.containsKey(key)) {
 				return map.get(key);
@@ -20,29 +22,52 @@ public class EnumUtil {
 			}
 		}
 		
-		public Map<String, Enum<?>> enumMap;
+		private Map<String, Enum<?>> enumMap;
 		EnumInfo(Class<?> clazz) {
-			enumMap = new TreeMap<>();
+			enumMap = new HashMap<>();
 			
 			@SuppressWarnings("unchecked")
 			Class<Enum<?>> enumClazz = (Class<Enum<?>>)clazz;
 			for(Enum<?> e: enumClazz.getEnumConstants()) {
-				enumMap.put(e.toString(), e);
+				String key = e.toString();
+				if (enumMap.containsKey(key)) {
+					logger.error("Unexpected value");
+					logger.error("  clazz {}", clazz.toString());
+					logger.error("  key   {}", key);
+					throw new UnexpectedException("Unexpected value");
+				} else {
+					enumMap.put(key, e);
+				}
 			}
 		}
 	}
 	
 	public static <E extends Enum<?>> E getInstance(Class<E> clazz, String value) {
-		EnumInfo classInfo = EnumInfo.get(clazz);
-		if (classInfo.enumMap.containsKey(value)) {
-			Object o = classInfo.enumMap.get(value);
+		EnumInfo enumInfo = EnumInfo.get(clazz);
+		if (enumInfo.enumMap.containsKey(value)) {
+			Object o = enumInfo.enumMap.get(value);
 			@SuppressWarnings("unchecked")
 			E e = (E)o;
 			return e;
 		} else {
 			logger.error("Unexpected value");
+			logger.error("  clazz {}", clazz.toString());
 			logger.error("  value {}", value);
 			throw new UnexpectedException("Unexpected value");
+		}
+	}
+	
+	public static class GenericXmlAdapter extends XmlAdapter<String, Enum<?>> {
+		private final Class<? extends Enum<?>> enumClass;
+		protected GenericXmlAdapter(Class<? extends Enum<?>> enumClass) {
+			this.enumClass = enumClass;
+		}
+		
+		public String marshal(Enum<?> type) {
+			return type.toString();
+		}
+		public Enum<?> unmarshal(String string) {
+			return EnumUtil.getInstance(this.enumClass, string);
 		}
 	}
 }
