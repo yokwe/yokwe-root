2808f34ddf58487a837992cc7310007b37095b0a
hasegawa <hasegawa@localhost>
2021-11-30 09:26:52 +0900

    Add Price and UpdatePrice class.

---
+++ yokwe-stock-us/src/main/java/yokwe/stock/us/nasdaq/Price.java
@@ -0,0 +1,113 @@
+package yokwe.stock.us.nasdaq;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import yokwe.stock.us.Storage;
+import yokwe.util.CSVUtil;
+import yokwe.util.DoubleUtil;
+import yokwe.util.StringUtil;
+import yokwe.util.UnexpectedException;
+
+public class Price implements Comparable<Price> {
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Price.class);
+
+	private static final String PATH_DIR = Storage.NASDAQ.getPath("price");
+	public static String getPath(String symbol) {
+		return String.format("%s/%s.csv", PATH_DIR, symbol);
+	}
+	
+	
+	public static void save(Collection<Price> collection) {
+		save(new ArrayList<>(collection));
+	}
+	public static void save(List<Price> list) {
+		if (list.isEmpty()) return;
+		String symbol = list.get(0).symbol;
+		
+		// Sort before save
+		Collections.sort(list);
+		CSVUtil.write(Price.class).file(getPath(symbol), list);
+	}
+	
+	public static List<Price> getList(String symbol) {
+		List<Price> ret = CSVUtil.read(Price.class).file(getPath(symbol));
+		return ret == null ? new ArrayList<>() : ret;
+	}
+	public static Map<String, Price> getMap(String symbol) {
+		//            date
+		Map<String, Price> ret = new TreeMap<>();
+		
+		for(var e: getList(symbol)) {
+			String date = e.date;
+			if (ret.containsKey(date)) {
+				logger.error("Duplicate date");
+				logger.error("  date {}", date);
+				logger.error("  old {}", ret.get(symbol));
+				logger.error("  new {}", e);
+				throw new UnexpectedException("Duplicate date");
+			} else {
+				ret.put(date, e);
+			}
+		}
+		
+		return ret;
+	}
+
+	public String symbol; // normalized symbol like TRNT-A and RDS.A not like TRTN^A and RDS/A
+	public String date;   // YYYY-MM-DD
+	
+	@CSVUtil.DecimalPlaces(2)
+	public double open;
+	@CSVUtil.DecimalPlaces(2)
+	public double high;
+	@CSVUtil.DecimalPlaces(2)
+	public double low;
+	@CSVUtil.DecimalPlaces(2)
+	public double close;
+	public long   volume;
+	
+	public Price(String symbol, String date, double open, double high, double low, double close, long volume) {
+		this.symbol = symbol;
+		this.date   = date;
+		this.open   = DoubleUtil.roundPrice(open);
+		this.high   = DoubleUtil.roundPrice(high);
+		this.low    = DoubleUtil.roundPrice(low);
+		this.close  = DoubleUtil.roundPrice(close);
+		this.volume = volume;
+	}
+	public Price() {
+		this(null, null, 0, 0, 0, 0, 0);
+	}
+	
+	@Override
+	public String toString() {
+		return StringUtil.toString(this);
+	}
+
+	@Override
+	public int compareTo(Price that) {
+		return this.symbol.compareTo(that.symbol);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Price) {
+			Price that = (Price) o;
+			return
+				this.symbol.equals(that.symbol) &&
+				this.date.equals(that.date) &&
+				DoubleUtil.isAlmostEqual(this.open,  that.open) &&
+				DoubleUtil.isAlmostEqual(this.high,  that.high) &&
+				DoubleUtil.isAlmostEqual(this.low,   that.low) &&
+				DoubleUtil.isAlmostEqual(this.close, that.close) &&
+				this.volume == that.volume;
+		} else {
+			return false;
+		}
+	}
+}
---
+++ yokwe-stock-us/src/main/java/yokwe/stock/us/nasdaq/UpdatePrice.java
@@ -0,0 +1,217 @@
+package yokwe.stock.us.nasdaq;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import yokwe.stock.us.Storage;
+import yokwe.stock.us.nasdaq.api.Quote;
+import yokwe.util.CSVUtil;
+import yokwe.util.Market;
+import yokwe.util.StringUtil;
+import yokwe.util.UnexpectedException;
+
+public class UpdatePrice {
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(UpdatePrice.class);
+	
+	public static class Request {
+		public String symbol;     // normalized symbol like TRNT-A and RDS.A not like TRTN^A and RDS/A
+		public String assetClass; // STOCKS or ETF
+		
+		public Request(String symbol, String assetClass) {
+			this.symbol     = symbol;
+			this.assetClass = assetClass;
+		}
+
+		@Override
+		public String toString() {
+			return StringUtil.toString(this);
+		}
+	}
+	
+	public static class Duration {
+		public static enum Unit {
+			YEAR, MONTH, WEEK, DAY
+		}
+		
+		public static Duration Year(int value) {
+			return new Duration(Unit.YEAR, value);
+		}
+		public static Duration Month(int value) {
+			return new Duration(Unit.MONTH, value);
+		}
+		public static Duration Week(int value) {
+			return new Duration(Unit.WEEK, value);
+		}
+		public static Duration Day(int value) {
+			return new Duration(Unit.DAY, value);
+		}
+		
+		public Unit unit;
+		public int  value;
+		
+		public Duration(Unit unit, int value) {
+			this.unit  = unit;
+			this.value = value;
+		}
+		
+		@Override
+		public String toString() {
+			return StringUtil.toString(this);
+		}
+		
+		public LocalDate apply(LocalDate date) {
+			switch(unit) {
+			case YEAR:
+				return date.minusYears(value);
+			case MONTH:
+				return date.minusMonths(value);
+			case WEEK:
+				return date.minusWeeks(value);
+			case DAY:
+				return date.minusDays(value);
+			default:
+				logger.error("Unexpected");
+				logger.error("  {}", this);
+				throw new UnexpectedException("Unexpected");
+			}
+		}
+	}
+	
+	public static void update(Request request, LocalDate fromDate, LocalDate toDate) {
+		Quote.Historical historical = Quote.Historical.getInstance(request.assetClass, request.symbol, fromDate, toDate);
+		
+		if (historical.data == null) {
+			logger.warn("no data {}", request.symbol);
+			return; // no data
+		}
+		if (historical.data.tradesTable == null) {
+			logger.warn("no tradesTable {}", request.symbol);
+			return; // no data
+		}
+		if (historical.data.tradesTable.rows == null) {
+			logger.warn("no rows {}", request.symbol);
+			return; // no data
+		}
+		
+		Map<String, Price> map = Price.getMap(request.symbol);
+		
+		for(var e: historical.data.tradesTable.rows) {
+			// close: "194.39", date: "11/26/2021", high: "196.82", low: "194.19", open: "196.82", volume: "11,113"
+			// close: "$17.86", date: "11/26/2021", high: "$18.155", low: "$17.765", open: "$18.03", volume: "1,645,865
+			String[] ymd = e.date.split("\\/");
+			if (ymd.length != 3) {
+				logger.error("Unpexpected");
+				logger.error("  date {}", e.date);
+				throw new UnexpectedException("Unpexpected");
+			}
+			String date  = ymd[2] + "-" + ymd[0] + "-" + ymd[1];
+			String open  = e.open.replace("$", "");
+			String high  = e.high.replace("$", "");
+			String low   = e.low.replace("$", "");
+			String close = e.close.replace("$", "");
+			String value = e.volume.replace(",", "").replace("N/A", "0");
+			
+			Price price = new Price(
+				request.symbol,
+				date,
+				Double.parseDouble(open),
+				Double.parseDouble(high),
+				Double.parseDouble(low),
+				Double.parseDouble(close),
+				Long.parseLong(value));
+			
+			if (map.containsKey(date)) {
+				Price old = map.get(date);
+				if (price.equals(old)) {
+					// OK
+				} else {
+					logger.error("Unpexpected");
+					logger.error("  old {}", old);
+					logger.error("  new {}", price);
+					throw new UnexpectedException("Unpexpected");
+				}
+			} else {
+				map.put(date, price);
+			}
+		}
+
+		// save
+		Price.save(map.values());
+	}
+	
+	public static class Symbol {
+		public String symbol;
+		
+		public Symbol() {
+			symbol = null;
+		}
+	}
+	
+	public static void main(String[] args) {
+		logger.info("START");
+		
+		Duration duration = Duration.Year(1);
+		logger.info("duration {}", duration);
+				
+		// build toDate and fromDate
+		LocalDate toDate   = Market.getLastTradingDate();
+		LocalDate fromDate = duration.apply(toDate);
+		if (Market.isClosed(fromDate)) {
+			fromDate = Market.getPreviousTradeDate(fromDate);
+		}
+		logger.info("date range {} - {}", fromDate, toDate);
+		String toDateString = toDate.toString();
+		
+		// build symbolList from symbol.csv
+		List<Symbol> symbolList = CSVUtil.read(Symbol.class).file(Storage.NASDAQ.getPath("symbol.csv"));
+		logger.info("symbol    {}", symbolList.size());
+		
+		// build requestList
+		List<Request> requestList = new ArrayList<>();
+		{
+			List<String> unknownList = new ArrayList<>();
+			Map<String, Stock> stockMap = Stock.getMap();
+			int countProcessed = 0;
+			for(var e: symbolList) {
+				String symbol = e.symbol;
+				if (stockMap.containsKey(symbol)) {
+					Stock stock = stockMap.get(symbol);
+					
+					Set<String> dateSet = Price.getList(symbol).stream().map(o -> o.date).collect(Collectors.toSet());
+					if (dateSet.contains(toDateString)) {
+						// already processed
+						countProcessed++;
+					} else {
+						requestList.add(new Request(stock.symbol, stock.assetClass));
+					}
+				} else {
+					unknownList.add(symbol);
+//					logger.warn("Unknown symbol {}", symbol);
+				}
+			}
+			logger.info("unknown   {} {}", unknownList.size(), unknownList);
+			logger.info("processed {}", countProcessed);
+			logger.info("request   {}", requestList.size());
+		}
+		
+
+		int count = 0;
+		int toatlCount = requestList.size();
+		for(var e: requestList) {
+			if ((count % 100) == 0) {
+				logger.info("{}", String.format("%5d / %5d %s", count, toatlCount, e.symbol));
+			}
+			count++;
+			
+			update(e, fromDate, toDate);
+		}
+		
+		logger.info("STOP");
+	}
+	
+}
