14a6cfc663640852d92ca29043391bd7afa2db03
hasegawa <hasegawa@localhost>
2021-12-26 13:51:39 +0900

    Update Symbol using StockUS in trade.monex.

--- yokwe-stock-trade/src/main/java/yokwe/stock/trade/monex/UpdateSymbol.java
+++ yokwe-stock-trade/src/main/java/yokwe/stock/trade/monex/UpdateSymbol.java
@@ -2,8 +2,13 @@ package yokwe.stock.trade.monex;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
+import yokwe.stock.trade.monex.StockUS.Type;
 import yokwe.stock.us.nasdaq.Symbol;
+import yokwe.stock.us.nasdaq.api.AssetClass;
+import yokwe.util.UnexpectedException;
 
 //
 // Update nasdaq.Symbol for nasdaq
@@ -11,13 +16,27 @@ import yokwe.stock.us.nasdaq.Symbol;
 public class UpdateSymbol {
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(UpdateSymbol.class);
 
+	private static final Map<Type, AssetClass> map = new TreeMap<>();
+	static {
+		map.put(Type.ETF,   AssetClass.ETF);
+		map.put(Type.Stock, AssetClass.STOCK);
+	}
+	
 	public static void main(String[] args) {
 		logger.info("START");
 		
 		List<Symbol> list = new ArrayList<>();
 		
 		for(var e: StockUS.getList()) {
-			Symbol symbol = new Symbol(e.symbol, e.name);
+			AssetClass assetClass;
+			if (map.containsKey(e.type)) {
+				assetClass = map.get(e.type);
+			} else {
+				logger.error("Unexpected");
+				logger.error("  type {}", e.type);
+				throw new UnexpectedException("Unexptected");
+			}
+			Symbol symbol = new Symbol(e.symbol, assetClass, e.name);
 			list.add(symbol);
 		}
 		
--- yokwe-stock-us/src/main/java/yokwe/stock/us/nasdaq/Symbol.java
+++ yokwe-stock-us/src/main/java/yokwe/stock/us/nasdaq/Symbol.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.List;
 
 import yokwe.stock.us.Storage;
+import yokwe.stock.us.nasdaq.api.AssetClass;
 import yokwe.util.CSVUtil;
 import yokwe.util.StringUtil;
 
@@ -29,15 +30,17 @@ public class Symbol implements Comparable<Symbol> {
 		return ret == null ? new ArrayList<>() : ret;
 	}
 	
-	public String symbol; // normalized symbol like TRNT-A and RDS.A not like TRTN^A and RDS/A
-	public String name;   // name of stock to analyze equality of symbol
+	public String     symbol; // normalized symbol like TRNT-A and RDS.A not like TRTN^A and RDS/A
+	public AssetClass assetClass;
+	public String     name;   // name of stock to analyze equality of symbol
 	
-	public Symbol(String symbol, String name) {
-		this.symbol = symbol.trim();
-		this.name   = name.trim();
+	public Symbol(String symbol, AssetClass assetClass, String name) {
+		this.symbol     = symbol.trim();
+		this.assetClass = assetClass;
+		this.name       = name.trim();
 	}
 	public Symbol() {
-		this("", "");
+		this("", AssetClass.STOCK, "");
 	}
 	
 	@Override
