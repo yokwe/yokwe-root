eef441a38027400d67dccd249890b2dae18b2ab2
hasegawa <hasegawa@localhost>
2022-02-18 13:40:54 +0900

    Add getLogger() that use caller's class as logger name.

--- yokwe-util/src/main/java/yokwe/util/FormatLogger.java
+++ yokwe-util/src/main/java/yokwe/util/FormatLogger.java
@@ -5,6 +5,11 @@ public final class FormatLogger {
 		var logger = org.slf4j.LoggerFactory.getLogger(clazz);
 		return new FormatLogger(logger);
 	}
+	// use caller class for logger name
+	public static FormatLogger getLogger() {
+		Class<?> caller = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).getCallerClass();
+		return getLogger(caller);
+	}
 
 	private final org.slf4j.Logger logger;
 	
@@ -12,34 +17,36 @@ public final class FormatLogger {
 		this.logger = logger;
 	}
 	
+	public void trace(String message) {
+		logger.trace(message);
+	}
+	public void debug(String message) {
+		logger.trace(message);
+	}
+	public void info(String message) {
+		logger.info(message);
+	}
+	public void warn(String message) {
+		logger.warn(message);
+	}
+	public void error(String message) {
+		logger.error(message);
+	}
+	
 	public void trace(String format, Object... arguments) {
-		if (logger.isTraceEnabled()) {
-			String message = String.format(format, arguments);
-			logger.trace(message);
-		}
+		logger.trace(String.format(format, arguments));
 	}
 	public void debug(String format, Object... arguments) {
-		if (logger.isDebugEnabled()) {
-			String message = String.format(format, arguments);
-			logger.debug(message);
-		}
+		logger.debug(String.format(format, arguments));
 	}
 	public void info(String format, Object... arguments) {
-		if (logger.isInfoEnabled()) {
-			String message = String.format(format, arguments);
-			logger.info(message);
-		}
+		logger.info(String.format(format, arguments));
 	}
 	public void warn(String format, Object... arguments) {
-		if (logger.isWarnEnabled()) {
-			String message = String.format(format, arguments);
-			logger.warn(message);
-		}
+		logger.warn(String.format(format, arguments));
 	}
 	public void error(String format, Object... arguments) {
-		if (logger.isErrorEnabled()) {
-			String message = String.format(format, arguments);
-			logger.error(message);
-		}
+		logger.error(String.format(format, arguments));
 	}
+	
 }
