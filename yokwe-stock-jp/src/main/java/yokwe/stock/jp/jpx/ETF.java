9632f95db6b704f9eca9c97b088dbcac27cbe8c3
hasegawa <hasegawa@localhost>
2022-06-25 22:37:07 +0900

    Temporary commit.

---
+++ yokwe-stock-jp/src/main/java/yokwe/stock/jp/jpx/ETF.java
@@ -0,0 +1,154 @@
+package yokwe.stock.jp.jpx;
+
+import java.math.BigDecimal;
+
+import yokwe.util.StringUtil;
+import yokwe.util.UnexpectedException;
+
+public class ETF implements Comparable<ETF> {
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ETF.class);
+
+	public static class Dividend implements Comparable<Dividend> {
+		public String     stockCode;
+	    public String     payDate;
+	    public BigDecimal dividend;
+	    
+	    public Dividend(String stockCode, String payDate, BigDecimal dividend) {
+	    	this.stockCode = stockCode;
+	    	this.payDate   = payDate;
+	    	this.dividend  = dividend;
+	    }
+	    
+	    @Override
+	    public String toString() {
+	        return StringUtil.toString(this);
+	    }
+
+		@Override
+		public int compareTo(Dividend that) {
+			int ret = this.stockCode.compareTo(that.stockCode);
+			if (ret == 0) ret = this.payDate.compareTo(that.payDate);
+			return ret;
+		}
+	}
+	
+	public static class TargetIndex implements Comparable<TargetIndex> {
+		public String name;
+		public String descirtion;
+		
+	    @Override
+	    public String toString() {
+	        return StringUtil.toString(this);
+	    }
+
+		@Override
+		public int compareTo(TargetIndex that) {
+			return this.name.compareTo(that.name);
+		}
+	}
+	
+	public String     date;      // update date? YYYY-MM-DD
+	public String     stockCode; // NNNNN
+//	public int        unit;      // 売買単位
+//	public int        exType;    // 0 for ETF
+	
+	public String     listingDate;   // YYYY/MM/DD
+//	public BigDecimal managementFee; // 信託報酬 in percent
+	public BigDecimal expenseRatio;
+
+	// dividend
+//	public BigDecimal dividend;      // 分配金…直近1年間の分配金額
+	public BigDecimal divAnnual;     // 分配金…直近1年間の分配金額
+//	public String     dividendDate;  // （年4回）
+	public int        divFreq;
+//	public BigDecimal dividendYield; // 分配金利回り
+
+	// description
+	public String     stockName; // "ＮＥＸＴ ＦＵＮＤＳ 東証ＲＥＩＴ指数連動型上場投信"
+//	public String     feature;   // "わが国の不動産投信（J-REIT）市場全体の値動きを表す代表的な株価指数である「東証REIT指数｣との連動を目指すＥＴＦです。"
+
+	// category
+	public String     categoryName;      // "不動産ETF"
+	public String     productType;       // "国内不動産"
+	public String     targetName;        // "東証REIT指数"
+	public String     targetDescription; // "東証REIT指数は、東京証券取引所に上場している不動産投信（J-REIT）全銘柄を対象とした「浮動株時価総額加重型」の指数です。JPX総研が算出・公表しています。算出方法は2003年3月31日の時価総額を1,000ポイントとして、その後の時価総額を指数化したものです。"
+
+	
+	// public DividendHist[]    dividendHist;
+	
+	// price -- can be take from stock-price.csv
+//	public BigDecimal  price;     // 終値・直近取引値
+//	public String      priceDate; // YYYY/MM/DD
+//	public long        volume;    // 売買高
+	
+	// net asset
+//	public boolean    hasNav;
+//	public String     nav;        // 基準価額 "2,042.94"
+
+//	public BigDecimal deviation; // 乖離率…前日市場価格÷前日基準価額を1口あたりに換算した値の割合を表示 -- 0.32
+
+	// spread
+//	public BigDecimal spread;     // スプレッド…最良の売気配値段と買気配値段の価格差（%）
+//	public String     spreadDate; // YYYY/MM/DD
+	
+
+//	public long       rightUnit;  // 受益権口数
+//	public String     sharesDate; // YYYY/MM/DD -- can be old data
+	
+//	public long       netAssets;     // 純資産総額
+//	public String     netAssetsDate; // YYYY/MM/DD -- can be old data
+	
+	// trading value
+//	public long       tradingValue; // 売買代金
+//	public String     tvDate;       // YYYY/MM/DD
+	
+	// average trading values
+//	public long        quarterTradingValue;  // 平均売買代金（直近90日）
+//	public long        quarterTradingVolume; // 平均売買高（直近90日）
+	
+	
+	public ETF(
+			String     date,
+			String     stockCode,
+			String     listingDate,
+			BigDecimal expenseRatio,
+			BigDecimal divAnnual,
+			int        divFreq,
+			String     stockName,
+			String     categoryName,
+			String     productType,
+			String     targetName,
+			String     targetDescription
+			) {
+			this.date              = date;
+			this.stockCode         = stockCode;
+			this.listingDate       = listingDate;
+			this.expenseRatio      = expenseRatio;
+			this.divAnnual         = divAnnual;
+			this.divFreq           = divFreq;
+			this.stockName         = stockName;
+			this.categoryName      = categoryName;
+			this.productType       = productType;
+			this.targetName        = targetName;
+			this.targetDescription = targetDescription;
+	}
+	
+	@Override
+	public String toString() {
+	    return StringUtil.toString(this);
+	}
+
+	@Override
+	public int compareTo(ETF that) {
+		int ret = this.stockCode.compareTo(that.stockCode);
+		if (ret == 0) {
+			logger.error("Unexpected value");
+			logger.error("  this  {}");
+			logger.error("  that  {}");
+			throw new UnexpectedException("Unexpected value");
+		} else {
+			return ret;
+		}
+	}
+
+}
---
+++ yokwe-stock-jp/src/main/java/yokwe/stock/jp/jpx/UpdateETF.java
@@ -0,0 +1,296 @@
+package yokwe.stock.jp.jpx;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import yokwe.util.FileUtil;
+import yokwe.util.StringUtil;
+import yokwe.util.UnexpectedException;
+import yokwe.util.http.HttpUtil;
+import yokwe.util.json.JSON;
+import yokwe.util.json.JSON.Ignore;
+
+public class UpdateETF {
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(UpdateETF.class);
+
+	public static final class RAW {
+	    public static final class Data {
+	        public static final class DividendHist {
+	            public String     date; // YYYY/MM/DD
+	            public BigDecimal dividend;
+	            
+	            @Override
+	            public String toString() {
+	                return StringUtil.toString(this);
+	            }
+	        }
+	        
+	        public static final class Icons {
+	            public int etn;
+	            public int financialfuture;
+	            public int foreign;
+	            public int genbutsu;
+	            public int inv;
+	            public int jdr;
+	            public int lev;
+	            public int linked;
+	            public int longterm;
+	            public int otcswap;
+
+	            @Override
+	            public String toString() {
+	                return StringUtil.toString(this);
+	            }
+	        }
+	        
+	        public static final class ManagementCompany {
+	            public String code;
+	            public String name;
+	            public String url;
+
+	            @Override
+	            public String toString() {
+	                return StringUtil.toString(this);
+	            }
+	        }
+
+	        public static final class PcfWeight {
+	            public String     code;
+	            public String     name;
+	            public BigDecimal rank;
+	            public BigDecimal weight;
+
+	            @Override
+	            public String toString() {
+	                return StringUtil.toString(this);
+	            }
+	        }
+
+	        public int               categoryCode;
+	        public String            categoryName;
+	        public String            date;         // YYYY/MM/DD
+	        public BigDecimal        depth;
+	        public String            depthDate;
+	        
+	        public BigDecimal        deviation;     // 乖離率…前日市場価格÷前日基準価額を1口あたりに換算した値の割合を表示
+	        public String[]          disclaimer;
+	        public BigDecimal        dividend;      // 分配金…直近1年間の分配金額を表示
+	        public String            dividendDate;  // （年4回）
+	        public DividendHist[]    dividendHist;
+	        public BigDecimal        dividendYield; // 分配金利回り
+	        public int               exType;        // 0 for ETF
+	        public int               favoriteCount;
+	        public String            feature;       // explanation
+	        public boolean           hasNav;
+	        public String            iNavDate;
+	        
+	        @Ignore
+	        public Icons             icons;
+	        
+	        public int               inav;
+	        public int               isFavorite;
+	        public BigDecimal        liquidity;
+	        public String            listingDate;       // YYYY/MM/DD
+	        public ManagementCompany managementCompany;
+	        public BigDecimal        managementFee;     // 信託報酬 in percent
+	        public int               marketMake;
+	        public int               minInvest;
+	        public String            nav;               // 基準価額 "2,042.94"
+	        public long              netAssets;         // 純資産総額
+	        public String            netAssetsDate;     // YYYY/MM/DD
+	        public String            notice;
+	        public String            nriDate;
+	        public String            otherExpense;
+	        
+	        public String            pcfDataDate;
+	        public String            pcfFundDate;
+	        
+	        @Ignore
+	        public PcfWeight         pcfWeight;
+
+	        public BigDecimal        price;     // 終値・直近取引値
+	        public String            priceDate; // YYYY/MM/DD
+	        
+	        public int               productCode;
+	        public String            productType;
+	        
+	        public long              quarterTradingValue;  // 平均売買代金（直近90日）
+	        public long              quarterTradingVolume; // 平均売買高（直近90日）
+	        
+	        public int               reserve;
+	        
+	        public long              rightUnit;  // 受益権口数
+	        public String            sharesDate; // YYYY/MM/DD
+	        
+	        public String            shintakuRyuhogaku;
+	        
+	        public BigDecimal        spread;     // スプレッド…最良の売気配値段と買気配値段の価格差（%）
+	        public String            spreadDate; // STRING STRING
+	        
+	        public String            stockCode; // NNNN
+	        public String            stockName;
+	        
+	        public String            targetIndex;      // 対象指標
+	        public String            underlierOutline; // explanation of underline index
+	        
+	        public BigDecimal        tradingValue; // 売買代金
+	        public String            tvDate;       // YYYY/MM/DD
+	        
+	        public int               unit;   // 売買単位
+	        public long              volume; // 売買高
+
+	        public String            yokogaoLink;
+	        
+	        @Override
+	        public String toString() {
+	            return StringUtil.toString(this);
+	        }
+	    }
+
+	    public Data   data;
+	    public String status;
+
+	    @Override
+	    public String toString() {
+	        return StringUtil.toString(this);
+	    }
+	}
+
+	public static String convertDate(String string) {
+		// 2021/12/07 => 2021-12-07
+		String[] mdy = string.split("\\/");
+		if (mdy.length != 3) {
+			logger.error("Unpexpected");
+			logger.error("  date {}", string);
+			throw new UnexpectedException("Unpexpected");
+		}
+		return mdy[0] + "-" + mdy[1] + "-" + mdy[2];
+	}
+	
+	private static final Pattern PAT_DIVIDEND_DATE = Pattern.compile("（年([0-9]+)回）");
+	
+	private static ETF getInstance(RAW raw) {
+		String     date              = convertDate(raw.data.date);
+		String     stockCode         = Stock.toStockCode5(raw.data.stockCode);
+		String     listingDate       = convertDate(raw.data.listingDate);
+		BigDecimal expenseRatio      = raw.data.managementFee.scaleByPowerOfTen(-2);
+		BigDecimal divAnnual         = (raw.data.dividend == null) ? BigDecimal.ZERO : raw.data.dividend;
+		String     stockName         = raw.data.stockName;
+		String     categoryName      = raw.data.categoryName;
+		String     productType       = raw.data.productType;
+		String     targetName        = raw.data.targetIndex;
+		String     targetDescription = raw.data.underlierOutline;
+		
+		int divFreq;
+		if (raw.data.dividendDate == null) {
+			divFreq = 0;
+		} else {
+			if (raw.data.dividendDate.isEmpty()) {
+				divFreq = 0;
+			} else {
+				var m = PAT_DIVIDEND_DATE.matcher(raw.data.dividendDate);
+				if (m.matches()) {
+					String group1 = m.group(1);
+					divFreq = Integer.parseInt(group1);
+				} else {
+					logger.warn("Unpexpected dividendDate {}!", raw.data.dividendDate);
+					divFreq = 0;
+				}
+			}
+		}
+		
+		ETF etf = new ETF(
+				date, stockCode, listingDate, expenseRatio,
+				divAnnual, divFreq,
+				stockName, categoryName, productType,
+				targetName, targetDescription
+				);
+		
+		return etf;
+	}
+	
+	private static final String URL = "https://jpx.cloud.qri.jp/tosyo-moneybu/api/detail/info";
+
+	public static ETF getInstance(String stockCode, List<ETF.Dividend> dividendList) {
+		logger.info("stockCode {}", stockCode);
+		
+		String string;
+		
+		String path = String.format("tmp/etf/%s.json", stockCode);
+		File file = new File(path);
+		if (file.exists()) {
+			string = FileUtil.read().file(file);
+		} else {
+			String body        = String.format("{\"stockCode\":\"%s\"}", stockCode);
+			String contentType = "application/json;charset=UTF-8";
+			
+			string = HttpUtil.getInstance().withPost(body, contentType).download(URL).result;
+			
+			if (string == null) {
+				logger.warn("failed to download {}", stockCode);
+				return null;
+			} else {
+				FileUtil.write().file(file, string);
+			}
+		}
+				
+		RAW raw = JSON.unmarshal(RAW.class, string);
+		if (raw == null) {
+			logger.warn("failed to parse {}", stockCode);
+			return null;
+		} else {
+			if (raw.status.equals("0")) {
+				if (raw.data.dividendHist != null) {
+					for(var e: raw.data.dividendHist) {
+						var div = new ETF.Dividend(Stock.toStockCode5(stockCode), convertDate(e.date), e.dividend);
+						dividendList.add(div);
+					}
+				}
+				
+				return getInstance(raw);
+			} else {
+				logger.warn("status is no zero {}", stockCode);
+				return null;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		logger.info("START");
+		
+		{
+			List<Stock> stockList = Stock.getList().stream().filter(o -> o.isETF()).collect(Collectors.toList());
+			logger.info("stockList {}", stockList.size());
+			
+			List<ETF.Dividend>  divList = new ArrayList<>();
+			
+			int count = 0;
+			for(var e: stockList) {
+//				logger.info(String.format("%4d / %4d  %s", count, stockList.size(), e.stockCode));
+
+				count++;
+
+				String stockCode4 = Stock.toStockCode4(e.stockCode);
+				ETF etf = getInstance(stockCode4, divList);
+//				logger.info("etf {}", etf.toString());
+			}
+			
+			logger.info("divList        {}", divList.size());
+			Collections.sort(divList);
+			for(var e: divList) {
+				logger.info("div {}", e);
+			}
+		}
+		
+		logger.info("STOP");
+	}
+	
+}
