79bebec7ea1ccd0f2664e0b3bdbac0ba84f421bd
hasegawa <hasegawa@localhost>
2022-11-23 18:47:52 +0900

    Update for new Symbol.

--- yokwe-stock-trade/src/main/java/yokwe/stock/trade/monex/UpdateSymbol.java
+++ yokwe-stock-trade/src/main/java/yokwe/stock/trade/monex/UpdateSymbol.java
@@ -1,14 +1,8 @@
 package yokwe.stock.trade.monex;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
-import yokwe.stock.trade.monex.StockUS.Type;
-import yokwe.stock.us.nasdaq.Symbol;
-import yokwe.stock.us.nasdaq.api.AssetClass;
-import yokwe.util.UnexpectedException;
+import yokwe.stock.us.Symbol;
 
 //
 // Update nasdaq.Symbol for nasdaq
@@ -16,12 +10,6 @@ import yokwe.util.UnexpectedException;
 public class UpdateSymbol {
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(UpdateSymbol.class);
 
-	private static final Map<Type, AssetClass> map = new TreeMap<>();
-	static {
-		map.put(Type.ETF,   AssetClass.ETF);
-		map.put(Type.Stock, AssetClass.STOCK);
-	}
-	
 	public static void main(String[] args) {
 		logger.info("START");
 		
@@ -29,16 +17,7 @@ public class UpdateSymbol {
 		logger.info("extra {} {}", list.size(), Symbol.getPathExtra());
 		
 		for(var e: StockUS.getList()) {
-			AssetClass assetClass;
-			if (map.containsKey(e.type)) {
-				assetClass = map.get(e.type);
-			} else {
-				logger.error("Unexpected");
-				logger.error("  type {}", e.type);
-				throw new UnexpectedException("Unexptected");
-			}
-			Symbol symbol = new Symbol(e.symbol, assetClass, e.name);
-			list.add(symbol);
+			list.add(new Symbol(e.symbol));
 		}
 		
 		logger.info("save  {} {}", list.size(), Symbol.getPath());
