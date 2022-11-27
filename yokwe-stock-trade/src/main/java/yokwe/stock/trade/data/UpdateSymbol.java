204d8f9e9133f66826a875847890663ed34f2c4d
hasegawa <hasegawa@localhost>
2022-11-27 21:29:23 +0900

    Remove symbol that is not appeared in NASDAQSymbol

--- yokwe-stock-trade/src/main/java/yokwe/stock/trade/data/UpdateSymbol.java
+++ yokwe-stock-trade/src/main/java/yokwe/stock/trade/data/UpdateSymbol.java
@@ -5,6 +5,7 @@ import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import yokwe.stock.us.Symbol;
+import yokwe.stock.us.nasdaq.NASDAQSymbol;
 
 public class UpdateSymbol {
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(UpdateSymbol.class);
@@ -26,6 +27,29 @@ public class UpdateSymbol {
 		set.addAll(monexList);
 		set.addAll(sbiList);
 		set.addAll(rakutenList);
+		logger.info("set         {}", set.size());
+		
+		// remove symbol that is not appeared in NASDAQSymbol
+		{
+			Set<Symbol> nasdaqSet = NASDAQSymbol.getList().stream().map(o -> new Symbol(o.symbol)).collect(Collectors.toSet());
+			logger.info("nasdaqSet   {}", nasdaqSet.size());
+			
+			Set<Symbol> newSet = new TreeSet<>(set);
+			int count = 0;
+			for(Symbol e: newSet) {
+				if (nasdaqSet.contains(e)) {
+					//
+				} else {
+					// not appeared in nasdaqSet
+					logger.warn("Unknown symbol {}", e.symbol);
+					count++;
+					set.remove(e);
+				}
+			}
+			if (count != 0) {
+				logger.warn("Remove count   {}", count);
+			}
+		}
 		
 		logger.info("save        {} {}", set.size(), Symbol.getPath());
 		Symbol.save(set);
