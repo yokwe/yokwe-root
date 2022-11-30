2f050c055ceaa886073efb698ec492a3d5acc29b
hasegawa <hasegawa@localhost>
2022-11-30 17:54:14 +0900

    Use collection for map.values().

--- yokwe-util/src/main/java/yokwe/util/ListUtil.java
+++ yokwe-util/src/main/java/yokwe/util/ListUtil.java
@@ -4,52 +4,55 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.function.Function;
 
-public class ListUtil {
+public final class ListUtil {
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ListUtil.class);
 
 	// To support List and Set, use Collection here.
 	public static <E, K> SortedMap<K, E> checkDuplicate(Collection<E> collection, Function<E, K> getKey) {
 		var map = new TreeMap<K, E>();
-		for(var e: collection) {
-			var key = getKey.apply(e);
-			var old = map.put(key, e);
-			if (old != null) {
-				logger.error("Duplicate key");
-				logger.error("  key {}", key);
-				logger.error("  old {}", old);
-				logger.error("  new {}", e);
-				throw new UnexpectedException("Duplicate symbol");
+		if (collection != null) {
+			for(var e: collection) {
+				var key = getKey.apply(e);
+				var old = map.put(key, e);
+				if (old != null) {
+					logger.warn("Duplicate key");
+					logger.warn("  key {}", key);
+					logger.warn("  old {}", old);
+					logger.warn("  new {}", e);
+					// throw new UnexpectedException("Duplicate symbol");
+				}
 			}
 		}
-		
+				
 		return map;
 	}
 	
 	// To support List and Set, use Collection here.
 	public static <E> SortedSet<E> checkDuplicate(Collection<E> collection) {
 		var set = new TreeSet<E>();
-		for(var e: collection) {
-			if (set.add(e)) {
-				// new entry
-			} else {
-				// existing entry
-				logger.error("Duplicate value");
-				logger.error("  value {}", e.toString());
-				throw new UnexpectedException("Duplicate value");
+		if (collection != null) {
+			for(var e: collection) {
+				if (set.add(e)) {
+					// new entry
+				} else {
+					// existing entry
+					logger.error("Duplicate value");
+					logger.error("  value {}", e.toString());
+					throw new UnexpectedException("Duplicate value");
+				}
 			}
 		}
 		return set;
 	}
 	
-	public static <E extends Comparable<E>, K> void save(Class<E> clazz, String path, Set<E> set) {
-		save(clazz, path, new ArrayList<>(set));
+	public static <E extends Comparable<E>, K> void save(Class<E> clazz, String path, Collection<E> collection) {
+		save(clazz, path, new ArrayList<>(collection));
 	}
 	public static <E extends Comparable<E>, K> void save(Class<E> clazz, String path, List<E> list) {
 		// Sort before save
