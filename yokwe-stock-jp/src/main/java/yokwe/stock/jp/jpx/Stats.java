4470b1ef586d0162efbb42a6e84678846063dc62
hasegawa <hasegawa@localhost>
2022-12-23 10:24:34 +0900

    Add SheetName and ColumnName annotation

--- yokwe-stock-jp/src/main/java/yokwe/stock/jp/jpx/Stats.java
+++ yokwe-stock-jp/src/main/java/yokwe/stock/jp/jpx/Stats.java
@@ -3,8 +3,12 @@ package yokwe.stock.jp.jpx;
 import java.util.List;
 
 import yokwe.util.CSVUtil;
+import yokwe.util.libreoffice.Sheet;
 
-public class Stats {
+@Sheet.SheetName("stats")
+@Sheet.HeaderRow(0)
+@Sheet.DataRow(1)
+public class Stats extends Sheet {
 	private static final String PATH_FILE = getPath();
 	public static String getPath() {
 		return JPX.getPath("stats.csv");
@@ -20,54 +24,84 @@ public class Stats {
 
 	
 	//	public String exchange;
+	@Sheet.ColumnName("stockCode")
 	public String stockCode;
 	
+	@Sheet.ColumnName("name")
 	public String name;
 	
+	@Sheet.ColumnName("unit")
 	public int unit;
 	
 	// 33業種区分
+	@Sheet.ColumnName("sector33")
 	public String sector33;
 	
 	// 17業種区分
+	@Sheet.ColumnName("sector17")
 	public String sector17;
 	
 	// last values
+	@Sheet.ColumnName("date")
 	public String date;
+	@Sheet.ColumnName("price")
 	public double price;
 	
 	// price
+	@Sheet.ColumnName("pricec")
 	public int    pricec;
+	@Sheet.ColumnName("sd")
 	public double sd;
+	@Sheet.ColumnName("hv")
 	public double hv;
+	@Sheet.ColumnName("rsi")
 	public double rsi;
+	@Sheet.ColumnName("min")
 	public double min;
+	@Sheet.ColumnName("max")
 	public double max;
+	@Sheet.ColumnName("minPCT")
 	public double minPCT;
+	@Sheet.ColumnName("maxPCT")
 	public double maxPCT;
 
 	// dividend
+	@Sheet.ColumnName("div")
 	public double div;
+	@Sheet.ColumnName("divc")
 	public int    divc;
+	@Sheet.ColumnName("yield")
 	public double yield;
 	
 	// volume
+	@Sheet.ColumnName("vol")
 	public long   vol;
+	@Sheet.ColumnName("vol5")
 	public long   vol5;
+	@Sheet.ColumnName("vol21")
 	public long   vol21;
 	
 	// price change detection
+	@Sheet.ColumnName("last")
 	public double last;
+	@Sheet.ColumnName("lastPCT")
 	public double lastPCT;
 	
 	// fiscal year end date and half fiscal year end date
+	@Sheet.ColumnName("endDate1")
 	public String endDate1;
+	@Sheet.ColumnName("endDate2")
 	public String endDate2;
 	
 	// misc
+	@Sheet.ColumnName("numberOfIssuedK")
 	public long   numberOfIssuedK;
+	@Sheet.ColumnName("marketCapM")
 	public long   marketCapM;      // price * numberOfIssued
+	@Sheet.ColumnName("tradeCapM")
 	public long   tradeCapM;       // price * vol
+	@Sheet.ColumnName("volPCT")
 	public double volPCT;          // vol / numberOfIssued
+	@Sheet.ColumnName("feb17PCT")
 	public double feb17PCT;
 }
