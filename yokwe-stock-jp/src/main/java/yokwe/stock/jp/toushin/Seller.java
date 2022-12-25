93d8b0c4c3c0387116cdb465e15729aaf6436191
hasegawa <hasegawa@localhost>
2022-12-25 16:15:19 +0900

    Use ListUtil.load/save

--- yokwe-stock-jp/src/main/java/yokwe/stock/jp/toushin/Seller.java
+++ yokwe-stock-jp/src/main/java/yokwe/stock/jp/toushin/Seller.java
@@ -1,25 +1,23 @@
 package yokwe.stock.jp.toushin;
 
 import java.math.BigDecimal;
-import java.util.Collections;
 import java.util.List;
 
 import yokwe.stock.jp.Storage;
-import yokwe.util.CSVUtil;
+import yokwe.util.ListUtil;
 import yokwe.util.StringUtil;
 
-public class Seller implements Comparable<Seller>{
+public class Seller implements Comparable<Seller> {
 	public static final String PREFIX = "seller";
 	
 	public static final String getPath(String isinCode) {
 		return Storage.Toushin.getPath(PREFIX, isinCode + ".csv");
 	}
 	public static void save(String isinCode, List<Seller> list) {
-		Collections.sort(list);
-		CSVUtil.write(Seller.class).file(getPath(isinCode), list);
+		ListUtil.save(Seller.class, getPath(isinCode), list);
 	}
 	public static List<Seller> load(String isinCode) {
-		return CSVUtil.read(Seller.class).file(getPath(isinCode));
+		return ListUtil.load(Seller.class, getPath(isinCode));
 	}
 
 	public String     name;
