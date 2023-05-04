6c4c989a80fdc763bea8590f0110fd7bd7ec7e61
hasegawa <hasegawa@localhost>
2023-05-04 16:08:15 +0900

    Use stream to simplify calculation.

--- yokwe-stock-jp/src/main/java/yokwe/stock/jp/sony/UpdateFundStats.java
+++ yokwe-stock-jp/src/main/java/yokwe/stock/jp/sony/UpdateFundStats.java
@@ -63,40 +63,40 @@ public class UpdateFundStats {
 				List<Price> priceLast1Y = priceList.stream().filter(m -> m.date.isAfter(TODAY_1Y)).collect(Collectors.toList());
 				
 				if (priceLast1Y.size() == 0) {
-					stats.date         = "-";
-					stats.pricec      = 0;
-					stats.price       = BigDecimal.ZERO;
-					stats.priceMinPCT = BigDecimal.ZERO;
-					stats.priceMaxPCT = BigDecimal.ZERO;
-					stats.uam         = BigDecimal.ZERO;
-					stats.uamMinPCT   = BigDecimal.ZERO;
-					stats.uamMaxPCT   = BigDecimal.ZERO;
-					stats.unit        = BigDecimal.ZERO;
-					stats.unitMinPCT  = BigDecimal.ZERO;
-					stats.unitMaxPCT  = BigDecimal.ZERO;
+					stats.date      = "-";
+					stats.pricec   = 0;
+					stats.price    = BigDecimal.ZERO;
+					stats.priceMin = BigDecimal.ZERO;
+					stats.priceMax = BigDecimal.ZERO;
+					stats.uam      = BigDecimal.ZERO;
+					stats.uamMin   = BigDecimal.ZERO;
+					stats.uamMax   = BigDecimal.ZERO;
+					stats.unit     = BigDecimal.ZERO;
+					stats.unitMin  = BigDecimal.ZERO;
+					stats.unitMax  = BigDecimal.ZERO;
 				} else {
-					Price lastPrice   = priceLast1Y.get(priceLast1Y.size() - 1);
-					stats.date        = lastPrice.date.toString();
-					stats.pricec      = priceLast1Y.size();
-					stats.price       = lastPrice.price;
+					Price lastPrice = priceLast1Y.get(priceLast1Y.size() - 1);
+					stats.date      = lastPrice.date.toString();
+					stats.pricec    = priceLast1Y.size();
+					stats.price     = lastPrice.price;
 					
-					var priceMin      = priceLast1Y.stream().map(m -> m.price).min(Comparator.naturalOrder()).get();
-					var priceMax      = priceLast1Y.stream().map(m -> m.price).max(Comparator.naturalOrder()).get();
-					stats.priceMinPCT = stats.price.subtract(priceMin).divide(stats.price, 3, RoundingMode.HALF_UP);
-					stats.priceMaxPCT = priceMax.subtract(stats.price).divide(stats.price, 3, RoundingMode.HALF_UP);
+					var priceMin    = priceLast1Y.stream().map(m -> m.price).min(Comparator.naturalOrder()).get();
+					var priceMax    = priceLast1Y.stream().map(m -> m.price).max(Comparator.naturalOrder()).get();
+					stats.priceMin  = stats.price.subtract(priceMin).divide(stats.price, 3, RoundingMode.HALF_UP);
+					stats.priceMax  = priceMax.subtract(stats.price).divide(stats.price, 3, RoundingMode.HALF_UP);
 					
-					var uam           = lastPrice.uam;
-					var uamMin        = priceLast1Y.stream().map(m -> m.uam).min(Comparator.naturalOrder()).get();
-					var uamMax        = priceLast1Y.stream().map(m -> m.uam).max(Comparator.naturalOrder()).get();
-					stats.uam         = uam;
-					stats.uamMinPCT   = uam.subtract(uamMin).divide(uam, 3, RoundingMode.HALF_UP);
-					stats.uamMaxPCT   = uamMax.subtract(stats.uam).divide(uam, 3, RoundingMode.HALF_UP);
-					stats.unit        = lastPrice.unit;
+					var uam         = lastPrice.uam;
+					var uamMin      = priceLast1Y.stream().map(m -> m.uam).min(Comparator.naturalOrder()).get();
+					var uamMax      = priceLast1Y.stream().map(m -> m.uam).max(Comparator.naturalOrder()).get();
+					stats.uam       = uam;
+					stats.uamMin    = uam.subtract(uamMin).divide(uam, 3, RoundingMode.HALF_UP);
+					stats.uamMax    = uamMax.subtract(stats.uam).divide(uam, 3, RoundingMode.HALF_UP);
+					stats.unit      = lastPrice.unit;
 					
-					var unitMin       = priceLast1Y.stream().map(m -> m.unit).min(Comparator.naturalOrder()).get();
-					var unitMax       = priceLast1Y.stream().map(m -> m.unit).max(Comparator.naturalOrder()).get();
-					stats.unitMinPCT  = stats.unit.subtract(unitMin).divide(stats.unit, 3, RoundingMode.HALF_UP);
-					stats.unitMaxPCT  = unitMax.subtract(stats.unit).divide(stats.unit, 3, RoundingMode.HALF_UP);
+					var unitMin     = priceLast1Y.stream().map(m -> m.unit).min(Comparator.naturalOrder()).get();
+					var unitMax     = priceLast1Y.stream().map(m -> m.unit).max(Comparator.naturalOrder()).get();
+					stats.unitMin   = stats.unit.subtract(unitMin).divide(stats.unit, 3, RoundingMode.HALF_UP);
+					stats.unitMax   = unitMax.subtract(stats.unit).divide(stats.unit, 3, RoundingMode.HALF_UP);
 				}
 				
 			}
@@ -109,26 +109,26 @@ public class UpdateFundStats {
 					stats.div1Y      = BigDecimal.ZERO;
 					stats.yieldLast  = BigDecimal.ZERO;
 					stats.yield1Y    = BigDecimal.ZERO;
-				} else {
-					Dividend lastDiv = divList.get(divList.size() - 1);
-					stats.divLast = lastDiv.dividend;
+				} else {					
+					var divList1Y = divList.stream().filter(m -> m.date.isAfter(TODAY_1Y)).map(o -> o.dividend).collect(Collectors.toList());
+					var div1Y     = divList1Y.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
 					
-					List<Dividend> divList1Y = divList.stream().filter(m -> m.date.isAfter(TODAY_1Y)).collect(Collectors.toList());
-					stats.divc = divList1Y.size();
-					var divc = BigDecimal.valueOf(stats.divc);
-					stats.div1Y = BigDecimal.ZERO;
-					for(var e: divList1Y) {
-						stats.div1Y = stats.div1Y.add(e.dividend);
-					}
-					stats.yieldLast = stats.divLast.multiply(divc).divide(stats.price, 8, RoundingMode.HALF_UP);
-					stats.yield1Y   = stats.div1Y.divide(stats.price, 8, RoundingMode.HALF_UP);
-					
-					if (stats.div1Y.equals(BigDecimal.ZERO)) {
+					if (div1Y.equals(BigDecimal.ZERO)) {
 						stats.divc       = 0;
 						stats.divLast    = BigDecimal.ZERO;
 						stats.div1Y      = BigDecimal.ZERO;
 						stats.yieldLast  = BigDecimal.ZERO;
 						stats.yield1Y    = BigDecimal.ZERO;	
+					} else {
+						int divc      = divList1Y.size();
+						var divLast   = divList1Y.get(divc - 1);
+						var divLast1Y = divLast.multiply(BigDecimal.valueOf(divc));
+						
+						stats.divc      = divc;
+						stats.divLast   = divLast;
+						stats.div1Y     = div1Y;
+						stats.yieldLast = divLast1Y.divide(stats.price, 8, RoundingMode.HALF_UP);
+						stats.yield1Y   = div1Y.divide(stats.price, 8, RoundingMode.HALF_UP);
 					}
 				}
 			}
