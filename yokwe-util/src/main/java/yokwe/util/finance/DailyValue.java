3edd38ea3b7c429ab9cebfd3346e8e5629a315f4
hasegawa <hasegawa@localhost>
2023-05-15 11:45:08 +0900

    Temporary commit.

--- yokwe-util/src/main/java/yokwe/util/finance/DailyValue.java
+++ yokwe-util/src/main/java/yokwe/util/finance/DailyValue.java
@@ -3,11 +3,40 @@ package yokwe.util.finance;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import yokwe.util.StringUtil;
 
 public class DailyValue implements Comparable<DailyValue> {
+	public static BigDecimal[] toArray(List<DailyValue> list) {
+		List<BigDecimal> ret = new ArrayList<>(list.size());
+		for(var e: list) {
+			ret.add(e.value);
+		}
+		return ret.toArray(new BigDecimal[0]);
+	}
+	public static BigDecimal[] toArray(List<DailyValue> list, LocalDate firstDate, LocalDate lastDate) {
+		// firstDate and lastDate is inclusive
+		LocalDate first = firstDate.minusDays(1);
+		LocalDate last  = lastDate.plusDays(1);
+		
+		List<BigDecimal> ret = new ArrayList<>(list.size());
+		for(var e: list) {
+			if (e.date.isAfter(first) && e.date.isBefore(last)) {
+				ret.add(e.value);
+			}
+		}
+		return ret.toArray(new BigDecimal[0]);
+	}
+	
+	
 	public static DailyValue getInstance(LocalDate date, BigDecimal value) {
+		return new DailyValue(date, value);
+	}
+	public static DailyValue getInstance(String stringDate, double doubleValue) {
+		LocalDate  date = LocalDate.parse(stringDate);
+		BigDecimal value = doubleValue == 0 ? BigDecimal.ZERO : BigDecimal.valueOf(doubleValue);
 		return new DailyValue(date, value);
 	}
 	
