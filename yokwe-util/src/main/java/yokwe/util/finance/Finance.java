f8db2ddf688855a072b099698ad40c3b8a3bc3c9
hasegawa <hasegawa@localhost>
2023-05-17 12:05:10 +0900

    Delete Math and create Finance.

--- yokwe-util/src/main/java/yokwe/util/finance/Finance.java
+++ yokwe-util/src/main/java/yokwe/util/finance/Finance.java
@@ -4,7 +4,9 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
 
-public final class Math {
+import yokwe.util.StringUtil;
+
+public final class Finance {
 	public static final int          DEFAULT_INTERNAL_SCALE = 15;
 	public static final RoundingMode DEFAULT_ROUNDING_MODE  = RoundingMode.HALF_UP;
 	public static final MathContext  DEFAULT_MATH_CONTEXT   = new MathContext(DEFAULT_INTERNAL_SCALE, DEFAULT_ROUNDING_MODE);
@@ -20,17 +22,33 @@ public final class Math {
 		return sum(values, mathContext).divide(BigDecimal.valueOf(values.length), mathContext);
 	}
 	
+	public static BigDecimal annualizedRetrun(BigDecimal first, BigDecimal last, int years) {
+		// annualized return = (((last / first) ^ (1 / years)) - 1
+		double power = 1.0 / years;
+		double absoluteReturn = last.doubleValue() / first.doubleValue();
+		double annualizedReturn = Math.pow(absoluteReturn, power) - 1.0;
+		return BigDecimal.valueOf(annualizedReturn).round(DEFAULT_MATH_CONTEXT);
+	}
+	
+	
 	public static class Stats {
 		public final BigDecimal mean;
 		public final BigDecimal variance;
-		public final BigDecimal sd;       // standard deviation
+		public final BigDecimal sd;       // Standard Deviation
+		public final BigDecimal cv;       // Coefficient of Variation
 		public final BigDecimal diff[];
 		
-		public Stats(BigDecimal mean, BigDecimal variance, BigDecimal sd, BigDecimal diff[]) {
+		public Stats(BigDecimal mean, BigDecimal variance, BigDecimal sd, BigDecimal cv, BigDecimal diff[]) {
 			this.mean     = mean;
 			this.variance = variance;
 			this.sd       = sd;
+			this.cv       = cv;
 			this.diff     = diff;
+		}
+		
+		@Override
+		public String toString() {
+			return StringUtil.toString(this);
 		}
 	}
 	public static Stats stats(BigDecimal[] values, MathContext mathContext) {
@@ -46,8 +64,26 @@ public final class Math {
 		}
 		BigDecimal variance = var.divide(BigDecimal.valueOf(size), mathContext);
 		BigDecimal sd       = variance.sqrt(mathContext);
+		BigDecimal cv       = sd.divide(mean, mathContext);
 		
-		return new Stats(mean, variance, sd, diff);
+		return new Stats(mean, variance, sd, cv, diff);
 	}
 	
+	public static void main(String[] args) {
+		final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
+
+		logger.info("START");
+		
+		int[] intArray = {14, 2, 13, 20, 16};
+		BigDecimal[] valueArray = new BigDecimal[intArray.length];
+		for(int i = 0; i < intArray.length; i++) {
+			valueArray[i] = BigDecimal.valueOf(intArray[i]);
+		}
+		
+		MathContext mathContext = new MathContext(4, DEFAULT_ROUNDING_MODE);
+		Stats stats = stats(valueArray, mathContext);
+		logger.info("stats  {}  {}  {}", stats.mean, stats.sd, stats.cv);
+		
+		logger.info("STOP");
+	}
 }
