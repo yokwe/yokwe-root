1f2cd42af7119a02d20248662dece5dbe4ac546f
hasegawa <hasegawa@localhost>
2023-05-21 11:16:43 +0900

    Temporary commi.t

--- yokwe-stock-us/src/main/java/yokwe/stock/us/nyse/NYSESymbol.java
+++ yokwe-stock-us/src/main/java/yokwe/stock/us/nyse/NYSESymbol.java
@@ -8,11 +8,11 @@ import yokwe.util.ListUtil;
 import yokwe.util.StringUtil;
 
 public final class NYSESymbol {
-	public static final class Data implements Comparable<Data> {
+	public static final class Data implements Comparable<Data> {		
 		public String exchangeId;
 		public String instrumentName;
-		public String instrumentType;
-		public String micCode;
+		public Type   instrumentType;
+		public MIC    micCode;
 		public String normalizedTicker;
 		public String symbolEsignalTicker;
 		public String symbolExchangeTicker;
@@ -27,9 +27,29 @@ public final class NYSESymbol {
 			return StringUtil.toString(this);
 		}
 
+		public String getKey() {
+			return normalizedTicker;
+		}
 		@Override
 		public int compareTo(Data that) {
-			return this.normalizedTicker.compareTo(that.normalizedTicker);
+			return this.getKey().compareTo(that.getKey());
+		}
+		@Override
+		public int hashCode() {
+			return getKey().hashCode();
+		}
+		@Override
+		public boolean equals(Object o) {
+			if (o != null) {
+				if (o instanceof Data) {
+					Data that = (Data)o;
+					return this.compareTo(that) == 0;
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
 		}
 	}
 	
@@ -72,4 +92,128 @@ public final class NYSESymbol {
 			return ListUtil.getList(Data.class, getPath());
 		}
 	}
+	
+	
+	public enum Type {
+		CEF    ("CLOSED_END_FUND",              true,  false),
+		COMMON ("COMMON_STOCK",                 true,  false),
+		ADR    ("DEPOSITORY_RECEIPT",           true,  false),
+		ETF    ("EXCHANGE_TRADED_FUND",         false, true),
+		ETN    ("EXCHANGE_TRADED_NOTE",         true,  false),
+		LP     ("LIMITED_PARTNERSHIP",          true,  false),
+		PREF   ("PREFERRED_STOCK",              true,  false),
+		REIT   ("REIT",                         true,  false),
+		TRUST  ("TRUST",                        false, false),
+		UNIT   ("UNIT",                         false, false),
+		UBI    ("UNITS_OF_BENEFICIAL_INTEREST", false, false);
+		
+		public final String value;
+		public final boolean stock;
+		public final boolean etf;
+		
+		Type(String value, boolean stock, boolean etf) {
+			this.value = value;
+			this.stock = stock;
+			this.etf   = etf;
+		}
+		
+		@Override
+		public String toString() {
+			return value;
+		}
+	}
+	
+	// Market Identifier Code
+	public enum MIC {
+		ARCX("ARCX", true,  false), // NYSE ARCA
+		BATS("BATS", false, false), // CBOE BZX U.S. EQUITIES EXCHANGE
+		XASE("XASE", true,  false), // NYSE MKT LLC
+		XNCM("XNCM", false, true),  // NASDAQ CAPITAL MARKET
+		XNGS("XNGS", false, true),  // NASDAQ/NGS (GLOBAL SELECT MARKET)
+		XNMS("XNMS", false, true),  // NASDAQ/NMS (GLOBAL MARKET)
+		XNYS("XNYS", true,  false); // NEW YORK STOCK EXCHANGE, INC.
+
+		public final String  value;
+		public final boolean nyse;
+		public final boolean nasdaq;
+		MIC(String value, boolean nyse, boolean nasdaq) {
+			this.value  = value;
+			this.nyse   = nyse;
+			this.nasdaq = nasdaq;
+		}
+		
+		@Override
+		public String toString() {
+			return value;
+		}
+		
+		public boolean isNYSE() {
+			return nyse;
+		}
+		public boolean isNASDAQ() {
+			return nasdaq;
+		}
+	}
+	
+
+	public static final class Symbol implements Comparable<Symbol> {
+		private static final String PATH_FILE = Storage.NYSE.getPath("symbol.csv");
+		public static String getPath() {
+			return PATH_FILE;
+		}
+		public static void save(Collection<Symbol> collection) {
+			ListUtil.save(Symbol.class, getPath(), collection);
+		}
+		public static void save(List<Symbol> list) {
+			ListUtil.save(Symbol.class, getPath(), list);
+		}
+		public static List<Symbol> load() {
+			return ListUtil.load(Symbol.class, getPath());
+		}
+		public static List<Symbol> getList() {
+			return ListUtil.getList(Symbol.class, getPath());
+		}
+		
+		public String symbol;
+		public MIC    market;
+		public Type   type;
+		public String name;
+		
+		public Symbol(String symbol, MIC market, Type type, String name) {
+			this.symbol = symbol;
+			this.market = market;
+			this.type   = type;
+			this.name   = name;
+		}
+
+		@Override
+		public String toString() {
+			return StringUtil.toString(this);
+		}
+		public String getKey() {
+			return symbol;
+		}
+		@Override
+		public int compareTo(Symbol that) {
+			return this.getKey().compareTo(that.getKey());
+		}
+		@Override
+		public int hashCode() {
+			return getKey().hashCode();
+		}
+		@Override
+		public boolean equals(Object o) {
+			if (o != null) {
+				if (o instanceof Symbol) {
+					Symbol that = (Symbol)o;
+					return this.compareTo(that) == 0;
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+	}
+
 }
--- yokwe-stock-us/src/main/java/yokwe/stock/us/nyse/UpdateNYSESymbol.java
+++ yokwe-stock-us/src/main/java/yokwe/stock/us/nyse/UpdateNYSESymbol.java
@@ -1,25 +1,35 @@
 package yokwe.stock.us.nyse;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import yokwe.stock.us.nyse.Filter.Kind;
 import yokwe.stock.us.nyse.NYSESymbol.Data;
+import yokwe.stock.us.nyse.NYSESymbol.Symbol;
+import yokwe.util.ListUtil;
 
 public class UpdateNYSESymbol {
 	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
 
-	private static void downloadStock() {
+	public static void downloadStock() {
 		List<Data> list = Filter.download(Kind.STOCK);
 		List<Data> list2 = list.stream().filter(o -> !o.symbolTicker.startsWith("E:")).collect(Collectors.toList());
+		for(var e: list2) {
+			e.instrumentName = e.instrumentName.replace(",", "");
+		}
 		
 		logger.info("save  {}  {}", list2.size(), NYSESymbol.Stock.getPath());
 		NYSESymbol.Stock.save(list2);
 	}
 
-	private static void downloadETF() {
+	public static void downloadETF() {
 		List<Data> list = Filter.download(Kind.ETF);
 		List<Data> list2 = list.stream().filter(o -> !o.symbolTicker.startsWith("E:")).collect(Collectors.toList());
+		for(var e: list2) {
+			e.instrumentName = e.instrumentName.replace(",", "");
+		}
 
 		logger.info("save  {}  {}", list2.size(), NYSESymbol.ETF.getPath());
 		NYSESymbol.ETF.save(list2);
@@ -28,9 +38,30 @@ public class UpdateNYSESymbol {
 	public static void main(String[] args) {
 		logger.info("START");
 		
-		downloadStock();
-		downloadETF();
+//		downloadStock();
+//		downloadETF();
 		
+		{
+			List<Symbol> list = new ArrayList<>();
+			
+			for(var e: NYSESymbol.Stock.getList()) {
+				list.add(new Symbol(e.symbolTicker, e.micCode, e.instrumentType, e.instrumentName));
+			}
+			for(var e: NYSESymbol.ETF.getList()) {
+				list.add(new Symbol(e.symbolTicker, e.micCode, e.instrumentType, e.instrumentName));
+			}
+			logger.info("list   {}", list.size());
+			
+			List<Symbol> list2 = list.stream().filter(o -> o.type.stock || o.type.etf).collect(Collectors.toList());
+			logger.info("list2  {}", list2.size());
+			
+			// sanity check
+			ListUtil.checkDuplicate(list2, Symbol::getKey);
+			
+			Collections.sort(list2);
+			logger.info("save  {}  {}", list2.size(), Symbol.getPath());
+			Symbol.save(list2);
+		}
 		logger.info("STOP");
 	}
 }
