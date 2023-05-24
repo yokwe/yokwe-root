e0747ebe3552b061d449bdd72178dcffb57f7649
hasegawa <hasegawa@localhost>
2023-05-24 09:03:41 +0900

    Add conversion static method AssetClass.getInstance(Type)

--- yokwe-stock-us/src/main/java/yokwe/stock/us/nasdaq/api/AssetClass.java
+++ yokwe-stock-us/src/main/java/yokwe/stock/us/nasdaq/api/AssetClass.java
@@ -1,8 +1,15 @@
 package yokwe.stock.us.nasdaq.api;
 
+import yokwe.stock.us.Stock.SimpleType;
+import yokwe.stock.us.Stock.Type;
+
 public enum AssetClass {
 	STOCK("stocks"),
 	ETF  ("etf");
+	
+	public static AssetClass getInstance(Type type) {
+		return type.simpleType == SimpleType.ETF ? ETF : STOCK;
+	}
 	
 	public final String value;
 	AssetClass(String value) {
