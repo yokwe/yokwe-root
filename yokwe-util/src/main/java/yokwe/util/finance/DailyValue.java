d2779529ffb620d9c759d7c7e56272c7a4064968
hasegawa <hasegawa@localhost>
2023-05-29 16:28:34 +0900

    Add generic method that takes Function

--- yokwe-util/src/main/java/yokwe/util/finance/DailyValue.java
+++ yokwe-util/src/main/java/yokwe/util/finance/DailyValue.java
@@ -3,15 +3,46 @@ package yokwe.util.finance;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Function;
 
 import yokwe.util.StringUtil;
+import yokwe.util.UnexpectedException;
 
 public final class DailyValue implements Comparable<DailyValue> {
-	// value array
-	public static BigDecimal[] toValueArray(List<DailyValue> list) {
-		return list.stream().map(o -> o.value).toArray(BigDecimal[]::new);
+	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
+	
+	//
+	// create DailyValue array from array of other type
+	//
+	public static <T> DailyValue[] toArray(T[] array, Function<T, LocalDate> toDate, Function<T, BigDecimal> toValue) {
+		DailyValue[] ret = new DailyValue[array.length];
+		
+		for(int i = 0; i < array.length; i++) {
+			T element = array[i];
+			ret[i] = new DailyValue(toDate.apply(element), toValue.apply(element));
+		}
+		
+		return ret;
 	}
+	
+	
+	//
+	// filter array using firstDate and lastDate
+	//
+	public static DailyValue[] filter(DailyValue[] array, LocalDate firstDate, LocalDate lastDate) {
+		return Arrays.stream(array).filter(o -> o.date.isAfter(firstDate.minusDays(1)) && o.date.isBefore(lastDate.plusDays(1))).toArray(DailyValue[]::new);
+	}
+	public static BigDecimal[] filterValue(DailyValue[] array, LocalDate firstDate, LocalDate lastDate) {
+		return Arrays.stream(array).filter(o -> o.date.isAfter(firstDate.minusDays(1)) && o.date.isBefore(lastDate.plusDays(1))).map(o -> o.value).toArray(BigDecimal[]::new);
+	}
+	
+	
+	//
+	// create value array from DailyValue array
+	//
 	public static BigDecimal[] toValueArray(DailyValue[] array, int startIndex, int stopIndexPlusOne) {
 		return BigDecimalArrays.toArray(array, startIndex, stopIndexPlusOne, o -> o.value);
 	}
@@ -19,11 +50,20 @@ public final class DailyValue implements Comparable<DailyValue> {
 		return toValueArray(array, 0, array.length);
 	}
 	
-	// duration
-	public static BigDecimal duration(DailyValue[] array) {
-		LocalDate startDate = array[0].date;
+	
+	
+	//
+	// get duration of DailyValue array
+	//
+	public static BigDecimal duration(LocalDate startDate, LocalDate endDate) {
+		Period period = startDate.until(endDate);
+		String string = String.format("%d.%02d", period.getYears(), period.getMonths());
+		return new BigDecimal(string);
+	}
+	public static BigDecimal duration(DailyValue[] array, int startIndex, int stopIndexPlusOne) {
+		LocalDate startDate = array[startIndex].date;
 		LocalDate endDate  = startDate;
-		for(int i = 1; i < array.length; i++) {
+		for(int i = startIndex + 1; i < stopIndexPlusOne; i++) {
 			LocalDate date = array[i].date;
 			if (date.isBefore(startDate)) startDate = date;
 			if (date.isAfter(endDate))   endDate = date;
@@ -31,10 +71,40 @@ public final class DailyValue implements Comparable<DailyValue> {
 		
 		return duration(startDate, endDate);
 	}
-	public static BigDecimal duration(LocalDate startDate, LocalDate endDate) {
-		Period period = startDate.until(endDate);
-		String string = String.format("%d.%02d", period.getYears(), period.getMonths());
-		return new BigDecimal(string);
+	public static BigDecimal duration(DailyValue[] array) {
+		return duration(array, 0, array.length);
+	}
+	
+	
+	//
+	// reverse element of array
+	//
+	public static void reverse(DailyValue[] array) {
+		for(int i = 0, j = array.length - 1; i < array.length / 2; i++, j--) {
+			DailyValue save = array[j];
+			array[j] = array[i];
+			array[i] = save;
+		}
+	}
+	
+	
+	//
+	// toMap
+	//
+	public static Map<LocalDate, BigDecimal> toMap(DailyValue[] array) {
+		Map<LocalDate, BigDecimal> map = new TreeMap<>();
+		
+		for(var e: array) {
+			var oldValue = map.put(e.date, e.value);
+			if (oldValue != null) {
+				logger.error("Duplicate date");
+				logger.error("  date  {}", e.date);
+				logger.error("  new  {}",  e.value);
+				logger.error("  old  {}",  oldValue);
+				throw new UnexpectedException("Duplicate date");
+			}
+		}
+		return map;
 	}
 	
 	
@@ -50,9 +120,12 @@ public final class DailyValue implements Comparable<DailyValue> {
 	public String toString() {
 		return StringUtil.toString(this);
 	}
+	LocalDate getKey() {
+		return date;
+	}
 	@Override
 	public int compareTo(DailyValue that) {
-		return this.date.compareTo(that.date);
+		return this.getKey().compareTo(that.getKey());
 	}
 	@Override
 	public boolean equals(Object o) {
@@ -69,6 +142,6 @@ public final class DailyValue implements Comparable<DailyValue> {
 	}
 	@Override
 	public int hashCode() {
-		return this.date.hashCode();
+		return this.getKey().hashCode();
 	}
 }
