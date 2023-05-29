62adc1e6dd5cde419f66f78fff9353107b8f058c
hasegawa <hasegawa@localhost>
2023-05-29 16:22:15 +0900

    Use MonthlyStats and AnnualStats

--- yokwe-stock-jp/src/main/java/yokwe/stock/jp/toushin/UpdateStats.java
+++ yokwe-stock-jp/src/main/java/yokwe/stock/jp/toushin/UpdateStats.java
@@ -4,57 +4,19 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import yokwe.util.finance.BigDecimalArrays;
-import yokwe.util.finance.BigDecimalUtil;
+import yokwe.util.finance.AnnualStats;
+import yokwe.util.finance.DailyValue;
+import yokwe.util.finance.Finance;
+import yokwe.util.finance.MonthlyStats;
 
 public class UpdateStats {
 	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
 	
-	private static record MyStats (
-		BigDecimal sd, BigDecimal div, BigDecimal yield, BigDecimal reinvestedReturn) {}
-	
-private static MyStats getMyStats(List<PriceStats> priceStatsList, List<Dividend>divList, LocalDate targetDate, int years) {
-		// years = 1  targetDate = 2010-01-02  lastDate = 2010-01-03  firstDate = 2009-01-02
-		final LocalDate lastDate  = targetDate.plusDays(1);
-		final LocalDate firstDate = targetDate.minusYears(years);
-		
-		BigDecimal[] logReturnArray = priceStatsList.stream().filter(o -> o.date.isAfter(firstDate) && o.date.isBefore(lastDate)).map(o -> o.logReturn).toArray(BigDecimal[]::new);
-		BigDecimal[] reinvestedPriceArray = priceStatsList.stream().filter(o -> o.date.isAfter(firstDate) && o.date.isBefore(lastDate)).map(o -> o.reinvestedPrice).toArray(BigDecimal[]::new);
-		BigDecimal[] priceArray = priceStatsList.stream().filter(o -> o.date.isAfter(firstDate) && o.date.isBefore(lastDate)).map(o -> o.price).toArray(BigDecimal[]::new);
-		BigDecimal[] divArray = divList.stream().filter(o -> o.date.isAfter(firstDate) && o.date.isBefore(lastDate)).map(o -> o.amount).toArray(BigDecimal[]::new);
-
-		final BigDecimal sd;
-		final BigDecimal div;
-		final BigDecimal yield;
-		final BigDecimal reinvestedReturn;
-		
-		sd = BigDecimalArrays.sd(logReturnArray, BigDecimalUtil.DEFAULT_MATH_CONTEXT);
-		{
-			BigDecimal lastPrice = priceArray[priceArray.length - 1];
-			BigDecimal totalDiv = BigDecimal.ZERO;
-			for(var e: divArray) {
-				totalDiv = totalDiv.add(e);
-			}
-			div   = totalDiv.divide(BigDecimal.valueOf(years), BigDecimalUtil.DEFAULT_MATH_CONTEXT);
-			yield = div.divide(lastPrice, BigDecimalUtil.DEFAULT_MATH_CONTEXT);
-		}
-		{
-			var first = reinvestedPriceArray[0];
-			var last  = reinvestedPriceArray[reinvestedPriceArray.length - 1];
-			reinvestedReturn = BigDecimalUtil.annualizedRetrun(first, last, years);
-		}
-		return new MyStats(sd, div, yield, reinvestedReturn);
-	}
-	
 	public static void main(String[] args) {
 		logger.info("START");
-		
-//		final LocalDate today = LocalDate.now();
-		final LocalDate today = LocalDate.parse("2023-04-30");
-
-		logger.info("today {}", today);
 		
 		final BigDecimal minus1 = BigDecimal.ONE.negate();
 		
@@ -77,15 +39,20 @@ private static MyStats getMyStats(List<PriceStats> priceStatsList, List<Dividend
 			count++;
 			if ((count % 500) == 1) logger.info("{}", String.format("%4d / %4d", count, fundList.size()));
 			
-			List<PriceStats> priceStatsList = PriceStats.getList(isinCode);
-			if (priceStatsList.isEmpty()) {
-//				logger.warn("{}  priceStatsList is empty", isinCode);
+			Price[] rawPriceArray = Price.getList(isinCode).stream().toArray(Price[]::new);
+			if (rawPriceArray.length == 0) {
+//				logger.warn("{}  rawPriceArray is empty", isinCode);
 				countNoPrice++;
 				continue;
 			}
+			Price lastPrice = rawPriceArray[rawPriceArray.length - 1];
 			
-//			List<DailyValue> priceList = priceStatsList.stream().map(o -> DailyValue.getInstance(o.date, o.reinvestedPrice)).toList();
-			PriceStats lastPrice = priceStatsList.get(priceStatsList.size() - 1);
+			DailyValue[] priceArray = Arrays.stream(rawPriceArray).map(o -> new DailyValue(o.date, o.price)).toArray(DailyValue[]::new);
+			DailyValue[] divArray   = Dividend.getList(isinCode).stream().map(o -> new DailyValue(o.date, o.amount)).toArray(DailyValue[]::new);
+			DailyValue[] reinvestedPriceArray = Finance.toReinvested(priceArray, divArray);
+			
+			MonthlyStats[] monthlyStatsArray = MonthlyStats.monthlyStatsArray(reinvestedPriceArray, divArray, 121);
+			logger.info("monthlyStatsArray0  {}", monthlyStatsArray[0].endDate);
 
 			var nikkei = nikkeiMap.get(isinCode);
 			if (nikkei == null) {
@@ -93,8 +60,6 @@ private static MyStats getMyStats(List<PriceStats> priceStatsList, List<Dividend
 				countNoNikkei++;
 				continue;
 			}
-			
-			var divList = Dividend.getList(isinCode);
 			
 			Stats stats = new Stats();
 			
@@ -104,12 +69,13 @@ private static MyStats getMyStats(List<PriceStats> priceStatsList, List<Dividend
 			stats.inception  = fund.inceptionDate;
 			stats.redemption = fund.redemptionDate;
 			{
+				LocalDate today = LocalDate.now();
 				Period period = stats.inception.until(today);
 				stats.age = new BigDecimal(String.format("%d.%02d", period.getYears(), period.getMonths()));
 			}
 			stats.qCat1  = nikkei.category1;
 			stats.qCat2  = nikkei.category2;
-			stats.forex = nikkei.category3.replace("為替リスク", "");
+			stats.forex  = nikkei.category3.replace("為替リスク", "");
 			stats.type   = nikkei.fundType.compareTo("アクティブ型") == 0 ? "ACTIVE" : "INDEX"; // アクティブ型 or インデックス型
 			
 			stats.date  = lastPrice.date;
@@ -121,11 +87,13 @@ private static MyStats getMyStats(List<PriceStats> priceStatsList, List<Dividend
 
 			// 1 year
 			if (1.0 <= stats.age.doubleValue()) {
-				MyStats myStats = getMyStats(priceStatsList, divList, today, 1);
-				stats.sd1Y     = myStats.sd;
-				stats.div1Y    = myStats.div;
-				stats.yield1Y  = myStats.yield;
-				stats.return1Y = myStats.reinvestedReturn;
+				int nYear = 1;
+				AnnualStats  aStats = new AnnualStats(monthlyStatsArray, nYear);
+
+				stats.sd1Y     = aStats.sd;
+				stats.div1Y    = aStats.div;
+				stats.yield1Y  = aStats.yield;
+				stats.return1Y = aStats.returns;
 			} else {
 				stats.sd1Y     = minus1;
 				stats.div1Y    = minus1;
@@ -134,11 +102,13 @@ private static MyStats getMyStats(List<PriceStats> priceStatsList, List<Dividend
 			}
 			// 3 year
 			if (3.0 <= stats.age.doubleValue()) {
-				MyStats myStats = getMyStats(priceStatsList, divList, today, 3);
-				stats.sd3Y     = myStats.sd;
-				stats.div3Y    = myStats.div;
-				stats.yield3Y  = myStats.yield;
-				stats.return3Y = myStats.reinvestedReturn;
+				int nYear = 3;
+				AnnualStats  aStats = new AnnualStats(monthlyStatsArray, nYear);
+				
+				stats.sd3Y     = aStats.sd;
+				stats.div3Y    = aStats.div;
+				stats.yield3Y  = aStats.yield;
+				stats.return3Y = aStats.returns;
 			} else {
 				stats.sd3Y     = minus1;
 				stats.div3Y    = minus1;
@@ -147,11 +117,13 @@ private static MyStats getMyStats(List<PriceStats> priceStatsList, List<Dividend
 			}
 			// 5 year
 			if (5.0 <= stats.age.doubleValue()) {
-				MyStats myStats = getMyStats(priceStatsList, divList, today, 5);
-				stats.sd5Y     = myStats.sd;
-				stats.div5Y    = myStats.div;
-				stats.yield5Y  = myStats.yield;
-				stats.return5Y = myStats.reinvestedReturn;
+				int nYear = 5;
+				AnnualStats  aStats = new AnnualStats(monthlyStatsArray, nYear);
+				
+				stats.sd5Y     = aStats.sd;
+				stats.div5Y    = aStats.div;
+				stats.yield5Y  = aStats.yield;
+				stats.return5Y = aStats.returns;
 			} else {
 				stats.sd5Y     = minus1;
 				stats.div5Y    = minus1;
