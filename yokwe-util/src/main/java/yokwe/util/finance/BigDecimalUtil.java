60e93cfbd73967614beadc7b4fc67da79efe8019
hasegawa <hasegawa@localhost>
2023-05-29 19:38:14 +0900

    Call round(DEFAULT_MATH_CONTEXT) at return of method.

--- yokwe-util/src/main/java/yokwe/util/finance/BigDecimalUtil.java
+++ yokwe-util/src/main/java/yokwe/util/finance/BigDecimalUtil.java
@@ -12,20 +12,12 @@ public final class BigDecimalUtil {
 	
 	// return log value using Math.log
 	public static BigDecimal mathLog(BigDecimal x) {
-		return BigDecimal.valueOf(Math.log(x.doubleValue()));
+		return BigDecimal.valueOf(Math.log(x.doubleValue())).round(DEFAULT_MATH_CONTEXT);
 	}
 	public static BigDecimal mathExp(BigDecimal x) {
-		return BigDecimal.valueOf(Math.exp(x.doubleValue()));
+		return BigDecimal.valueOf(Math.exp(x.doubleValue())).round(DEFAULT_MATH_CONTEXT);
 	}
 	public static BigDecimal mathPow(BigDecimal x, BigDecimal y) {
-		return BigDecimal.valueOf(Math.pow(x.doubleValue(), y.doubleValue()));
-	}
-	
-	public static BigDecimal annualizedRetrun(BigDecimal first, BigDecimal last, int years) {
-		// annualized return = (((last / first) ^ (1 / years)) - 1
-		double power = 1.0 / years;
-		double absoluteReturn = last.doubleValue() / first.doubleValue();
-		double annualizedReturn = Math.pow(absoluteReturn, power) - 1.0;
-		return BigDecimal.valueOf(annualizedReturn).round(DEFAULT_MATH_CONTEXT);
+		return BigDecimal.valueOf(Math.pow(x.doubleValue(), y.doubleValue())).round(DEFAULT_MATH_CONTEXT);
 	}
 }
