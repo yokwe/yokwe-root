e6e95d906d23d6967a00d13b25c5356410dfaaac
hasegawa <hasegawa@localhost>
2023-05-29 16:24:44 +0900

    Move RinvestedPrice to Finance.

--- yokwe-util/src/main/java/yokwe/util/finance/Finance.java
+++ yokwe-util/src/main/java/yokwe/util/finance/Finance.java
@@ -1,5 +1,9 @@
 package yokwe.util.finance;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Map;
+
 //Morningstar
 //https://web.stanford.edu/~wfsharpe/art/stars/stars2.htm
 
@@ -13,5 +17,65 @@ package yokwe.util.finance;
 //https://en.wikipedia.org/wiki/Modified_Dietz_method
 
 public interface Finance {
+	//
+	// calculate reinvested price
+	//
+	public static class ReinvestedPrice {
+		private BigDecimal previousPrice;
+		private BigDecimal previousReinvestedPrice;
+		
+		public ReinvestedPrice(BigDecimal initialPrice) {
+			previousPrice = previousReinvestedPrice = initialPrice;
+		}
+		
+		public BigDecimal apply(BigDecimal price, BigDecimal div) {
+			// 日次リターンを「（当日の基準価格＋分配金）÷ 前営業日基準価格 -1」として計算
+			//	<計算式>前営業日の分配金再投資基準価格 × (1+日次リターン)
+			BigDecimal reinvestedPrice = previousReinvestedPrice.multiply(price.add(div)).divide(previousPrice, BigDecimalUtil.DEFAULT_MATH_CONTEXT);
+			
+			// update for next iteration
+			previousPrice           = price;
+			previousReinvestedPrice = reinvestedPrice;
+			
+			return reinvestedPrice;
+		}
+	}
+	
+	
+	//
+	// create reinvested array from price and dividend array
+	//
+	public static DailyValue[] toReinvested(DailyValue[] priceArray, DailyValue[] divArray) {
+		DailyValue[] ret = new DailyValue[priceArray.length];
+		
+		Map<LocalDate, BigDecimal> divMap = DailyValue.toMap(divArray);
+		ReinvestedPrice reinvestedPrice = new ReinvestedPrice(priceArray[0].value);
+
+		for(int i = 0; i < priceArray.length; i++) {
+			LocalDate  date  = priceArray[i].date;
+			BigDecimal price = priceArray[i].value;
+			BigDecimal div   = divMap.getOrDefault(date, BigDecimal.ZERO);
+			
+			ret[i] = new DailyValue(date, reinvestedPrice.apply(price, div));
+		}
+		
+		return ret;
+	}
+	public static BigDecimal[] toReinvestedValue(DailyValue[] priceArray, DailyValue[] divArray) {
+		BigDecimal[] ret = new BigDecimal[priceArray.length];
+		
+		Map<LocalDate, BigDecimal> divMap = DailyValue.toMap(divArray);
+		ReinvestedPrice reinvestedPrice = new ReinvestedPrice(priceArray[0].value);
+
+		for(int i = 0; i < priceArray.length; i++) {
+			LocalDate  date  = priceArray[i].date;
+			BigDecimal price = priceArray[i].value;
+			BigDecimal div   = divMap.getOrDefault(date, BigDecimal.ZERO);
+			
+			ret[i] = reinvestedPrice.apply(price, div);
+		}
+		
+		return ret;
+	}
 	
 }
