3be054d5ebe16f43b5e09ff93c6f50b2d7fabefa
hasegawa <hasegawa@localhost>
2023-05-29 16:25:32 +0900

    Add reinvestedPriceList in MonthlyStats

--- yokwe-util/src/main/java/yokwe/util/finance/MonthlyStats.java
+++ yokwe-util/src/main/java/yokwe/util/finance/MonthlyStats.java
@@ -1,26 +1,21 @@
 package yokwe.util.finance;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import yokwe.util.UnexpectedException;
-
 public class MonthlyStats {
-	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
-
-	public static MonthlyStats[] monthlyStatsArray(DailyValue[] array, int limit, MathContext mathContext) {
+	public static MonthlyStats[] monthlyStatsArray(DailyValue[] priceArray, DailyValue[] divArray, int limit) {
 		// array of index that point to stopIndexPlusOne of each month
 		// stopIndexPlusOneArray[0] contains stopIndexPlusOne of newest month
 		int[] stopIndexPlusOneArray;
 		{			
-			List<Integer> list = new ArrayList<>((array.length / 12) + 1);
-			int lastMonthValue = array[0].date.getMonthValue();
-			for(int i = 1; i < array.length; i++) {
-				int monthValue = array[i].date.getMonthValue();
+			List<Integer> list = new ArrayList<>((priceArray.length / 12) + 1);
+			int lastMonthValue = priceArray[0].date.getMonthValue();
+			for(int i = 1; i < priceArray.length; i++) {
+				int monthValue = priceArray[i].date.getMonthValue();
 				if (monthValue == lastMonthValue) continue;
 				
 				// monthValue has changed, add previous index date and index to list
@@ -30,18 +25,20 @@ public class MonthlyStats {
 				lastMonthValue = monthValue;
 			}
 			
-			// reverse order of list
+			// reverse list
 			Collections.reverse(list);
 			// list to array
 			stopIndexPlusOneArray = list.stream().mapToInt(o -> o).toArray();
 		}
+		
+		DailyValue[] reinvestedPriceArray = Finance.toReinvested(priceArray, divArray);
 		
 		List<MonthlyStats> list = new ArrayList<>();
 		for(int i = 0; i < stopIndexPlusOneArray.length - 2; i++) {
 			int stopIndexPlusOne = stopIndexPlusOneArray[i];
 			int startIndex       = stopIndexPlusOneArray[i + 1];
 			
-			list.add(new MonthlyStats(array, startIndex, stopIndexPlusOne, mathContext));
+			list.add(new MonthlyStats(reinvestedPriceArray, priceArray, startIndex, stopIndexPlusOne, divArray));
 			// needs only limit entries
 			if (list.size() == limit) break;
 		}
@@ -49,8 +46,8 @@ public class MonthlyStats {
 		return list.toArray(MonthlyStats[]::new);
 	}
 	
-
-	public final DailyValue[] dailyValueArray;
+	public final DailyValue[] reinvestedPriceArray;
+	public final DailyValue[] priceArray;
 	public final int          startIndex;
 	public final int          stopIndexPlusOne;
 	
@@ -61,25 +58,45 @@ public class MonthlyStats {
 	public final BigDecimal   endValue;
 	public final BigDecimal   returns;  // return ratio from previous period
 	
+	public final BigDecimal   div;
+	
 	public final BigDecimal   mean;
 	public final BigDecimal   sd;
 	
-	public MonthlyStats(DailyValue[] dailyValueArray, int startIndex, int stopIndexPlusOne, MathContext mathContext) {
-		this.dailyValueArray = dailyValueArray;
-		this.startIndex      = startIndex;
-		this.stopIndexPlusOne = stopIndexPlusOne;
+	public MonthlyStats(final DailyValue[] reinvestedPriceArray, final DailyValue[] priceArray, final int startIndex, final int stopIndexPlusOne, final DailyValue[] divArray) {
+		this.reinvestedPriceArray = reinvestedPriceArray;
+		this.priceArray           = priceArray;
+		this.startIndex           = startIndex;
+		this.stopIndexPlusOne     = stopIndexPlusOne;
 		
-		startDate = dailyValueArray[startIndex].date;
-		endDate   = dailyValueArray[stopIndexPlusOne - 1].date;
+		startDate = priceArray[startIndex].date;
+		endDate   = priceArray[stopIndexPlusOne - 1].date;
 		
 		// startValue is price of startDate before trade
-		startValue = dailyValueArray[startIndex - 1].value;
-		endValue   = dailyValueArray[stopIndexPlusOne - 1].value;
+		startValue = priceArray[startIndex - 1].value;
+		endValue   = priceArray[stopIndexPlusOne - 1].value;
 		// percent change of startValue to endValue
-		returns    = endValue.divide(startValue, mathContext).subtract(BigDecimal.ONE);
+		{
+			// To compare return of fund with and without dividend, use reinvestedStartValue
+			BigDecimal reinvestedStartValue = reinvestedPriceArray[startIndex - 1].value;
+			BigDecimal reinvestedEendValue  = reinvestedPriceArray[stopIndexPlusOne - 1].value;
+			returns    = reinvestedEendValue.divide(reinvestedStartValue, BigDecimalUtil.DEFAULT_MATH_CONTEXT).subtract(BigDecimal.ONE);
+		}
 		
-		BigDecimal[] valueArray = DailyValue.toValueArray(dailyValueArray);
-		mean = BigDecimalArrays.mean(valueArray, startIndex, stopIndexPlusOne, mathContext);
-		sd   = BigDecimalArrays.sd(valueArray, startIndex, stopIndexPlusOne, mean, mathContext);
+		{
+			BigDecimal[] divs = DailyValue.filterValue(divArray, startDate, endDate);
+			div = BigDecimalArrays.sum(divs);
+		}
+		
+		{
+			BigDecimal[] valueArray = DailyValue.toValueArray(priceArray, startIndex, stopIndexPlusOne);
+			mean = BigDecimalArrays.mean(valueArray);
+		}
+
+		{
+			BigDecimal[] valueArray = DailyValue.toValueArray(priceArray, startIndex, stopIndexPlusOne);
+			BigDecimal[] simpleRatioArray = BigDecimalArrays.toSimpleRatio(valueArray);
+			sd   = BigDecimalArrays.sd(simpleRatioArray);
+		}
 	}
 }
