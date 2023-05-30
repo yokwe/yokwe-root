7e778b0999ddcf1978be54bb84a16dc75a5f4c1c
hasegawa <hasegawa@localhost>
2023-05-30 11:20:30 +0900

    Refactor source code
    Add isinCode to identify data source.
    Add getInstance() that can return null for not enough monthly data.

--- yokwe-util/src/main/java/yokwe/util/finance/AnnualStats.java
+++ yokwe-util/src/main/java/yokwe/util/finance/AnnualStats.java
@@ -5,9 +5,21 @@ import java.time.LocalDate;
 
 import yokwe.util.UnexpectedException;
 
-public class AnnualStats {
+public final class AnnualStats {
 	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
-
+	
+	public static AnnualStats getInstance(final MonthlyStats[] monthlyStatsArray, final int nYear) {
+		if (monthlyStatsArray == null) {
+			logger.error("array == null");
+			throw new UnexpectedException("array == null");
+		}
+		
+		int nMonth = nYear * 12;
+		return (nMonth <= monthlyStatsArray.length) ? new AnnualStats(monthlyStatsArray, nYear) : null;
+	}
+	
+	
+	public final String       isinCode;
 	public final DailyValue[] reinvestedPriceArray;
 	public final DailyValue[] priceArray;
 	public final int          startIndex;
@@ -18,8 +30,11 @@ public class AnnualStats {
 	public final BigDecimal   startValue;
 	public final BigDecimal   endValue;
 	
-	public final BigDecimal   totalReturn;    // from statValue and endValue
-	public final BigDecimal   annualReturn;
+	public final BigDecimal   absoluteReturn;
+	public final BigDecimal   absoluteReturnWithReinvest;
+	
+	public final BigDecimal   annualizedReturn;
+	public final BigDecimal   annualizedReturnWithReinvest;
 	
 	public final BigDecimal   mean;
 	public final BigDecimal   sd;
@@ -28,19 +43,10 @@ public class AnnualStats {
 	public final BigDecimal   yield;
 	
 	
-	public AnnualStats(final MonthlyStats[] monthlyStatsArray, final int nYear) {
+	private AnnualStats(final MonthlyStats[] monthlyStatsArray, final int nYear) {
 		final int nMonth = nYear * 12;
 		
-		if (monthlyStatsArray == null) {
-			logger.error("array == null");
-			throw new UnexpectedException("array == null");
-		}
-		if (monthlyStatsArray.length < nMonth) {
-			logger.error("array.length < nMonth");
-			logger.error("  array.length {}", monthlyStatsArray.length);
-			logger.error("  nMonth       {}", nMonth);
-			throw new UnexpectedException("array.length < nMonth");
-		}
+		this.isinCode = monthlyStatsArray[0].isinCode;
 				
 		MonthlyStats startMonth = monthlyStatsArray[nMonth - 1];
 		MonthlyStats endMonth   = monthlyStatsArray[0];
@@ -54,13 +60,37 @@ public class AnnualStats {
 		endDate    = endMonth.endDate;
 		startValue = startMonth.startValue;
 		endValue   = endMonth.endValue;
+				
+		mean  = BigDecimalArrays.geometricMean(monthlyStatsArray, 0, nMonth, o -> o.mean);
+		sd    = BigDecimalArrays.sd(monthlyStatsArray, 0, nMonth, o -> o.sd); // FIXME WRONG
+		div   = BigDecimalArrays.sum(monthlyStatsArray, 0, nMonth, o -> o.div);
 		
-		totalReturn  = endValue.divide(startValue, BigDecimalUtil.DEFAULT_MATH_CONTEXT).subtract(BigDecimal.ONE);
-		annualReturn = Finance.annualizeMonthlyReturn(monthlyStatsArray, 0, nMonth, o -> o.totalReturn);
+		yield = div.divide(endValue.multiply(BigDecimal.valueOf(nYear)), BigDecimalUtil.DEFAULT_MATH_CONTEXT); // FIXME annualization
 		
-		mean  = BigDecimalArrays.geometricMean(monthlyStatsArray, o -> o.mean);
-		sd    = BigDecimalArrays.sd(monthlyStatsArray, o -> o.sd);
-		div   = BigDecimalArrays.sum(monthlyStatsArray, o -> o.div);
-		yield = div.divide(endValue.multiply(BigDecimal.valueOf(nYear))); // FIXME annualization		
+		// https://www.nikkei.com/help/contents/markets/fund/
+		// 分配金受取基準価格
+		// 受け取った分配金の合計額を基準価格に足した値です。
+		// 【計算内容】
+		// <計算式>基準価格 + 分配金累計
+		// <例>基準価格が15000、分配金累計が100の場合、15000+100=15100。分配金は税引き前。
+		absoluteReturn = endValue.add(div).divide(startValue, BigDecimalUtil.DEFAULT_MATH_CONTEXT).subtract(BigDecimal.ONE);
+
+		// https://www.nikkei.com/help/contents/markets/fund/
+		// 分配金受取ベースのリターン(年率)
+		// 分配金を足した基準価格がどれだけ上昇または下落したかをパーセントで表示しています。
+		// その投資信託を購入した投資家の保有期間中の損得の実感に近いリターンと言えます。
+		// 対象期間中のリターンを１年間に換算した年率で表示しています。
+		// 【計算内容】
+		// {(計算期末基準価格＋計算期間分配金合計)／計算期初基準価格} ^ (12／n) - 1
+		// n=6,12,36,60,120
+		annualizedReturn = Finance.annualizeReturn(absoluteReturn, nYear);
+	
+		{
+			// To compare return of fund with and without dividend, use reinvestedStartValue
+			BigDecimal reinvestedStartValue = reinvestedPriceArray[startIndex - 1].value;
+			BigDecimal reinvestedEendValue  = reinvestedPriceArray[stopIndexPlusOne - 1].value;
+			absoluteReturnWithReinvest = reinvestedEendValue.divide(reinvestedStartValue, BigDecimalUtil.DEFAULT_MATH_CONTEXT).subtract(BigDecimal.ONE);
+		}
+		annualizedReturnWithReinvest = Finance.annualizeReturn(absoluteReturnWithReinvest, nYear);
 	}
 }
