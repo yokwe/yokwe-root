5ae34f925ff754a31059ab5bcfc98cf8e5780401
hasegawa <hasegawa@localhost>
2023-05-30 11:18:36 +0900

    Refactor source code
    Add isinCode to identify data source.
    Rename totalReturn to absoluteReturn

--- yokwe-util/src/main/java/yokwe/util/finance/MonthlyStats.java
+++ yokwe-util/src/main/java/yokwe/util/finance/MonthlyStats.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class MonthlyStats {
-	public static MonthlyStats[] monthlyStatsArray(DailyValue[] priceArray, DailyValue[] divArray, int limit) {
+	public static MonthlyStats[] monthlyStatsArray(String isinCode, DailyValue[] priceArray, DailyValue[] divArray, int limit) {
 		// array of index that point to stopIndexPlusOne of each month
 		// stopIndexPlusOneArray[0] contains stopIndexPlusOne of newest month
 		int[] stopIndexPlusOneArray;
@@ -38,7 +38,7 @@ public class MonthlyStats {
 			int stopIndexPlusOne = stopIndexPlusOneArray[i];
 			int startIndex       = stopIndexPlusOneArray[i + 1];
 			
-			list.add(new MonthlyStats(reinvestedPriceArray, priceArray, startIndex, stopIndexPlusOne, divArray));
+			list.add(new MonthlyStats(isinCode, reinvestedPriceArray, priceArray, startIndex, stopIndexPlusOne, divArray));
 			// needs only limit entries
 			if (list.size() == limit) break;
 		}
@@ -46,6 +46,7 @@ public class MonthlyStats {
 		return list.toArray(MonthlyStats[]::new);
 	}
 	
+	public final String       isinCode;
 	public final DailyValue[] reinvestedPriceArray;
 	public final DailyValue[] priceArray;
 	public final int          startIndex;
@@ -56,14 +57,17 @@ public class MonthlyStats {
 	
 	public final BigDecimal   startValue;
 	public final BigDecimal   endValue;
-	public final BigDecimal   totalReturn;
+	
+	public final BigDecimal   absoluteReturn;
+	public final BigDecimal   absoluteReturnReinvest;
 	
 	public final BigDecimal   div;
 	
 	public final BigDecimal   mean;
 	public final BigDecimal   sd;
 	
-	public MonthlyStats(final DailyValue[] reinvestedPriceArray, final DailyValue[] priceArray, final int startIndex, final int stopIndexPlusOne, final DailyValue[] divArray) {
+	public MonthlyStats(final String isinCode, final DailyValue[] reinvestedPriceArray, final DailyValue[] priceArray, final int startIndex, final int stopIndexPlusOne, final DailyValue[] divArray) {
+		this.isinCode             = isinCode;
 		this.reinvestedPriceArray = reinvestedPriceArray;
 		this.priceArray           = priceArray;
 		this.startIndex           = startIndex;
@@ -75,14 +79,6 @@ public class MonthlyStats {
 		// startValue is price of startDate before trade
 		startValue = priceArray[startIndex - 1].value;
 		endValue   = priceArray[stopIndexPlusOne - 1].value;
-		// percent change of startValue to endValue
-		{
-			// To compare return of fund with and without dividend, use reinvestedStartValue
-			BigDecimal reinvestedStartValue = reinvestedPriceArray[startIndex - 1].value;
-			BigDecimal reinvestedEendValue  = reinvestedPriceArray[stopIndexPlusOne - 1].value;
-			totalReturn = reinvestedEendValue.divide(reinvestedStartValue, BigDecimalUtil.DEFAULT_MATH_CONTEXT).subtract(BigDecimal.ONE);
-		}
-		
 		{
 			BigDecimal[] divs = DailyValue.filterValue(divArray, startDate, endDate);
 			div = BigDecimalArrays.sum(divs);
@@ -98,5 +94,22 @@ public class MonthlyStats {
 			BigDecimal[] simpleRatioArray = BigDecimalArrays.toSimpleRatio(valueArray);
 			sd   = BigDecimalArrays.sd(simpleRatioArray);
 		}
+		
+		// https://www.nikkei.com/help/contents/markets/fund/
+		// 分配金受取基準価格
+		// 受け取った分配金の合計額を基準価格に足した値です。
+		// 【計算内容】
+		// <計算式>基準価格 + 分配金累計
+		// <例>基準価格が15000、分配金累計が100の場合、15000+100=15100。分配金は税引き前。
+		absoluteReturn = endValue.add(div).divide(startValue, BigDecimalUtil.DEFAULT_MATH_CONTEXT).subtract(BigDecimal.ONE);
+
+		// percent change of startValue to endValue
+		{
+			// To compare return of fund with and without dividend, use reinvestedStartValue
+			BigDecimal reinvestedStartValue = reinvestedPriceArray[startIndex - 1].value;
+			BigDecimal reinvestedEendValue  = reinvestedPriceArray[stopIndexPlusOne - 1].value;
+			absoluteReturnReinvest = reinvestedEendValue.divide(reinvestedStartValue, BigDecimalUtil.DEFAULT_MATH_CONTEXT).subtract(BigDecimal.ONE);
+		}
+		
 	}
 }
