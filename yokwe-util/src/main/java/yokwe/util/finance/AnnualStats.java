6d733eae075d5b2fcf449cd2dd1320785a107cff
hasegawa <hasegawa@localhost>
2023-06-02 16:45:54 +0900

    Refactor MonthylyStats and AnnualStats.

--- yokwe-util/src/main/java/yokwe/util/finance/AnnualStats.java
+++ yokwe-util/src/main/java/yokwe/util/finance/AnnualStats.java
@@ -3,6 +3,8 @@ package yokwe.util.finance;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import yokwe.util.BigDecimalArray;
+import yokwe.util.BigDecimalUtil;
 import yokwe.util.UnexpectedException;
 
 public final class AnnualStats {
@@ -20,121 +22,62 @@ public final class AnnualStats {
 	
 	
 	public final String       isinCode;
-	public final DailyValue[] reinvestedPriceArray;         // 分配金再投資基準価格
-	public final DailyValue[] priceArray;                   // 基準価格
-	public final int          startIndex;
-	public final int          stopIndexPlusOne;
+	public final LocalDate[]  dateArray;
+	public final BigDecimal[] priceArray;                   // 分配金再投資基準価格
+	public final BigDecimal[] dividendArray;                // 分配金
+	public final BigDecimal[] dailyReturnArray;             // 分配金再投資基準価格の日次リターン
 
+	public final int          startIndex;
 	public final LocalDate    startDate;                    // この期間の取引初日
-	public final BigDecimal   startValue;                   // 取引初日開始前 基準価格
-	public final BigDecimal   startValueWithReinvest;       // 取引初日開始前 分配金再投資基準価格
+	public final BigDecimal   startValue;                   // 取引初日開始前 分配金再投資基準価格
 	
+	public final int          stopIndexPlusOne;
 	public final LocalDate    endDate;                      // この期間の取引末日
-	public final BigDecimal   endValue;                     // 取引末日終了後 基準価格
-	public final BigDecimal   endValueWithReinvest;         // 取引末日終了後 分配金再投資基準価格
+	public final BigDecimal   endValue;                     // 取引末日終了後 分配金再投資基準価格
 	
-	public final BigDecimal   div;                          // 分配金累計
+	public final BigDecimal   dividend;                     // 分配金累計
 	public final BigDecimal   absoluteYield;                // 分配金利率
 	public final BigDecimal   annualizedYield;              // 年率換算した分配金利率
 	
-	public final BigDecimal   absoluteReturn;               // 分配金受取ベースのリターン
-	public final BigDecimal   annualizedReturn;             // 分配金受取ベースのリターン 年率換算
+	public final BigDecimal   absoluteReturn;               // 分配金再投資ベースのリターン
+	public final BigDecimal   annualizedReturn;             // 分配金再投資ベースのリターン 年率換算
 	
-	public final BigDecimal   absoluteReturnWithReinvest;   // 分配金再投資ベースのリターン
-	public final BigDecimal   annualizedReturnWithReinvest; // 分配金再投資ベースのリターン 年率換算
-	
-	public final BigDecimal   cumulativeReturn;             // 月統計の分配金再投資ベースのリターンより求めた累積リターン
-	public final BigDecimal   annualizedCumulativeReturn;   // 月統計の分配金再投資ベースのリターンより求めた累積リターン 年率換算
-
-	public final BigDecimal   annualizedStandardDeviationA;
-	public final BigDecimal   annualizedStandardDeviationB;
-	public final BigDecimal   annualizedStandardDeviationC;
+	public final BigDecimal   annualizedStandardDeviation;
 	
 	private AnnualStats(final MonthlyStats[] monthlyStatsArray, final int nYear) {
 		final int nMonth = nYear * 12;
 		
-		this.isinCode = monthlyStatsArray[0].isinCode;
 				
 		MonthlyStats startMonth = monthlyStatsArray[nMonth - 1];
 		MonthlyStats endMonth   = monthlyStatsArray[0];
 		
-		reinvestedPriceArray = startMonth.reinvestedPriceArray;
+		isinCode             = startMonth.isinCode;
+		dateArray            = startMonth.dateArray;
 		priceArray           = startMonth.priceArray;
+		dividendArray        = startMonth.dividendArray;
+		dailyReturnArray     = startMonth.dailyReturnArray;
+		
 		startIndex           = startMonth.startIndex;
+		startDate            = startMonth.startDate;
+		startValue           = startMonth.startValue;
+		
 		stopIndexPlusOne     = endMonth.stopIndexPlusOne;
-		
-		startDate              = startMonth.startDate;
-		startValue             = startMonth.startValue;
-		startValueWithReinvest = startMonth.startValueWithReinvest;
-		
 		endDate              = endMonth.endDate;
 		endValue             = endMonth.endValue;
-		endValueWithReinvest = endMonth.endValueWithReinvest;
 				
-		div             = BigDecimalUtil.sum(monthlyStatsArray, 0, nMonth, o -> o.div);
-		absoluteYield   = div.divide(endValue, BigDecimalUtil.DEFAULT_MATH_CONTEXT);
-		annualizedYield = absoluteYield.divide(BigDecimal.valueOf(nYear), BigDecimalUtil.DEFAULT_MATH_CONTEXT);
+		dividend             = BigDecimalArray.sum(monthlyStatsArray, 0, nMonth, o -> o.dividend);
+		absoluteYield        = dividend.divide(endValue, BigDecimalUtil.DEFAULT_MATH_CONTEXT);
+		annualizedYield      = absoluteYield.divide(BigDecimal.valueOf(nYear), BigDecimalUtil.DEFAULT_MATH_CONTEXT);
 		
-		// https://www.nikkei.com/help/contents/markets/fund/
-		// 分配金受取基準価格
-		// 受け取った分配金の合計額を基準価格に足した値です。
-		// 【計算内容】
-		// <計算式>基準価格 + 分配金累計
-		// <例>基準価格が15000、分配金累計が100の場合、15000+100=15100。分配金は税引き前。
-		absoluteReturn = BigDecimalUtil.toSimpleReturn(startValue, endValue.add(div));
-	
-		// https://www.nikkei.com/help/contents/markets/fund/
-		// 分配金受取ベースのリターン(年率)
-		// 分配金を足した基準価格がどれだけ上昇または下落したかをパーセントで表示しています。
-		// その投資信託を購入した投資家の保有期間中の損得の実感に近いリターンと言えます。
-		// 対象期間中のリターンを１年間に換算した年率で表示しています。
-		// 【計算内容】
-		// {(計算期末基準価格＋計算期間分配金合計)／計算期初基準価格} ^ (12／n) - 1
-		// n=6,12,36,60,120
-		absoluteReturnWithReinvest = BigDecimalUtil.toSimpleReturn(startValueWithReinvest, endValueWithReinvest);
-		
-		// https://www.nikkei.com/help/contents/markets/fund/
-		// リターン(年率)は対象期間中のリターンを１年間に換算した年率で表示しています。
-		// 【計算内容】
-		// ・リターン　(＝累積リターン)
-		// {nΠ1(1+月次リターンn)} - 1 　n=6,12,36,60,120,設定来月数
-		// ・リターン(1年)、リターン(年率)　（＝年率累積リターン）
-		// (1+上記累積リターン)^(12/n) - 1 　n=6,12,36,60,120,設定来月数
-		cumulativeReturn = Finance.cumulativeReturn(monthlyStatsArray, 0, nMonth, o -> o.absoluteReturnWithReinvest);
-		
-		annualizedReturn             = Finance.annualizeReturn(absoluteReturn, nYear);
-		annualizedReturnWithReinvest = Finance.annualizeReturn(absoluteReturnWithReinvest, nYear);
-		annualizedCumulativeReturn   = Finance.annualizeReturn(cumulativeReturn, nYear);
-		
+		absoluteReturn       = BigDecimalUtil.toSimpleReturn(startValue, endValue);
+		annualizedReturn     = Finance.annualizeReturn(absoluteReturn, nYear);
 		
 		// FIXME which method is better?
 		{
-			// calculate standard deviation from reinvestedPriceArray
-			// NOTE using reinvestedPriceArray
-			// NOTE using daily value
-			BigDecimal[] array = BigDecimalUtil.toSimpleReturn(reinvestedPriceArray, startIndex, stopIndexPlusOne, o -> o.value);
-			BigDecimal   mean  = BigDecimalArrays.mean(array);
-			BigDecimal   sd    = BigDecimalArrays.standardDeviation(array, mean);
-			annualizedStandardDeviationA = Finance.annualizeDailyStandardDeviation(sd);
-			logger.info("## AA  {}  {}", nYear, sd.stripTrailingZeros().toPlainString());
-		}
-		{
-			// calculate standard deviation from endValueWithReinvest of monthlyStatsArray
-			// NOTE using reinvestedPriceArray
-			// NOTE using monthly value
-			BigDecimal[] array = BigDecimalUtil.toSimpleReturn(monthlyStatsArray, 0, nMonth, o -> o.endValueWithReinvest);
-			BigDecimal   mean  = BigDecimalArrays.mean(array);
-			BigDecimal   sd    = BigDecimalArrays.standardDeviation(array, mean);
-			annualizedStandardDeviationB = Finance.annualizeMonthlyStandardDeviation(sd);
-			logger.info("## BB  {}  {}", nYear, sd.stripTrailingZeros().toPlainString());
-		}
-		{
-			// calculate standard deviation from variance of monthlyStatsArray
-			// NOTE using reinvestedPriceArray
-			// NOTE using daily value
-			BigDecimal sd      = BigDecimalUtil.mean(monthlyStatsArray, 0, nMonth, o -> o.standardDeviation);
-			annualizedStandardDeviationC = Finance.annualizeDailyStandardDeviation(sd);
-			logger.info("## CC  {}  {}", nYear, sd.stripTrailingZeros().toPlainString());
+			// calculate standard deviation from dailyReturnArray
+			BigDecimal   mean  = BigDecimalArray.mean(dailyReturnArray, startIndex, stopIndexPlusOne, o -> o);
+			BigDecimal   sd    = BigDecimalArray.standardDeviation(dailyReturnArray, startIndex, stopIndexPlusOne, mean, o -> o);
+			annualizedStandardDeviation = Finance.annualizeDailyStandardDeviation(sd);
 		}
 
 	}
--- yokwe-util/src/main/java/yokwe/util/finance/MonthlyStats.java
+++ yokwe-util/src/main/java/yokwe/util/finance/MonthlyStats.java
@@ -3,19 +3,50 @@ package yokwe.util.finance;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class MonthlyStats {
-	public static MonthlyStats[] monthlyStatsArray(String isinCode, DailyValue[] priceArray, DailyValue[] divArray, int limit) {
+import yokwe.util.BigDecimalArray;
+import yokwe.util.UnexpectedException;
+import yokwe.util.finance.Finance.ReinvestedPrice;
+
+public final class MonthlyStats {
+	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
+	
+	public static MonthlyStats[] monthlyStatsArray(String isinCode, DailyValue[] rawPriceArray, DailyValue[] divArray, int nMonth) {
+		LocalDate[]  dateArray     = DailyValue.toDateArray(rawPriceArray);
+		BigDecimal[] dividendArray = new BigDecimal[rawPriceArray.length];
+		{
+			Map<LocalDate, BigDecimal> map = Arrays.stream(divArray).collect(Collectors.toMap(o -> o.date, o -> o.value));
+			for(int i = 0; i < rawPriceArray.length; i++) {
+				LocalDate date = rawPriceArray[i].date;
+				dividendArray[i] = map.getOrDefault(date, BigDecimal.ZERO);
+			}
+		}
+		BigDecimal[] priceArray = new BigDecimal[rawPriceArray.length];
+		{
+			ReinvestedPrice reinvestedPrice = new ReinvestedPrice(rawPriceArray[0].value);
+			for(int i = 0; i < rawPriceArray.length; i++) {
+				var price = rawPriceArray[i].value;
+				var div   = dividendArray[i];
+				
+				priceArray[i] = reinvestedPrice.apply(price, div);;
+			}
+		}
+		BigDecimal[] dailyReturnArray = BigDecimalArray.toSimpleReturn(priceArray, o -> o);
+		
+		
 		// array of index that point to stopIndexPlusOne of each month
 		// stopIndexPlusOneArray[0] contains stopIndexPlusOne of newest month
 		int[] stopIndexPlusOneArray;
 		{			
 			List<Integer> list = new ArrayList<>((priceArray.length / 12) + 1);
-			int lastMonthValue = priceArray[0].date.getMonthValue();
-			for(int i = 1; i < priceArray.length; i++) {
-				int monthValue = priceArray[i].date.getMonthValue();
+			int lastMonthValue = dateArray[0].getMonthValue();
+			for(int i = 1; i < dateArray.length; i++) {
+				int monthValue = dateArray[i].getMonthValue();
 				if (monthValue == lastMonthValue) continue;
 				
 				// monthValue has changed, add previous index date and index to list
@@ -30,75 +61,63 @@ public class MonthlyStats {
 			// list to array
 			stopIndexPlusOneArray = list.stream().mapToInt(o -> o).toArray();
 		}
-		
-		DailyValue[] reinvestedPriceArray = Finance.toReinvestedPrice(priceArray, divArray);
-		
+
 		List<MonthlyStats> list = new ArrayList<>();
 		for(int i = 0; i < stopIndexPlusOneArray.length - 2; i++) {
 			int stopIndexPlusOne = stopIndexPlusOneArray[i];
 			int startIndex       = stopIndexPlusOneArray[i + 1];
 			
-			list.add(new MonthlyStats(isinCode, reinvestedPriceArray, priceArray, startIndex, stopIndexPlusOne, divArray));
-			// needs only limit entries
-			if (list.size() == limit) break;
+			list.add(new MonthlyStats(isinCode, startIndex, stopIndexPlusOne, dateArray, priceArray, dailyReturnArray, dividendArray));
+			if (list.size() == nMonth) break;
 		}
-		
+
 		return list.toArray(MonthlyStats[]::new);
 	}
 	
+
 	public final String       isinCode;
-	public final DailyValue[] reinvestedPriceArray;         // 分配金再投資基準価格
-	public final DailyValue[] priceArray;                   // 基準価格
-	public final int          startIndex;
-	public final int          stopIndexPlusOne;
+	public final int          startIndex;                   // この期間の取引初日への配列インデックス
+	public final int          stopIndexPlusOne;             // 次の期間の取引初日への配列インデックス
+	public final LocalDate[]  dateArray;
+	public final BigDecimal[] priceArray;                   // 分配金再投資基準価格
+	public final BigDecimal[] dividendArray;                // 分配金
+	public final BigDecimal[] dailyReturnArray;             // 分配金再投資基準価格の日次リターン
 	
+	// derived value		
 	public final LocalDate    startDate;                    // この期間の取引初日
-	public final BigDecimal   startValue;                   // 取引初日開始前 基準価格
-	public final BigDecimal   startValueWithReinvest;       // 取引初日開始前 分配金再投資基準価格
-	
 	public final LocalDate    endDate;                      // この期間の取引末日
-	public final BigDecimal   endValue;                     // 取引末日終了後 基準価格
-	public final BigDecimal   endValueWithReinvest;         // 取引末日終了後 分配金再投資基準価格
-
-	public final BigDecimal   div;                          // 分配金累計
-
-	public final BigDecimal   absoluteReturn;               // 分配金受取ベースのリターン
-	public final BigDecimal   absoluteReturnWithReinvest;   // 分配金再投資ベースのリターン
 	
-	public final BigDecimal   standardDeviation;
-	
-	
-	public MonthlyStats(final String isinCode, final DailyValue[] reinvestedPriceArray, final DailyValue[] priceArray, final int startIndex, final int stopIndexPlusOne, final DailyValue[] divArray) {
-		this.isinCode             = isinCode;
-		this.reinvestedPriceArray = reinvestedPriceArray;
-		this.priceArray           = priceArray;
-		this.startIndex           = startIndex;
-		this.stopIndexPlusOne     = stopIndexPlusOne;
-		
-		startDate              = priceArray[startIndex].date;
-		// startValue is a price of startDate before trade
-		startValue             = priceArray[startIndex - 1].value;
-		startValueWithReinvest = reinvestedPriceArray[startIndex - 1].value;
-		
-		endDate              = priceArray[stopIndexPlusOne - 1].date;
-		// endValue is a price of endValue after trade
-		endValue             = priceArray[stopIndexPlusOne - 1].value;
-		endValueWithReinvest = reinvestedPriceArray[stopIndexPlusOne - 1].value;
+	public final BigDecimal   startValue;                   // 取引初日開始前 分配金再投資基準価格
+	public final BigDecimal   endValue;                     // 取引末日終了後 分配金再投資基準価格
 
-		{
-			BigDecimal[] array = DailyValue.filterValue(divArray, startDate, endDate);
-			div = BigDecimalArrays.sum(array);
+	public final BigDecimal   dividend;                     // この期間の分配金累計
+
+	private MonthlyStats(String isinCode_, int startIndex_, int stopIndexPlusOne_, 
+			LocalDate[] dateArray_, BigDecimal[] priceArray_, BigDecimal[] dailyReturnArray_, BigDecimal[] dividendArray_) {
+		if (priceArray_.length != dailyReturnArray_.length || priceArray_.length != dividendArray_.length) {
+			logger.error("size of priceArray, dailytRetur and divArray not equal");
+			logger.error("  isinCode_         {}", isinCode_);
+			logger.error("  priceArray_       {}", priceArray_.length);
+			logger.error("  dailyReturnArray_ {}", dailyReturnArray_.length);
+			logger.error("  divArray_         {}", dividendArray_.length);
+			throw new UnexpectedException("size of priceArray, dailytRetur and divArray not equal");
 		}
 		
-		absoluteReturn             = BigDecimalUtil.toSimpleReturn(startValue, endValue.add(div));
-		absoluteReturnWithReinvest = BigDecimalUtil.toSimpleReturn(startValueWithReinvest, endValueWithReinvest);
+		isinCode         = isinCode_;
+		startIndex       = startIndex_;
+		stopIndexPlusOne = stopIndexPlusOne_;
 		
-		{
-			// NOTE using reinvestedPriceArray
-			// NOTE using daily value
-			BigDecimal[] array = BigDecimalUtil.toSimpleReturn(reinvestedPriceArray, startIndex, stopIndexPlusOne, o -> o.value);
-			BigDecimal   mean  = BigDecimalArrays.mean(array);
-			standardDeviation = BigDecimalArrays.standardDeviation(array, mean);
-		}
+		dateArray        = dateArray_;
+		priceArray       = priceArray_;
+		dailyReturnArray = dailyReturnArray_;
+		dividendArray    = dividendArray_;
+		
+		startDate  = dateArray[startIndex];
+		endDate    = dateArray[stopIndexPlusOne - 1];
+		
+		startValue = priceArray[startIndex - 1];
+		endValue   = priceArray[stopIndexPlusOne - 1];
+		
+		dividend   = BigDecimalArray.sum(dividendArray, startIndex, stopIndexPlusOne, o -> o);
 	}
 }
