8f67d5760ca475c350a427c5c95275cb00817987
hasegawa <hasegawa@localhost>
2023-06-24 17:42:52 +0900

    Add subclass for Nikko

--- yokwe-stock-jp/src/main/java/yokwe/stock/jp/Storage.java
+++ yokwe-stock-jp/src/main/java/yokwe/stock/jp/Storage.java
@@ -193,5 +193,20 @@ public final class Storage {
 			return getPath(String.format("%s/%s", prefix, path));
 		}
 	}
-
+	
+	// Nikko
+	public static final class Nikko {
+		public static final String PREFIX = "nikko";
+		
+		public static String getPath() {
+			return Storage.getPath(PREFIX);
+		}
+		public static String getPath(String path) {
+			return Storage.getPath(PREFIX, path);
+		}
+		public static String getPath(String prefix, String path) {
+			return getPath(String.format("%s/%s", prefix, path));
+		}
+	}
+	
 }
