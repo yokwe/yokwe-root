ac08b05f09d11b1050bddbcec8519120ed98cb1e
hasegawa <hasegawa@localhost>
2023-07-17 23:29:59 +0900

    Unify variable naming convention.

--- yokwe-util/src/main/java/yokwe/util/finance/Variance.java
+++ yokwe-util/src/main/java/yokwe/util/finance/Variance.java
@@ -5,30 +5,30 @@ import yokwe.util.finance.DoubleArray.DoubleReducer;
 
 class Variance implements DoubleReducer {
 	private int    count  = 0;
-	private double sum    = 0;
-	private double sum2   = 0;
+	private double sumA   = 0;
+	private double sumAA  = 0;
 	
 	@Override
-	public void accept(double value) {
+	public void accept(double a) {
 		// sanity check
-		if (Double.isInfinite(value)) {
-			DoubleArray.logger.error("value is infinite");
-			DoubleArray.logger.error("  value {}", Double.toString(value));
-			throw new UnexpectedException("value is infinite");
+		if (Double.isInfinite(a)) {
+			DoubleArray.logger.error("a is infinite");
+			DoubleArray.logger.error("  a {}", Double.toString(a));
+			throw new UnexpectedException("a is infinite");
 		}
 		
 		count++;
 		
-		sum  += value;
-		sum2 += value * value;
+		sumA  += a;
+		sumAA += a * a;
 	}
 
 	@Override
 	public double get() {
 		if (count == 0) return Double.NaN;
-		double ex  = sum  / count;
-		double ex2 = sum2 / count;
-		// variance = E(X ^ 2) - E(X) ^ 2
-		return ex2 - (ex * ex);
+		double eA  = sumA  / count;
+		double eAA = sumAA / count;
+		// variance = E(a * a) - E(a) * E(a)
+		return eAA - eA * eA;
 	}
 }
