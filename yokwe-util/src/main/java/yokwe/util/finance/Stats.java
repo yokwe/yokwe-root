462efe69a1841dc03630fc40730c854ae15a2f08
hasegawa <hasegawa@localhost>
2023-07-18 10:49:49 +0900

    Don't copy data in Stats class.

--- yokwe-util/src/main/java/yokwe/util/finance/Stats.java
+++ yokwe-util/src/main/java/yokwe/util/finance/Stats.java
@@ -2,26 +2,10 @@ package yokwe.util.finance;
 
 import yokwe.util.UnexpectedException;
 
-public class Stats {
+public final class Stats {
 	static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
 
-	private static double covariance(Stats a, Stats b) {		
-		final int length = a.length;
-		
-		double sumAB = 0;
-		for(int i = 0; i < length; i++) {
-			sumAB += a.data[i] * b.data[i];
-		}
-		
-		double eAB  = sumAB / length;
-		double eA   = a.mean();
-		double eB   = b.mean();
-		// covariance = E(a * b) - E(a) * E(b)
-		return eAB - (eA * eB);
-	}
-	
-	public static double correlation(Stats a, Stats b) {
-		// sanity check
+	private static void checkStats(Stats a, Stats b) {
 		if (a == null) {
 			logger.error("a is null");
 			throw new UnexpectedException("a is null");
@@ -30,16 +14,48 @@ public class Stats {
 			logger.error("b is null");
 			throw new UnexpectedException("b is null");
 		}
+		if (a.startIndex != b.startIndex) {
+			logger.error("startIndex of a and b is different");
+			logger.error("  a  {}", a.startIndex);
+			logger.error("  b  {}", b.startIndex);
+			throw new UnexpectedException("startIndex of a and b is different");
+		}
+		if (a.stopIndexPlusOne != b.stopIndexPlusOne) {
+			logger.error("stopIndexPlusOne of a and b is different");
+			logger.error("  a  {}", a.stopIndexPlusOne);
+			logger.error("  b  {}", b.stopIndexPlusOne);
+			throw new UnexpectedException("stopIndexPlusOne of a and b is different");
+		}
 		if (a.length != b.length) {
 			logger.error("length of a and b is different");
 			logger.error("  a  {}", a.length);
 			logger.error("  b  {}", b.length);
 			throw new UnexpectedException("length of a and b is different");
 		}
+	}
+	
+	public static double covariance(Stats a, Stats b) {
+		// sanity check
+		checkStats(a, b);
+		
+		double sumAB = 0;
+		for(int i = a.startIndex; i < a.stopIndexPlusOne; i++) {
+			sumAB += a.data[i] * b.data[i];
+		}
+		
+		double eAB  = sumAB / a.length;
+		double eA   = a.mean();
+		double eB   = b.mean();
+		// covariance = E(a * b) - E(a) * E(b)
+		return eAB - (eA * eB);
+	}
+	public static double correlation(Stats a, Stats b) {
+		// sanity check
+		checkStats(a, b);
 
 		double covAB = covariance(a, b);
-		double sdA   = a.statndardDeviation();
-		double sdB   = b.statndardDeviation();
+		double sdA   = a.standardDeviation();
+		double sdB   = b.standardDeviation();
 		// correlation = covariance(a, b) / statndardDeviation(a) * statndardDeviation(b)
 		return covAB / (sdA * sdB);
 	}
@@ -47,6 +63,8 @@ public class Stats {
 	
 	
 	private final double[] data;
+	private final int      startIndex;
+	private final int      stopIndexPlusOne;
 	private final int      length;
 	
 	private double sum   = Double.NaN;
@@ -56,21 +74,22 @@ public class Stats {
 	private double var   = Double.NaN;
 	private double sd    = Double.NaN;
 	
-	public Stats(double[] data_, int startIndex, int stopIndexPlusOne) {
-		DoubleArray.checkIndex(data_, startIndex, stopIndexPlusOne);
+	public Stats(double[] data, int startIndex, int stopIndexPlusOne) {
+		DoubleArray.checkIndex(data, startIndex, stopIndexPlusOne);
 		
-		length = stopIndexPlusOne - startIndex;
-		data = new double[length];
-		for(int i = 0, j = startIndex; i < length; i++, j++) {
-			double value = data_[j];
+		this.data             = data;
+		this.startIndex       = startIndex;
+		this.stopIndexPlusOne = stopIndexPlusOne;
+		this.length           = stopIndexPlusOne - startIndex;
+		
+		// sanity check
+		for(int i = startIndex; i < stopIndexPlusOne; i++) {
 			// sanity check
-			if (Double.isInfinite(value)) {
+			if (Double.isInfinite(data[i])) {
 				logger.error("value is infinite");
-				logger.error("  data[{}} = {}", j, Double.toString(value));
+				logger.error("  data[{}} = {}", i, Double.toString(data[i]));
 				throw new UnexpectedException("value is infinite");
 			}
-
-			data[i] = data_[j];
 		}
 	}
 	public Stats(double[] data) {
@@ -80,8 +99,8 @@ public class Stats {
 	public double sum() {
 		if (Double.isNaN(sum)) {
 			sum = 0;
-			for(var e: data) {
-				sum += e;
+			for(int i = startIndex; i < stopIndexPlusOne; i++) {
+				sum += data[i];
 			}
 			// sanity check
 			if (Double.isInfinite(sum)) {
@@ -94,8 +113,9 @@ public class Stats {
 	public double sum2() {
 		if (Double.isNaN(sum2)) {
 			sum2 = 0;
-			for(var e: data) {
-				sum2 += e * e;
+			for(int i = startIndex; i < stopIndexPlusOne; i++) {
+				double t = data[i];
+				sum2 += t * t;
 			}
 			// sanity check
 			if (Double.isInfinite(sum2)) {
@@ -107,9 +127,7 @@ public class Stats {
 	}
 	public double mean() {
 		if (Double.isNaN(mean)) {
-			sum();
-			//
-			mean = sum / length;
+			mean = sum() / length;
 			// sanity check
 			if (Double.isInfinite(mean)) {
 				logger.error("mean is infinite");
@@ -120,9 +138,7 @@ public class Stats {
 	}
 	public double mean2() {
 		if (Double.isNaN(mean2)) {
-			sum2();
-			//
-			mean2 = sum2 / length;
+			mean2 = sum2() / length;
 			// sanity check
 			if (Double.isInfinite(mean2)) {
 				logger.error("mean2 is infinite");
@@ -133,11 +149,10 @@ public class Stats {
 	}
 	public double variance() {
 		if (Double.isNaN(var)) {
-			mean();
-			//
+			double mean_ = mean();
 			var = 0;
-			for(var e: data) {
-				double t = e - mean;
+			for(int i = startIndex; i < stopIndexPlusOne; i++) {
+				double t = data[i] - mean_;
 				var += t * t;
 			}
 			var = var / length;
@@ -149,11 +164,9 @@ public class Stats {
 		}
 		return var;
 	}
-	public double statndardDeviation() {
+	public double standardDeviation() {
 		if (Double.isNaN(sd)) {
-			variance();
-			//
-			sd = Math.sqrt(var);
+			sd = Math.sqrt(variance());
 			// sanity check
 			if (Double.isInfinite(sd)) {
 				logger.error("sd is infinite");
