b6ea3fbe2899b070506fa4989fed2952c565fc71
hasegawa <hasegawa@localhost>
2023-07-24 20:38:30 +0900

    Reorganize checkIndex methods.

--- yokwe-util/src/main/java/yokwe/util/finance/Util.java
+++ yokwe-util/src/main/java/yokwe/util/finance/Util.java
@@ -12,66 +12,92 @@ public final class Util {
 	///////////////////////////////////////////////////////////////////////////
 	// check index consistency with array
 	///////////////////////////////////////////////////////////////////////////
+	//
+	// one array
+	//
+	private static void checkindex(int length, int startIndex, int stopIndexPlusOne) {
+		if (length == 0 && startIndex == 0 && stopIndexPlusOne == 0) return;
+		
+		if (!(0 <= startIndex && startIndex < length)) {
+			logger.error("  alength           {}", length);
+			logger.error("  startIndex        {}", startIndex);
+			logger.error("  stopIndexPlusOne  {}", stopIndexPlusOne);
+			throw new UnexpectedException("offset is out of range");
+		}
+		if (!(startIndex < stopIndexPlusOne && stopIndexPlusOne <= length)) {
+			logger.error("  length            {}", length);
+			logger.error("  startIndex        {}", startIndex);
+			logger.error("  stopIndexPlusOne  {}", stopIndexPlusOne);
+			throw new UnexpectedException("offset is out of range");
+		}
+	}
+	// object array
 	public static <T> void checkIndex(T[] array, int startIndex, int stopIndexPlusOne) {
 		if (array == null) {
 			logger.error("array == null");
 			throw new UnexpectedException("array == null");
 		}
-		if (array.length == 0 && startIndex == 0 && stopIndexPlusOne == 0) return;
-		
-		if (!(0 <= startIndex && startIndex < array.length)) {
-			logger.error("  array.length      {}", array.length);
-			logger.error("  startIndex        {}", startIndex);
-			logger.error("  stopIndexPlusOne  {}", stopIndexPlusOne);
-			throw new UnexpectedException("offset is out of range");
-		}
-		if (!(startIndex < stopIndexPlusOne && stopIndexPlusOne <= array.length)) {
-			logger.error("  array.length      {}", array.length);
-			logger.error("  startIndex        {}", startIndex);
-			logger.error("  stopIndexPlusOne  {}", stopIndexPlusOne);
-			throw new UnexpectedException("offset is out of range");
-		}
+		checkindex(array.length, startIndex, stopIndexPlusOne);
 	}
-
+	// double array
 	public static void checkIndex(double[] array, int startIndex, int stopIndexPlusOne) {
 		if (array == null) {
 			logger.error("array == null");
 			throw new UnexpectedException("array == null");
 		}
-		if (array.length == 0 && startIndex == 0 && stopIndexPlusOne == 0) return;
-		
-		if (!(0 <= startIndex && startIndex < array.length)) {
-			logger.error("  array.length      {}", array.length);
-			logger.error("  startIndex        {}", startIndex);
-			logger.error("  stopIndexPlusOne  {}", stopIndexPlusOne);
-			throw new UnexpectedException("offset is out of range");
+		checkindex(array.length, startIndex, stopIndexPlusOne);
+	}
+	//
+	// two array
+	//
+	// object array  object array
+	public static <T, U> void checkIndex(T[] a, U[] b) {
+		// length of array a and b must be same
+		if (a == null) {
+			throw new UnexpectedException("array a is null");
 		}
-		if (!(startIndex < stopIndexPlusOne && stopIndexPlusOne <= array.length)) {
-			logger.error("  array.length      {}", array.length);
-			logger.error("  startIndex        {}", startIndex);
-			logger.error("  stopIndexPlusOne  {}", stopIndexPlusOne);
-			throw new UnexpectedException("offset is out of range");
+		if (b == null) {
+			throw new UnexpectedException("array b is null");
+		}
+		if (a.length != b.length) {
+			logger.error("  a.length          {}", a.length);
+			logger.error("  b.length          {}", b.length);
+			throw new UnexpectedException("array length is different");
+		}
+	}
+	
+	public static <T, U> void checkIndex(T[] a, U[] b, int startIndex, int stopIndexPlusOne) {
+		checkIndex(a, b);
+		checkIndex(a, startIndex, stopIndexPlusOne);
+		checkIndex(b, startIndex, stopIndexPlusOne);
+	}
+	// double array  double array
+	public static void checkIndex(double[] a, double[] b) {
+		// length of array a and b must be same
+		if (a == null) {
+			throw new UnexpectedException("array a is null");
+		}
+		if (b == null) {
+			throw new UnexpectedException("array b is null");
+		}
+		if (a.length != b.length) {
+			logger.error("  a.length          {}", a.length);
+			logger.error("  b.length          {}", b.length);
+			throw new UnexpectedException("array length is different");
 		}
 	}
 	public static void checkIndex(double[] a, double[] b, int startIndex, int stopIndexPlusOne) {
-		// length of array a and b must be same
-		if (a != null && b != null && a.length != b.length) {
-			logger.error("  a.length          {}", a.length);
-			logger.error("  b.length          {}", b.length);
-			logger.error("  startIndex        {}", startIndex);
-			logger.error("  stopIndexPlusOne  {}", stopIndexPlusOne);
-			throw new UnexpectedException("array length is different");
-		}
-		
+		checkIndex(a, b);
 		checkIndex(a, startIndex, stopIndexPlusOne);
 		checkIndex(b, startIndex, stopIndexPlusOne);
 	}
 
 	
+	
 	///////////////////////////////////////////////////////////////////////////
 	// IndexRange and indexRange
 	///////////////////////////////////////////////////////////////////////////
-	public static final class IndexRange {
+ 	public static final class IndexRange {
 		public final int startIndex;
 		public final int stopIndexPlusOne;
 		
