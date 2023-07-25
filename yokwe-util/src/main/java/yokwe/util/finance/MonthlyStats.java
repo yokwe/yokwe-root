e973dbb52415c15400a9800701b274078a2743de
hasegawa <hasegawa@localhost>
2023-07-25 14:17:48 +0900

    Minor modification.

--- yokwe-util/src/main/java/yokwe/util/finance/MonthlyStats.java
+++ yokwe-util/src/main/java/yokwe/util/finance/MonthlyStats.java
@@ -2,38 +2,42 @@ package yokwe.util.finance;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import yokwe.util.UnexpectedException;
-import yokwe.util.finance.online.ReinvestedValue;
-import yokwe.util.finance.online.SimpleReturn;
 
 public final class MonthlyStats {
 	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
 	
-	public static MonthlyStats[] monthlyStatsArray(String isinCode, DailyValue[] rawPriceArray, DailyValue[] divArray, int nMonth) {
-		LocalDate[]  dateArray     = DailyValue.toDateArray(rawPriceArray);
-		double[]     dividendArray = new double[rawPriceArray.length];
-		{
-			Map<LocalDate, Double> map = Arrays.stream(divArray).collect(Collectors.toMap(o -> o.date, o -> o.value));
-			Double zero = Double.valueOf(0);
-			for(int i = 0; i < rawPriceArray.length; i++) {
-				LocalDate date = rawPriceArray[i].date;
-				dividendArray[i] = map.getOrDefault(date, zero);
-			}
-		}
-		double[] priceArray;
-		{
-			double[] valueArray = DailyValue.toValueArray(rawPriceArray);
-			priceArray = DoubleArray.toDoubleArray(valueArray, dividendArray, new ReinvestedValue());
-		}
+	public static MonthlyStats[] monthlyStatsArray(String isinCode, DailyPriceDiv[] array, int nMonth) {
+		LocalDate[]  dateArray  = DailyPriceDiv.toDateArray(array);
+		double[]     priceArray = DailyPriceDiv.toPriceArray(array);
+		double[]     divArray   = DailyPriceDiv.toDivArray(array);
 		
-		double[] dailyReturnArray = DoubleArray.toDoubleArray(priceArray, new SimpleReturn());
-		
+		return monthlyStatsArray(isinCode, dateArray, priceArray, divArray, nMonth);
+	}
+	
+	public static MonthlyStats[] monthlyStatsArray(String isinCode, LocalDate[] dateArray, double[] priceArray, double[] divArray, int nMonth) {
+		// sanity check
+		if (dateArray == null) {
+			throw new UnexpectedException("dateArray is null");
+		}
+		if (priceArray == null) {
+			throw new UnexpectedException("priceArray is null");
+		}
+		if (divArray == null) {
+			throw new UnexpectedException("divArray is null");
+		}
+		if (dateArray.length != priceArray.length || dateArray.length != divArray.length) {
+			logger.error("Unexpected array length");
+			logger.error("  dateArray   {}", dateArray.length);
+			logger.error("  priceArray  {}", priceArray.length);
+			logger.error("  divArray    {}", divArray.length);
+			throw new UnexpectedException("Unexpected array length");
+		}
+			
+			
 		// array of index that point to stopIndexPlusOne of each month
 		// stopIndexPlusOneArray[0] contains stopIndexPlusOne of newest month
 		int[] stopIndexPlusOneArray;
@@ -44,14 +48,14 @@ public final class MonthlyStats {
 				int monthValue = dateArray[i].getMonthValue();
 				if (monthValue == lastMonthValue) continue;
 				
-				// monthValue has changed, add previous index date and index to list
+				// monthValue has changed, add i as stopIndexPlusOne to list
 				int stopIndexPlusOne = i;
 				list.add(stopIndexPlusOne);
 				// update lastMonthValue for next iteration
 				lastMonthValue = monthValue;
 			}
 			
-			// reverse list
+			// reverse list. So first entry of list point to latest month
 			Collections.reverse(list);
 			// list to array
 			stopIndexPlusOneArray = list.stream().mapToInt(o -> o).toArray();
@@ -62,7 +66,7 @@ public final class MonthlyStats {
 			int stopIndexPlusOne = stopIndexPlusOneArray[i];
 			int startIndex       = stopIndexPlusOneArray[i + 1];
 			
-			list.add(new MonthlyStats(isinCode, startIndex, stopIndexPlusOne, dateArray, priceArray, dailyReturnArray, dividendArray));
+			list.add(new MonthlyStats(isinCode, startIndex, stopIndexPlusOne, dateArray, priceArray, divArray));
 			if (list.size() == nMonth) break;
 		}
 
@@ -71,48 +75,29 @@ public final class MonthlyStats {
 	
 
 	public final String      isinCode;
-	public final int         startIndex;                   // この期間の取引初日への配列インデックス
-	public final int         stopIndexPlusOne;             // 次の期間の取引初日への配列インデックス
+	public final int         startIndex;         // この期間の取引初日への配列インデックス
+	public final int         stopIndexPlusOne;   // 次の期間の取引初日への配列インデックス
 	public final LocalDate[] dateArray;
-	public final double[]    priceArray;                   // 分配金再投資基準価格
-	public final double[]    dividendArray;                // 分配金
-	public final double[]    dailyReturnArray;             // 分配金再投資基準価格の日次リターン
-	
+	public final double[]    priceArray;         // 基準価格
+	public final double[]    divArray;           // 分配金
+		
 	// derived value		
-	public final LocalDate   startDate;                    // この期間の取引初日
-	public final LocalDate   endDate;                      // この期間の取引末日
+	public final LocalDate   startDate;          // この期間の取引初日
+	public final LocalDate   endDate;            // この期間の取引末日	
+
 	
-	public final double      startValue;                   // 取引初日開始前 分配金再投資基準価格
-	public final double      endValue;                     // 取引末日終了後 分配金再投資基準価格
-
-	public final double      dividend;                     // この期間の分配金累計
-
 	private MonthlyStats(String isinCode_, int startIndex_, int stopIndexPlusOne_, 
-			LocalDate[] dateArray_, double[] priceArray_, double[] dailyReturnArray_, double[] dividendArray_) {
-		if (priceArray_.length != dailyReturnArray_.length || priceArray_.length != dividendArray_.length) {
-			logger.error("size of priceArray, dailytRetur and divArray not equal");
-			logger.error("  isinCode_         {}", isinCode_);
-			logger.error("  priceArray_       {}", priceArray_.length);
-			logger.error("  dailyReturnArray_ {}", dailyReturnArray_.length);
-			logger.error("  divArray_         {}", dividendArray_.length);
-			throw new UnexpectedException("size of priceArray, dailytRetur and divArray not equal");
-		}
+		LocalDate[] dateArray_, double[] priceArray_, double[] divArray_) {
 		
 		isinCode         = isinCode_;
 		startIndex       = startIndex_;
 		stopIndexPlusOne = stopIndexPlusOne_;
 		
-		dateArray        = dateArray_;
-		priceArray       = priceArray_;
-		dailyReturnArray = dailyReturnArray_;
-		dividendArray    = dividendArray_;
+		dateArray          = dateArray_;
+		priceArray         = priceArray_;
+		divArray           = divArray_;
 		
 		startDate  = dateArray[startIndex];
 		endDate    = dateArray[stopIndexPlusOne - 1];
-		
-		startValue = priceArray[startIndex - 1];
-		endValue   = priceArray[stopIndexPlusOne - 1];
-		
-		dividend   = Stats.sum(dividendArray, startIndex, stopIndexPlusOne);
 	}
 }
