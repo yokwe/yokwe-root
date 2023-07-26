a345c3ffa5465d62f0de270b40e03556c45410b7
hasegawa <hasegawa@localhost>
2023-07-26 14:20:50 +0900

    Minor modification.

--- yokwe-util/src/main/java/yokwe/util/finance/AnnualStats.java
+++ yokwe-util/src/main/java/yokwe/util/finance/AnnualStats.java
@@ -24,34 +24,34 @@ public final class AnnualStats {
 	
 	public final String      isinCode;
 	public final LocalDate[] dateArray;
-	public final double[]    priceArray;         // 基準価格
-	public final double[]    divArray;           // 分配金
+	public final double[]    priceArray;          // 基準価格
+	public final double[]    divArray;            // 分配金
 	
 	// derived values
-	public final double[]    retPrice;           // この期間の基準価格
-	public final double[]    retReinvestment;    // この期間の分配金再投資基準価格
-	public final double[]    retNoReinvestment;  // この期間の分配金受取基準価格
+	public final double[]    retPrice;            // この期間の基準価格
+	public final double[]    retReinvestment;     // この期間の分配金再投資基準価格
+	public final double[]    retNoReinvestment;   // この期間の分配金受取基準価格
 
-	public final int         startIndex;         // この期間の取引初日への配列インデックス
-	public final LocalDate   startDate;          // この期間の取引初日
-	public final double      startPrice;         // この期間の取引初日の基準価格
-	public final double      startReinvested;    // この期間の取引初日の分配金再投資基準価格
-	public final double      startNoReinvested;  // この期間の取引初日の分配金受取基準価格
+	public final int         startIndex;          // この期間の取引初日への配列インデックス
+	public final LocalDate   startDate;           // この期間の取引初日
+	public final double      startPrice;          // この期間の取引初日の基準価格
+	public final double      startReinvestment;   // この期間の取引初日の分配金再投資基準価格
+	public final double      startNoReinvestment; // この期間の取引初日の分配金受取基準価格
 
 	public final int         stopIndexPlusOne;   // 次の期間の取引初日への配列インデックス
 	public final LocalDate   endDate;            // この期間の取引末日
 	public final double      endPrice;           // この期間の取引初日の基準価格
-	public final double      endReinvested;      // この期間の取引初日の分配金再投資基準価格
-	public final double      endNoReinvested;    // この期間の取引初日の分配金受取基準価格
+	public final double      endReinvestment;    // この期間の取引初日の分配金再投資基準価格
+	public final double      endNoReinvestment;  // この期間の取引初日の分配金受取基準価格
 
-	public final double      dividend;          // 分配金累計
-	public final double      yield;             // 分配金利率 ※年率換算
+	public final double      dividend;           // 分配金累計
+	public final double      yield;              // 分配金利率 ※年率換算
 	
-	public final double      rorPrice;          // 基準価格のリターン割合             ※年率換算
-	public final double      rorReinvestment;   // 分配金再投資基準価格のリターン割合 ※年率換算
-	public final double      rorNoReinvestment; // 分配金受取基準価格のリターン割合   ※年率換算
+	public final double      rorPrice;           // 基準価格のリターン割合             ※年率換算
+	public final double      rorReinvestment;    // 分配金再投資基準価格のリターン割合 ※年率換算
+	public final double      rorNoReinvestment;  // 分配金受取基準価格のリターン割合   ※年率換算
 	
-	public final double      standardDeviation; // 基準価格日次リターンの標準偏差 ※年率換算
+	public final double      standardDeviation;  // 基準価格日次リターンの標準偏差 ※年率換算
 	
 	private AnnualStats(final MonthlyStats[] monthlyStatsArray, final int nYear) {
 		final int nMonth = nYear * 12;
@@ -74,26 +74,32 @@ public final class AnnualStats {
 		
 		startDate            = dateArray[startIndex];
 		startPrice           = retPrice[0];
-		startReinvested      = retReinvestment[0];
-		startNoReinvested    = retNoReinvestment[0];
+		startReinvestment    = retReinvestment[0];
+		startNoReinvestment  = retNoReinvestment[0];
 
 		endDate              = dateArray[stopIndexPlusOne - 1];
 		endPrice             = retPrice[retPrice.length - 1];
-		endReinvested        = retReinvestment[retReinvestment.length - 1];
-		endNoReinvested      = retNoReinvestment[retNoReinvestment.length - 1];
+		endReinvestment      = retReinvestment[retReinvestment.length - 1];
+		endNoReinvestment    = retNoReinvestment[retNoReinvestment.length - 1];
 		
 		dividend             = Stats.sum(divArray, startIndex, stopIndexPlusOne);
 		yield                = (dividend / endPrice) / nYear; // yield per year
 		
 		rorPrice             = SimpleReturn.compoundAnnualReturn(SimpleReturn.getValue(startPrice, endPrice), nYear);
-		rorReinvestment      = SimpleReturn.compoundAnnualReturn(SimpleReturn.getValue(startReinvested, endReinvested), nYear);
-		rorNoReinvestment    = SimpleReturn.compoundAnnualReturn(SimpleReturn.getValue(startNoReinvested, endNoReinvested), nYear);
+		rorReinvestment      = SimpleReturn.compoundAnnualReturn(SimpleReturn.getValue(startReinvestment, endReinvestment), nYear);
+		rorNoReinvestment    = SimpleReturn.compoundAnnualReturn(SimpleReturn.getValue(startNoReinvestment, endNoReinvestment), nYear);
 		
 		{
 			// calculate standard deviation from retPrice
-			double[] simpleReturnArray = DoubleArray.toDoubleArray(retPrice, new SimpleReturn());
-			double sd = Stats.standardDeviation(simpleReturnArray);
+			double[] array = DoubleArray.toDoubleArray(retPrice, new SimpleReturn());
+			double sd = Stats.standardDeviation(array);
 			standardDeviation = Finance.annualStandardDeviationFromDailyStandardDeviation(sd);
+		}
+		
+		{
+			Stats stats = new Stats(retPrice);
+			double cov = stats.standardDeviation() / stats.mean();
+			logger.info("XXX  {}  {}", standardDeviation, cov);
 		}
 		
 	}
