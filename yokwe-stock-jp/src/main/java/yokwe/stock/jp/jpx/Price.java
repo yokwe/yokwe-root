ce70afa0895bd1d621df74fc64f1fa4bf50058ef
hasegawa <hasegawa@localhost>
2023-08-02 11:01:29 +0900

    Remove unused import.

--- yokwe-stock-jp/src/main/java/yokwe/stock/jp/jpx/Price.java
+++ yokwe-stock-jp/src/main/java/yokwe/stock/jp/jpx/Price.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 import yokwe.stock.jp.Storage;
 import yokwe.util.ListUtil;
