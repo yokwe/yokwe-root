95fb29e3656838f0d9be517c8d5bb48f5f960b2b
hasegawa <hasegawa@localhost>
2023-08-14 20:06:12 +0900

    Move IndexRange to DailyValue.

--- yokwe-util/src/main/java/yokwe/util/finance/DailyValue.java
+++ yokwe-util/src/main/java/yokwe/util/finance/DailyValue.java
@@ -2,6 +2,7 @@ package yokwe.util.finance;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.function.Function;
 
 import yokwe.util.GenericArray;
 import yokwe.util.StringUtil;
@@ -9,6 +10,71 @@ import yokwe.util.UnexpectedException;
 
 public final class DailyValue implements Comparable<DailyValue> {
 	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
+	
+	public static final class IndexRange {
+		public static <T> IndexRange getInstance(T[] array, LocalDate startDate, LocalDate endDate, Function<T, LocalDate> op) {
+			// return index of array between startDaten inclusive and endDate inclusive
+			
+			// sanity check
+			{
+				if (startDate.isAfter(endDate)) {
+					logger.error("Unexpeced date");
+					logger.error("  startDate         {}", startDate);
+					logger.error("  endDate           {}", endDate);
+					throw new UnexpectedException("Unexpeced date");
+				}
+			}
+			
+			int       startIndex           = -1;		
+			int       stopIndexPlusOne     = -1;
+			LocalDate startIndexDate       = null;
+			LocalDate stopIndexPlusOneDate = null;
+			
+			for(int i = 0; i < array.length; i++) {
+				LocalDate date = op.apply(array[i]);
+				if (date.isEqual(startDate) || date.isAfter(startDate)) {
+					// youngest date equals to startDate or after startDate
+					if (startIndexDate == null || date.isBefore(startIndexDate)) {
+						startIndex     = i;
+						startIndexDate = date;
+					}
+				}
+				if (date.isEqual(endDate) || date.isAfter(endDate)) {
+					// youngest date equals to endDate or after endDate
+					if (stopIndexPlusOneDate == null || date.isBefore(stopIndexPlusOneDate)) {
+						stopIndexPlusOne     = i + 1; // plus one for PluOne
+						stopIndexPlusOneDate = date;
+					}
+				}
+			}
+			return new IndexRange(startIndex, stopIndexPlusOne);
+		}
+
+		public static IndexRange getInstance(LocalDate[] array, LocalDate startDate, LocalDate endDate) {
+			return getInstance(array, startDate, endDate, Function.identity());
+		}
+		
+		public final int startIndex;
+		public final int stopIndexPlusOne;
+		
+		public IndexRange(int startIndex, int stopIndexPlusOne) {
+			this.startIndex       = startIndex;
+			this.stopIndexPlusOne = stopIndexPlusOne;
+		}
+		
+		public boolean isValid() {
+			return 0 <= startIndex && 0 <= stopIndexPlusOne && startIndex < stopIndexPlusOne;
+		}
+		
+		public int size() {
+			return stopIndexPlusOne - startIndex;
+		}
+		
+		@Override
+		public String toString() {
+			return String.format("{%d  %d}", startIndex, stopIndexPlusOne);
+		}
+	}
 	
 	///////////////////////////////////////////////////////////////////////////
 	// DailyValue[] to double[]
