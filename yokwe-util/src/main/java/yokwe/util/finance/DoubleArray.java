a9eabf2ac1b8e176de742e81ba0f76076ecd3413
hasegawa <hasegawa@localhost>
2023-08-15 21:01:13 +0900

    Add statistics methods.

--- yokwe-util/src/main/java/yokwe/util/finance/DoubleArray.java
+++ yokwe-util/src/main/java/yokwe/util/finance/DoubleArray.java
@@ -9,7 +9,13 @@ import java.util.function.IntFunction;
 import java.util.function.ToDoubleFunction;
 
 import yokwe.util.DoubleUtil;
+import yokwe.util.finance.online.Correlation;
+import yokwe.util.finance.online.Covariance;
+import yokwe.util.finance.online.Mean;
 import yokwe.util.finance.online.OnlineDoubleBinaryOperator;
+import yokwe.util.finance.online.OnlineDoubleUnaryOperator;
+import yokwe.util.finance.online.Sum;
+import yokwe.util.finance.online.Variance;
 
 public final class DoubleArray {
 	// FIXME add test case for each methods
@@ -96,9 +102,6 @@ public final class DoubleArray {
 		}
 		return result;
 	}
-	public static double[] toDoubleArray(double[] array, int startIndex, int stopIndexPlusOne) {
-		return toDoubleArray(array, startIndex, stopIndexPlusOne, DoubleUnaryOperator.identity());
-	}
 	public static double[] toDoubleArray(double[] array, DoubleUnaryOperator op) {
 		return toDoubleArray(array, 0, array.length, op);
 	}
@@ -119,6 +122,7 @@ public final class DoubleArray {
 		return result;
 	}
 	public static double[] toDoubleArray(double[] a, double[] b, DoubleBinaryOperator op) {
+		Util.checkIndex(a, b);
 		return toDoubleArray(a, b, 0, a.length, op);
 	}
 	///////////////////////////////////////////////////////////////////////////
@@ -129,6 +133,7 @@ public final class DoubleArray {
 		return toDoubleArray(a, b, startIndex, stopIndexPlusOne, multiplyOp);
 	}
 	public static double[] multiply(double[] a, double[] b) {
+		Util.checkIndex(a, b);
 		return multiply(a, b, 0, a.length);
 	}
 	
@@ -143,6 +148,90 @@ public final class DoubleArray {
 			op.accept(a[i], b[i]);
 		}
 		return op.getAsDouble();
+	}
+	///////////////////////////////////////////////////////////////////////////
+	// covariance
+	///////////////////////////////////////////////////////////////////////////
+	public static double covariance(double[] a, double[] b, int startIndex, int stopIndexPlusOne) {
+		return toDouble(a, b, startIndex, stopIndexPlusOne, new Covariance());
+	}
+	public static double covariance(double[] a, double[] b) {
+		Util.checkIndex(a, b);
+		return covariance(a, b, 0, a.length);
+	}
+	///////////////////////////////////////////////////////////////////////////
+	// correlation
+	///////////////////////////////////////////////////////////////////////////
+	public static double correlation(double[] a, double[] b, int startIndex, int stopIndexPlusOne) {
+		return toDouble(a, b, startIndex, stopIndexPlusOne, new Correlation());
+	}
+	public static double correlation(double[] a, double[] b) {
+		Util.checkIndex(a, b);
+		return correlation(a, b, 0, a.length);
+	}
+
+	
+	
+	///////////////////////////////////////////////////////////////////////////
+	// double[] to double
+	///////////////////////////////////////////////////////////////////////////
+	public static double toDouble(double[] array, int startIndex, int stopIndexPlusOne, OnlineDoubleUnaryOperator op) {
+		Util.checkIndex(array, startIndex, stopIndexPlusOne);
+		for(int i = startIndex; i < stopIndexPlusOne; i++) {
+			op.accept(array[i]);
+		}
+		return op.getAsDouble();
+	}
+	///////////////////////////////////////////////////////////////////////////
+	// sum
+	///////////////////////////////////////////////////////////////////////////
+	public static double sum(double[] array, int startIndex, int stopIndexPlusOne) {
+		return toDouble(array, startIndex, stopIndexPlusOne, new Sum());
+	}
+	public static double sum(double[] array) {
+		Util.checkIndex(array);
+		return sum(array, 0, array.length);
+	}
+	///////////////////////////////////////////////////////////////////////////
+	// man
+	///////////////////////////////////////////////////////////////////////////
+	public static double mean(double[] array, int startIndex, int stopIndexPlusOne) {
+		return toDouble(array, startIndex, stopIndexPlusOne, new Mean());
+	}
+	public static double mean(double[] array) {
+		return mean(array, 0, array.length);
+	}
+	///////////////////////////////////////////////////////////////////////////
+	// variance
+	///////////////////////////////////////////////////////////////////////////
+	public static double variance(double[] array, int startIndex, int stopIndexPlusOne) {
+		return toDouble(array, startIndex, stopIndexPlusOne, new Variance());
+	}
+	public static double variance(double[] array) {
+		Util.checkIndex(array);
+		return variance(array, 0, array.length);
+	}
+	///////////////////////////////////////////////////////////////////////////
+	// standard deviation
+	///////////////////////////////////////////////////////////////////////////
+	public static double standardDeviation(double[] array, int startIndex, int stopIndexPlusOne) {
+		return Math.sqrt(toDouble(array, startIndex, stopIndexPlusOne, new Variance()));
+	}
+	public static double standardDeviation(double[] array) {
+		Util.checkIndex(array);
+		return standardDeviation(array, 0, array.length);
+	}
+	///////////////////////////////////////////////////////////////////////////
+	// standard measure
+	///////////////////////////////////////////////////////////////////////////
+	public static double[] standardMeasure(double[] array) {
+		Util.checkIndex(array);
+		var op = new Variance();
+		op.accept(array);
+		
+		double sd   = op.standardDeviation();
+		double mean = op.mean();
+		return DoubleArray.toDoubleArray(array, o -> (o - mean) / sd);
 	}
 	
 }
