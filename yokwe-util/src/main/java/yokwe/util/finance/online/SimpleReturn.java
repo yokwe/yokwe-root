c6f8bb87ee04f13378cd7b0aea15e53152b6d8f1
hasegawa <hasegawa@localhost>
2023-08-16 17:52:56 +0900

    Simplify code.

--- yokwe-util/src/main/java/yokwe/util/finance/online/SimpleReturn.java
+++ yokwe-util/src/main/java/yokwe/util/finance/online/SimpleReturn.java
@@ -4,50 +4,25 @@ import yokwe.util.UnexpectedException;
 
 public final class SimpleReturn implements OnlineDoubleUnaryOperator {
 	public static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
-	
-	//
-	// annual rate of return
-	//
-	public static double annualReturn(double absoluteRateOfReturn, double durationInYear) {
-		// From https://en.wikipedia.org/wiki/Rate_of_return
-		//   According to the CFA Institute's Global Investment Performance Standards (GIPS),[3]
-		//   Returns for periods of less than one year must not be annualized.
-		if (durationInYear < 1.0) {
-			logger.warn("durationInYear is less than one");
-			logger.warn("  durationInYear  {}", durationInYear);
-//			throw new UnexpectedException("durationInYear is less than one");
-		}
 		
-		return absoluteRateOfReturn / durationInYear;
-	}
-	public static double compoundAnnualReturn(double simpleReturn, double durationInYear) {
-		// From https://en.wikipedia.org/wiki/Rate_of_return
-		//   According to the CFA Institute's Global Investment Performance Standards (GIPS),[3]
-		//   Returns for periods of less than one year must not be annualized.
-		if (durationInYear < 1.0) {
-			logger.warn("durationInYear is less than one");
-			logger.warn("  durationInYear  {}", durationInYear);
-//			throw new UnexpectedException("durationInYear is less than one");
-		}
-		
-		// if simpleReturn is for 3 month, durationInYear is 3 / 12 = 0.25
-		// if simpleReturn is for 2 year,  durationInYear is 2
-		// ((1 + simpleReturn) ^ (1 / durationInYear)) - 1
-		double base     = 1.0 + simpleReturn;
-		double exponent = 1.0 / durationInYear;
-		return Math.pow(base, exponent) - 1.0;
-	}
-	
-	public static double getValue(double startValue, double endValue) {
+	public static final double getValue(double startValue, double endValue) {
 		// (endValue - startValue) / startValue
 		// (endValue / statValue) - 1
 		return (endValue / startValue) - 1;
 	}
 	
 	
-	private boolean firstTime    = true;
-	private double  lastValue    = 0;
 	private double  simpleReturn = Double.NaN;
+	private boolean hasLastValue;
+	private double  lastValue;
+	
+	public SimpleReturn() {
+		this.hasLastValue = false;
+	}
+	public SimpleReturn(double lastValue) {
+		this.hasLastValue = true;
+		this.lastValue    = lastValue;
+	}
 	
 	@Override
 	public void accept(double value) {
@@ -58,13 +33,13 @@ public final class SimpleReturn implements OnlineDoubleUnaryOperator {
 			throw new UnexpectedException("value is infinite");
 		}
 		
-		if (firstTime) {
-			// use first value as previous
-			firstTime = false;
-			lastValue = value;
+		if (hasLastValue) {
+			simpleReturn = getValue(lastValue, value);
+		} else {
+			// treat value as lastValue
+			hasLastValue = true;
+			simpleReturn = 0; // simpleReturn(value, value) == 0
 		}
-		
-		simpleReturn = getValue(lastValue, value);
 		
 		// update for next iteration
 		lastValue = value;
