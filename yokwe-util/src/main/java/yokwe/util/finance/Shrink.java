494d5de7a93df341db80349c8e7480206c6d171d
hasegawa <hasegawa@localhost>
2023-08-17 14:15:57 +0900

    Teporary commit.

--- yokwe-util/src/main/java/yokwe/util/finance/Shrink.java
+++ yokwe-util/src/main/java/yokwe/util/finance/Shrink.java
@@ -1,38 +1,76 @@
 package yokwe.util.finance;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import yokwe.util.UnexpectedException;
 
 public class Shrink {
 	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
 	
-	private static final boolean[][] patternArrays;
+	
+	private static class ShrinkPattern {
+		final static List<ShrinkPattern> list = new ArrayList<>();
+		
+		static ShrinkPattern get(double targetPercent) {
+			if (list.size() < 5) {
+				logger.error("Unexpected value");
+				logger.error("  list  {}", list.size());
+				throw new UnexpectedException("Unexpected value");
+			}
+
+			ShrinkPattern result = list.get(0);
+			for(int i = 1; i < list.size(); i++) {
+				ShrinkPattern shrinkPattern = list.get(i);
+				double diff       = Math.abs(shrinkPattern.percent - targetPercent);
+				double resultDiff = Math.abs(result.percent - targetPercent);
+				if (diff < resultDiff) result = shrinkPattern;
+			}
+			
+			return result;
+		}
+		static void add(boolean... pattern) {
+			ShrinkPattern value = new ShrinkPattern(pattern);
+			list.add(value);
+			logger.info("ShrinkPattern {}", value);
+		}
+		
+		final double    percent;
+		final boolean[] pattern;
+		
+		private ShrinkPattern(boolean[] pattern) {
+			int count = 0;
+			for(var e: pattern) if (e) count++;
+
+			this.percent = (double)count / pattern.length;
+			this.pattern  = pattern;
+		}
+		
+		@Override
+		public String toString() {
+			List<String> list = new ArrayList<>();
+			for(var e: pattern) list.add(e ? "I" : "O");
+			return String.format("{%.3f, %s}", percent, list);
+		}
+	}
 	static {
 		boolean I = true;
 		boolean O = false;
-		patternArrays = new boolean[10][];
-		//                                0  1  2  3  4  5  6  7  8  9
-		patternArrays[9] = new boolean[] {I, I, I, I, I, O, I, I, I, I}; //  9 / 10      0 1 2 3 4   6 7 8 9 0 1 2 3 4   6 7 8 9
-		patternArrays[8] = new boolean[] {I, I, I, O, I, I, I, I, O, I}; //  8 / 10      0 1 2   4 5 6 7   9 0 1 2   4 5 6 7   9
-		patternArrays[7] = new boolean[] {I, I, I, O, I, I, O, I, I, O}; //  7 / 10      0 1 2   4 5   7 8   0 1 2   4 5   7 8  
-		patternArrays[6] = new boolean[] {I, O, I, O, I, I, O, I, O, I}; //  6 / 10      0   2   4 5   7   9 0   2   4 5   7   9
 		
-		patternArrays[5] = new boolean[] {O, I, O, I, O, I, O, I, O, I}; //  5 / 10        1   3   5   7   9   1   3   5   7   9
-
-		patternArrays[4] = new boolean[10]; // !pattern[6]
-		patternArrays[3] = new boolean[10]; // !pattern[7]
-		patternArrays[2] = new boolean[10]; // !pattern[8]
-		patternArrays[1] = new boolean[10]; // !pattern[9]
-		patternArrays[0] = new boolean[10];
-		for(int i = 1; i <= 4; i++) {
-			for(int j = 0; j < 10; j++) {
-				patternArrays[i][j] = !patternArrays[10 - i][j];
-			}
-		}
+		ShrinkPattern.add(I);
+		ShrinkPattern.add(O, I, I, I, I, I, I, I, I, I);
+		ShrinkPattern.add(O, I, I, I, I);
+		ShrinkPattern.add(O, I, I, O, I, I, I, O, I, I);
+		ShrinkPattern.add(O, I, I, O, I);
+		ShrinkPattern.add(O, I);
+		ShrinkPattern.add(O, O, I, O, I);
+		ShrinkPattern.add(O, O, I, O, O, O, I, O, O, I);
+		ShrinkPattern.add(O, O, O, O, I);
+		ShrinkPattern.add(O, O, O, O, O, O, O, O, O, I);
 	}
 	
-	double[] toDoubleArray(double[] array, int startIndex, int stopIndexPlusOne, double ratio) {
+	static double[] toDoubleArray(double[] array, int startIndex, int stopIndexPlusOne, double ratio) {
 		// sanity check
 		Util.checkIndex(array, startIndex, stopIndexPlusOne);
 		if (ratio < 0 || 1 < ratio) {
@@ -40,22 +78,12 @@ public class Shrink {
 			logger.error("  ratio  {}", ratio);
 			throw new UnexpectedException("Unexpected value");
 		}
-		
-		int ratioIndex = (int)(ratio * 10 + 0.5);
-		if (ratioIndex < 1) {
-			logger.error("Unexpected value");
-			logger.error("  ratio        {}", ratio);
-			logger.error("  ratioIndex   {}", ratioIndex);
-			throw new UnexpectedException("Unexpected value");
-		}
-		if (10 <= ratioIndex) {
-			return Arrays.copyOfRange(array, startIndex, stopIndexPlusOne);
-		}
-		boolean[] patternArray = patternArrays[ratioIndex];
+		ShrinkPattern shrinkPattern = ShrinkPattern.get(ratio);
+		int patternArrayLengh = shrinkPattern.pattern.length;
 		
 		int length      = stopIndexPlusOne - startIndex;
-		int resultLengh = (length * 10) / ratioIndex;
-		if (resultLengh < 100) {
+		int resultLengh = (int)(length * shrinkPattern.percent + 0.5);
+		if (resultLengh < 0) {
 			logger.error("Unexpected value");
 			logger.error("  ratio        {}", ratio);
 			logger.error("  length       {}", length);
@@ -66,11 +94,34 @@ public class Shrink {
 		double[] resultArray  = new double[resultLengh + 1];
 		int      resultLength = 0;
 		for(int i = 0; i < length; i++) {
-			if (patternArray[i % 10]) {
+			if (shrinkPattern.pattern[i % patternArrayLengh]) {
 				resultArray[resultLength++] = array[startIndex + i];
 			}
 		}
 		
 		return Arrays.copyOf(resultArray, resultLength);
+	}
+	static double[] toDoubleArray(double[] array, double ratio) {
+		return toDoubleArray(array, 0, array.length, ratio);
+	}
+	
+	public static void main(String[] args) {
+		logger.info("START");
+		
+		double[] array = new double[10000];
+		Arrays.fill(array, 1);
+		
+		{
+			double ratio = 1;
+			double[] shrinkArray = toDoubleArray(array, ratio);
+			logger.info("{}  {}", ratio, shrinkArray.length);
+		}
+		{
+			double ratio = 0.5;
+			double[] shrinkArray = toDoubleArray(array, ratio);
+			logger.info("{}  {}", ratio, shrinkArray.length);
+		}
+		
+		logger.info("STOP");
 	}
 }
