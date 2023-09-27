e19f6968de2b6a132b335b3e0ae6003ad99c227f
hasegawa <hasegawa@localhost>
2023-09-27 18:14:54 +0900

    Reorganize structure.

--- yokwe-finance/src/main/java/yokwe/finance/stock/us/UpdateStockPrice.java
+++ yokwe-finance/src/main/java/yokwe/finance/stock/us/UpdateStockPrice.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import yokwe.finance.provider.nasdaq.api.API;
 import yokwe.finance.provider.nasdaq.api.AssetClass;
 import yokwe.finance.provider.nasdaq.api.Historical;
 import yokwe.finance.type.OHLCV;
+import yokwe.finance.type.StockInfoUS;
 import yokwe.util.MarketHoliday;
 import yokwe.util.UnexpectedException;
 
@@ -40,16 +43,10 @@ public class UpdateStockPrice {
 			return String.format("{%s  %s  %s  %s  %s}", stockCode, assetClass, startDate, stopDate, name);
 		}
 	}
-		
-	private static void update() {
-		// build task list
+	
+	private static List<Task> getTaskList(LocalDate lastTradingDate, List<StockInfoUS> stockList) {
 		var taskList = new ArrayList<Task>();
 		{
-			var lastTradingDate = MarketHoliday.US.getLastTradingDate();
-			logger.info("date     {}", lastTradingDate);
-			var stockList       = StockInfo.getList();
-			logger.info("list     {}", stockList.size());
-
 			int countA = 0;
 			int countB = 0;
 			int countC = 0;
@@ -82,82 +79,126 @@ public class UpdateStockPrice {
 			logger.info("countC   {}", countC);
 			logger.info("task     {}", taskList.size());
 		}
-		
-		// process task list
-		{
-			int count    = 0;
-			int countMod = 0;
-			for(var task: taskList) {
-				String stockCode = task.stockCode;
-				if ((++count % 100) == 1) logger.info("{}  /  {}  {}", count, taskList.size(), task);
+		return taskList;
+	}
+	private static int processTask(List<Task> taskList) {
+		int count    = 0;
+		int countMod = 0;
+		int countA   = 0;
+		int countB   = 0;
+		int countC   = 0;
+		int countD   = 0;
+		int countE   = 0;
+		Collections.shuffle(taskList);
+		for(var task: taskList) {
+			String stockCode = task.stockCode;
+			if ((++count % 100) == 1) logger.info("{}  /  {}  {}", count, taskList.size(), task);
+			
+			// If starDate equals stopDate, Historical.getInstance() returns no data.
+			// So temporary decrease startDate and keep modified startDate as skipDate
+			// When enumerate row, skip data if date is same as skipDate
+			LocalDate skipDate = null;
+			if (task.startDate.equals(task.stopDate)) {
+				task.startDate = task.startDate.minusDays(1);
+				skipDate = task.startDate;
+			}
+							
+			var historical = Historical.getInstance(stockCode, task.assetClass, task.startDate, task.stopDate);
+			
+			if (historical == null) {
+				logger.warn("historical is null  {}", task);
+				countA++;
+				continue;
+			}
+			if (historical.data == null) {
+				logger.warn("historical.data is null  {}", task);
+				countB++;
+				continue;
+			}
+			if (historical.data.tradesTable == null) {
+				logger.warn("historical.data.tradesTable is null  {}", task);
+				countC++;
+				continue;
+			}
+			if (historical.data.tradesTable.rows == null) {
+				logger.warn("historical.data.tradesTable.rows is null  {}", task);
+				countD++;
+				continue;
+			}
+			
+			// read existing data
+			var list = StockPrice.getList(stockCode);
+			var set  = list.stream().map(o -> o.date).collect(Collectors.toSet());
+			int countAdd = 0;
+			for(var row: historical.data.tradesTable.rows) {
+				LocalDate  date   = LocalDate.parse(API.convertDate(row.date));
 				
-				// If starDate equals stopDate, Historical.getInstance() returns no data.
-				// So temporary decrease startDate and keep modified startDate as skipDate
-				// When enumerate row, skip data if date is same as skipDate
-				LocalDate skipDate = null;
-				if (task.startDate.equals(task.stopDate)) {
-					task.startDate = task.startDate.minusDays(1);
-					skipDate = task.startDate;
-				}
-								
-				var historical = Historical.getInstance(stockCode, task.assetClass, task.startDate, task.stopDate);
-				
-				if (historical == null) {
-					logger.warn("historical is null  {}", task);
-					continue;
-				}
-				if (historical.data == null) {
-					logger.warn("historical.data is null  {}", task);
-					continue;
-				}
-				if (historical.data.tradesTable == null) {
-					logger.warn("historical.data.tradesTable is null  {}", task);
-					continue;
-				}
-				if (historical.data.tradesTable.rows == null) {
-					logger.warn("historical.data.tradesTable.rows is null  {}", task);
+				// hack for the case startDate equals stopDate, see above comment
+				if (skipDate != null && date.equals(skipDate)) {
+//					logger.info("skipDate  {}", skipDate);
 					continue;
 				}
 				
-				// read existing data
-				var list = StockPrice.getList(stockCode);
-				var set  = list.stream().map(o -> o.date).collect(Collectors.toSet());
-				int countAdd = 0;
-				for(var row: historical.data.tradesTable.rows) {
-					LocalDate  date   = LocalDate.parse(API.convertDate(row.date));
-					
-					// hack for the case startDate equals stopDate, see above comment
-					if (skipDate != null && date.equals(skipDate)) {
-//						logger.info("skipDate  {}", skipDate);
-						continue;
-					}
-					
-					BigDecimal open   = new BigDecimal(row.open.replace(",", "").replace("$", ""));
-					BigDecimal high   = new BigDecimal(row.high.replace(",", "").replace("$", ""));
-					BigDecimal low    = new BigDecimal(row.low.replace(",", "").replace("$", ""));
-					BigDecimal close  = new BigDecimal(row.close.replace(",", "").replace("$", ""));
-					long       volume = Long.parseLong(row.volume.replace(",", "").replace("N/A", "0"));
-					
-					var price = new OHLCV(date, open, high, low, close, volume);
-					
-					if (set.contains(date)) {
-						logger.error("Unexpected date");
-						logger.error("  date  {}", date);
-						logger.error("  task  {}", task);
-						throw new UnexpectedException("Unexpected date");
-					} else {
-						list.add(price);
-						countAdd++;
-					}
-				}
-
-//				logger.info("save  {}  {}", list.size(), StockPrice.getPath(stockCode));
-				if (countAdd != 0) {
-					StockPrice.save(stockCode, list);
-					countMod++;
+				BigDecimal open   = new BigDecimal(row.open.replace(",", "").replace("$", ""));
+				BigDecimal high   = new BigDecimal(row.high.replace(",", "").replace("$", ""));
+				BigDecimal low    = new BigDecimal(row.low.replace(",", "").replace("$", ""));
+				BigDecimal close  = new BigDecimal(row.close.replace(",", "").replace("$", ""));
+				long       volume = Long.parseLong(row.volume.replace(",", "").replace("N/A", "0"));
+				
+				var price = new OHLCV(date, open, high, low, close, volume);
+				
+				if (set.contains(date)) {
+					logger.error("Unexpected date");
+					logger.error("  date  {}", date);
+					logger.error("  task  {}", task);
+					throw new UnexpectedException("Unexpected date");
+				} else {
+					list.add(price);
+					countAdd++;
 				}
 			}
-			logger.info("countMod {}", countMod);
+			countE++;
+
+//			logger.info("save  {}  {}", list.size(), StockPrice.getPath(stockCode));
+			if (countAdd != 0) {
+				StockPrice.save(stockCode, list);
+				countMod++;
+			}
+		}
+		logger.info("countA   {}", countA);
+		logger.info("countB   {}", countB);
+		logger.info("countC   {}", countC);
+		logger.info("countD   {}", countD);
+		logger.info("countE   {}", countE);
+
+		logger.info("countMod {}", countMod);
+		
+		return countMod;
+	}
+	
+	
+	private static void update() {
+		var lastTradingDate = MarketHoliday.US.getLastTradingDate();
+		logger.info("date     {}", lastTradingDate);
+		var stockList       = StockInfo.getList();
+		logger.info("list     {}", stockList.size());
+
+		for(int count = 1; count < 10; count++) {
+			logger.info("try      {}", count);
+			
+			// build task list
+			var taskList = getTaskList(lastTradingDate, stockList);
+			// process task list
+			int countMod = processTask(taskList);
+			
+			if (countMod == 0) break;
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				//
+			}
+			logger.info("retry");
 		}
 	}
 	
