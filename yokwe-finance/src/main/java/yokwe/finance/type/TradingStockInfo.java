44210c5b8a2eb39b2c3c8fd5ffcb02cc4feb4288
hasegawa <hasegawa@localhost>
2023-09-30 19:26:17 +0900

    Add common class for trading stock

---
+++ yokwe-finance/src/main/java/yokwe/finance/type/TradingStockInfo.java
@@ -0,0 +1,37 @@
+package yokwe.finance.type;
+
+public class TradingStockInfo implements Comparable<TradingStockInfo>{
+	public enum FeeType {
+		NOT_FREE,
+		BUY_FREE,
+		BUY_SELL_FREE,
+	}
+	public enum TradeType {
+		BUY_SELL,
+		SELL,
+		NONE,
+	}
+	
+	public String    stockCode;
+	public FeeType   feeType;
+	public TradeType tradeType;
+	
+	@Override
+	public String toString() {
+		return String.format("{%s  %s  %s}", stockCode, feeType, tradeType);
+	}
+	@Override
+	public int compareTo(TradingStockInfo that) {
+		return this.stockCode.compareTo(that.stockCode);
+	}
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof TradingStockInfo) {
+			TradingStockInfo that = (TradingStockInfo)o;
+			return
+				this.stockCode.equals(that.stockCode);
+		} else {
+			return false;
+		}
+	}
+}
