db0e245faa5e69b0c05a1e9b2b1ff7c25d62dfba
hasegawa <hasegawa@localhost>
2023-10-04 14:24:50 +0900

    Temporary commit.

--- yokwe-finance/src/main/java/yokwe/finance/stock/us/UpdateStockDiv.java
+++ yokwe-finance/src/main/java/yokwe/finance/stock/us/UpdateStockDiv.java
@@ -46,24 +46,27 @@ public class UpdateStockDiv {
 		return new BigDecimal(string.replace(",", "").replace("$", ""));
 	}
 
-	
+	private enum TaskType {
+		LAST_ONE,
+		WHOLE,
+	}
 	private static class Task {
 		String     stockCode;
 		AssetClass assetClass;
-		int        limit;
+		TaskType   taskType;
 		String     name;
 		
 		// startDate an stopDate is inclusive
-		Task(String stockCode, AssetClass assetClass, int limit, String name) {
+		Task(String stockCode, AssetClass assetClass, TaskType taskType, String name) {
 			this.stockCode  = stockCode;
 			this.assetClass = assetClass;
-			this.limit      = limit;
+			this.taskType   = taskType;
 			this.name       = name;
 		}
 		
 		@Override
 		public String toString() {
-			return String.format("{%s  %s  %d  %s}", stockCode, assetClass, limit, name);
+			return String.format("{%s  %s  %s  %s}", stockCode, assetClass, taskType, name);
 		}
 	}
 	
@@ -75,34 +78,45 @@ public class UpdateStockDiv {
 		int countA = 0;
 		int countB = 0;
 		int countC = 0;
+		int countD = 0;
 		
 		for (var stockInfo : stockList) {
 			String     stockCode  = stockInfo.stockCode;
 			AssetClass assetClass = stockInfo.type.isETF() ? AssetClass.ETF : AssetClass.STOCK;
-			int        limit;
+			TaskType   taskType;
 			
 			String path = StockDiv.getPath(stockCode);
 			if (FileUtil.canRead(path)) {
 				Instant  lastModified = FileUtil.getLastModified(path);
 				Duration duration     = Duration.between(lastModified, now);
 				if (GRACE_PERIOD_IN_DAYS < duration.toDays()) {
-					limit = 1;
-					countA++;
+					// after grace period
+					var list = StockDiv.getList(stockCode);
+					if (list.isEmpty()) {
+						taskType = TaskType.WHOLE;
+						countA++;
+					} else {
+						taskType = TaskType.LAST_ONE;
+						countB++;
+					}
 				} else {
-					countB++;
+					// within grace period
+					countC++;
 					continue;
 				}
 			} else {
-				limit = 9999;
-				countC++;
+				// no file
+				taskType = TaskType.WHOLE;
+				countD++;
 			}
 
-			taskList.add(new Task(stockCode, assetClass, limit, stockInfo.name));
+			taskList.add(new Task(stockCode, assetClass, taskType, stockInfo.name));
 		}
 		
 		logger.info("countA   {}", countA);
 		logger.info("countB   {}", countB);
 		logger.info("countC   {}", countC);
+		logger.info("countD   {}", countD);
 		logger.info("task     {}", taskList.size());
 		
 		return taskList;
@@ -112,19 +126,28 @@ public class UpdateStockDiv {
 		int count    = 0;
 		int countA   = 0;
 		int countB   = 0;
+		int countC   = 0;
 		int countMod = 0;
 		Collections.shuffle(taskList);
 		for(var task: taskList) {
 			String stockCode = task.stockCode;
+			
 			if ((++count % 100) == 1) logger.info("{}  /  {}  {}", count, taskList.size(), task);
 			
 			var list = StockDiv.getList(stockCode);
-
-			var div = Dividends.getInstance(StockInfoUS.toNASDAQSymbol(stockCode), task.assetClass, task.limit);
+			
+			Dividends div;
+			{
+				if (task.taskType == TaskType.WHOLE) {
+					div = Dividends.getInstance(StockInfoUS.toNASDAQSymbol(stockCode), task.assetClass);
+				} else {
+					div = Dividends.getInstance(StockInfoUS.toNASDAQSymbol(stockCode), task.assetClass, 2);
+				}
+			}
 			if (div == null || div.data == null || div.data.dividends == null || div.data.dividends.rows == null) {
 				if (list.isEmpty()) {
-					// Write file to update last modified time
-					StockDiv.save(stockCode, new ArrayList<DailyValue>());
+					// Update last modified time of file
+					StockDiv.save(stockCode, list);
 				}
 				countA++;
 				continue;
@@ -133,22 +156,31 @@ public class UpdateStockDiv {
 			int countAdd = 0;
 			
 			var myMap = new HashMap<LocalDate, DailyValue>();
-			// build myMap
-			for(var row: div.data.dividends.rows) {
-				// Skip if exOfEffDate is N/A
-				if (row.exOrEffDate.equals(API.NOT_AVAILABLE)) continue;
-				
-				LocalDate  date  = toLocalDate(row.exOrEffDate);
-				BigDecimal value = toBigDecimal(row.amount);
-				
-				if (myMap.containsKey(date)) {
-					DailyValue old = myMap.get(date);
-					old.value = old.value.add(value);
-				} else {
-					myMap.put(date, new DailyValue(date, value));
+			// build myMap  --  summaries data by date
+			{
+				for(var row: div.data.dividends.rows) {
+					// Skip if exOfEffDate is N/A
+					if (row.exOrEffDate.equals(API.NOT_AVAILABLE)) continue;
+					
+					LocalDate  date  = toLocalDate(row.exOrEffDate);
+					BigDecimal value = toBigDecimal(row.amount);
+					
+					if (myMap.containsKey(date)) {
+						DailyValue old = myMap.get(date);
+						old.value = old.value.add(value);
+					} else {
+						myMap.put(date, new DailyValue(date, value));
+					}
+				}
+				if (task.taskType == TaskType.LAST_ONE && 2 <= myMap.size()) {
+					LocalDate maxDate = myMap.keySet().stream().max(LocalDate::compareTo).get();
+					var value = myMap.get(maxDate);
+					myMap.clear();
+					myMap.put(value.date, value);
 				}
 			}
 			
+			// create map of existing data
 			var map = list.stream().collect(Collectors.toMap(o -> o.date, Function.identity()));
 			// update map using mayMap
 			for(var entry: myMap.entrySet()) {
@@ -173,13 +205,14 @@ public class UpdateStockDiv {
 			}
 			
 			StockDiv.save(stockCode, map.values());
-			countB++;
+			countC++;
 			
 			if (countAdd != 0) countMod++;
 		}
 		
 		logger.info("countA   {}", countA);
 		logger.info("countB   {}", countB);
+		logger.info("countC   {}", countC);
 		logger.info("countMod {}", countMod);
 
 		return countMod;
