0198de1a88da0bf04868d0acbd02439b8dad27f3
hasegawa <hasegawa@localhost>
2023-11-19 09:31:00 +0900

    To workaround exception of WindwoManager, add Manager class.

--- yokwe-finance/src/main/java/yokwe/finance/util/WebBrowser.java
+++ yokwe-finance/src/main/java/yokwe/finance/util/WebBrowser.java
@@ -2,12 +2,19 @@ package yokwe.finance.util;
 
 import java.io.Closeable;
 import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.TimeZone;
 
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.BuildInfo;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Point;
@@ -16,8 +23,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.manager.SeleniumManager;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariDriverService;
+import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -25,6 +34,7 @@ import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import yokwe.util.FileUtil;
 import yokwe.util.UnexpectedException;
+import yokwe.util.json.JSON;
 
 public class WebBrowser implements Closeable{
 	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
@@ -35,49 +45,142 @@ public class WebBrowser implements Closeable{
 		SLF4JBridgeHandler.install();
 	}
 	
-	private static final Duration DEFAULT_IMPLICITLY_WAIT = Duration.ofMillis(100);
+	public static class Manager {
+		private static final String VERSION_BETA = "0";
+		
+		public static class Log {
+			public String level;
+			public long   timestamp;
+			public String message;
+			
+			@Override
+			public String toString() {
+				LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), TimeZone.getDefault().toZoneId());					
+				return String.format("{}", level, localDateTime, message);
+			}
+		}
+		public static class Result {
+			@JSON.Name("code")         public int    code;
+		    @JSON.Name("message")      public String message;
+		    @JSON.Name("driver_path")  public String driverPath;
+		    @JSON.Name("browser_path") public String browserPath;
+		    
+		    @Override
+		    public String toString() {
+		    	return String.format("{%d  %s  %s  %s}", code, message, driverPath, browserPath);
+		    }
+		}
+		public static class Data {			
+			@JSON.Ignore public Log[]  logs;
+			             public Result result;
+		}
+		
+		private static String getCachePath() {
+			var env  = System.getenv("SE_CACHE_PATH");
+			var home = System.getProperty("user.home");
+			
+			final String path = (env != null) ? env : String.format("%s/.cache/selenium", home);
+			// sanity check
+			{
+				var file = new File(path);
+				if (!file.isDirectory()) {
+					logger.error("path is not directory");
+					logger.error("  path  {}", path);
+					logger.error("  env   {}", env);
+					logger.error("  home  {}", home);
+					throw new UnexpectedException("path is not directory");
+				}
+			}
+			return path;
+		}
+		
+		private static String getVersion() {
+			var label = new BuildInfo().getReleaseLabel();
+			int pos = label.lastIndexOf('.');
+			return String.format("%s.%s", VERSION_BETA, label.substring(0, pos));
+		}
+		
+		public static String geSeleniumManagerPath() {
+			// ~/.cache/selenium/manager/0.4.15/selenium-manager
+			var cachePath = getCachePath();
+			var version   = getVersion();
+			var path      = String.format("%s/manager/%s/selenium-manager", cachePath, version);
+			// sanity check
+			{
+				var file = new File(path);
+				if (!file.isFile()) {
+					logger.error("path is not file");
+					logger.error("  path  {}", path);
+					throw new UnexpectedException("path is not file");
+				}
+			}
+			return path;
+		}
+		
+		public static Result getResult(Capabilities capabilities) {
+			try {
+				var m = SeleniumManager.getInstance();
+				m.getDriverPath(capabilities, false);
+			} catch (Exception e) {
+				String exceptionName = e.getClass().getSimpleName();
+				logger.warn("excetion {}", exceptionName);
+			}
+			
+			try {
+				var out = new StringWriter();
+				{
+					String[] args    = {geSeleniumManagerPath(), "--browser", capabilities.getBrowserName(), "--output", "JSON"};
+					Process  process = Runtime.getRuntime().exec(args);
+					try (var stdin = process.inputReader()) {
+						stdin.transferTo(out);
+					}
+				}
+				var data = JSON.unmarshal(Data.class, out.toString());
+				if (data.result.code != 0) {
+					logger.error("Unexpected");
+					logger.error("  browser  {}", capabilities);
+					logger.error("  out      {}", out);
+					logger.error("  result   {}", data.result);
+					throw new UnexpectedException("Unexpected");
+				}
+				return data.result;
+			} catch (IOException e) {
+				String exceptionName = e.getClass().getSimpleName();
+				logger.error("{} {}", exceptionName, e);
+				throw new UnexpectedException(exceptionName, e);
+			}
+		}
+		
+	}
+	
+	
 	private static final int      DEFAULT_WINDOW_Y        = 0;
 	private static final int      DEFAULT_WINDOW_WIDTH    = 1000;
 	
 	public static WebDriver getWebDriverSafari(boolean logging) {
-		var service = new SafariDriverService.Builder().withLogging(false).build();
-		File driverExecutable = service.getDriverExecutable();
-		// sanity check
-		{
-			if (!driverExecutable.canExecute()) {
-				logger.error("No driver file");
-				logger.error("  path  {}", driverExecutable.getPath());
-				throw new UnexpectedException("No driver file");
-			}
-		}
+		var options = new SafariOptions();
+		var result  = Manager.getResult(options);
 		
+		var service = new SafariDriverService.Builder().withLogging(logging).build();		
 		// NOTE need to invoke setExecutable.
-		service.setExecutable(driverExecutable.getPath());
-		var driver = new SafariDriver(service);
+		service.setExecutable(result.driverPath);
 		
-		driver.manage().timeouts().implicitlyWait(DEFAULT_IMPLICITLY_WAIT);
-
-		return driver;
+		return new SafariDriver(service, options);
 	}
 	
 	public static WebDriver getWebDriverChrome() {
-		// FIXME
-		// NOTE SeleniumManager.getInstance() works.
-		//      But SeleniumManager.getInstance().getDriverPath() generate error
 		var options = new ChromeOptions();
 		
-//		var manager = SeleniumManager.getInstance();
-//		logger.info("manager {}", manager);
-//		logger.info("result  {}", manager.getDriverPath(options, false));
+		var result = Manager.getResult(options);
 		
 		var service = new ChromeDriverService.Builder().build();
-		// FIXME use SelenimumManager instead of hard code path of chromedriver.
-		service.setExecutable("/Users/hasegawa/.cache/selenium/chromedriver/mac-arm64/119.0.6045.105/chromedriver");
-		var driver  = new ChromeDriver(service, options);
-		return driver;
+		service.setExecutable(result.driverPath);
+		
+		return new ChromeDriver(service, options);
 	}
 	
 	public static WebDriver getWebDriver() {
+		// FIXME after update to macos 14.1.1, safari webdriver don't start
 //		return getWebDriverSafari(false);
 		return getWebDriverChrome(); 
 	}
