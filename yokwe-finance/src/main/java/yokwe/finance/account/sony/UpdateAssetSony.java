d1dacaa039c3e5ef4a503cabaa4d04da2cf8d3ac
hasegawa <hasegawa@localhost>
2023-11-20 18:57:04 +0900

    Add balance

--- yokwe-finance/src/main/java/yokwe/finance/account/sony/UpdateAssetSony.java
+++ yokwe-finance/src/main/java/yokwe/finance/account/sony/UpdateAssetSony.java
@@ -11,9 +11,24 @@ public class UpdateAssetSony {
 		try(var browser = new WebBrowserSony()) {
 			logger.info("login");
 			browser.login();
-			
 			browser.savePage(StorageSony.getPath("top.html"));
 			
+			logger.info("balance");
+			browser.balance();
+			browser.savePage(StorageSony.getPath("balance.html"));
+			
+			logger.info("balance deposit");
+			browser.balanceDeopsit();
+			browser.savePage(StorageSony.getPath("balance-deposit.html"));
+			
+			logger.info("balance deposit foreign");
+			browser.balanceDeopsitForeign();
+			browser.savePage(StorageSony.getPath("balance-deposit-foreign.html"));
+			
+			logger.info("balance fund");
+			browser.balanceFund();
+			browser.savePage(StorageSony.getPath("balance-fund.html"));
+
 			logger.info("logout");
 			browser.logout();
 		}
--- yokwe-finance/src/main/java/yokwe/finance/account/sony/WebBrowserSony.java
+++ yokwe-finance/src/main/java/yokwe/finance/account/sony/WebBrowserSony.java
@@ -12,7 +12,15 @@ public class WebBrowserSony extends WebBrowser {
 	private static final Target LOGOUT_A = new Target.ClickImpl(By.id("logout"));
 	private static final Target LOGOUT_B = new Target.JavascriptImpl("subYes()", "THANK YOU");
 	private static final Target LOGOUT_C = new Target.JavascriptImpl("allClose()");
-
+	
+	private static final Target TOP     = new Target.JavascriptImpl("hometop(1)");
+	
+	private static final Target BALANCE                 = new Target.JavascriptImpl("hometop(10)");
+	private static final Target BALANCE_DEPOSIT         = new Target.JavascriptImpl("balancecommon(1)");
+	private static final Target BALANCE_DEPOSIT_FOREIGN = new Target.JavascriptImpl("balancecommon(2)");
+	private static final Target BALANCE_FUND            = new Target.JavascriptImpl("balancecommon(3)");
+	
+	
 	public WebBrowserSony() {
 		super();
 	}
@@ -35,5 +43,22 @@ public class WebBrowserSony extends WebBrowser {
 		
 		LOGOUT_B.action(this);
 		LOGOUT_C.action(this);
+	}
+	
+	public void top() {
+		TOP.action(this);
+	}
+	
+	public void balance() {
+		BALANCE.action(this);
+	}
+	public void balanceDeopsit() {
+		BALANCE_DEPOSIT.action(this);
+	}
+	public void balanceDeopsitForeign() {
+		BALANCE_DEPOSIT_FOREIGN.action(this);
+	}
+	public void balanceFund() {
+		BALANCE_FUND.action(this);
 	}
 }
