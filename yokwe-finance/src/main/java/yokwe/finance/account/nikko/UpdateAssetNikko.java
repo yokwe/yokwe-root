3af0b4479deab6ade4b66e199582131b430d7bac
hasegawa <hasegawa@localhost>
2023-12-08 17:52:27 +0900

    Create Asset from balance page.

--- yokwe-finance/src/main/java/yokwe/finance/account/nikko/UpdateAssetNikko.java
+++ yokwe-finance/src/main/java/yokwe/finance/account/nikko/UpdateAssetNikko.java
@@ -1,10 +1,19 @@
 package yokwe.finance.account.nikko;
 
 import java.io.File;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
 import yokwe.finance.Storage;
+import yokwe.finance.account.Asset;
+import yokwe.finance.account.Asset.Company;
+import yokwe.finance.account.Asset.Currency;
 import yokwe.util.FileUtil;
 import yokwe.util.ScrapeUtil;
 import yokwe.util.StringUtil;
@@ -67,6 +76,7 @@ public class UpdateAssetNikko {
 		}
 	}
 	
+	
 	// 国内投資信託
 	public static class FundInfo {
 		public static final Pattern PAT = Pattern.compile(
@@ -126,6 +136,10 @@ public class UpdateAssetNikko {
 			"<td .+?>.+?</td>\\s+" +
 			"<td .+?><span .+?>(?<accountType>.+?)</span></td>\\s+" +
 			"<td .+?><span .+?>(?<units>.+?)<br>(?<currency>.+?)</span></td>\\s+" +
+			"<td .+?>\\s+<span .+?>(?<price>.+?)\\(.+?<br>(?<fxRate>.+?)</span>\\s+</td>\\s+" +
+			"<td .+?>\\s+(?<unitCostJPY>.+?)<br>\\s+<span>(?<costJPY>.+?)</span>\\s+</span></td>\\s+" +
+			"<td .+?><span .+?>(?<valueJPY>.+?)<br>\\s+<span .+?>(?<retrunsJPY>.+?)</span></span></td>\\s+" +
+			"</tr>" +
 			"",
 			Pattern.DOTALL
 		);
@@ -139,13 +153,33 @@ public class UpdateAssetNikko {
 		@ScrapeUtil.AsNumber
 		public final String units;
 		public final String currency;
+		@ScrapeUtil.AsNumber
+		public final String price;
+		@ScrapeUtil.AsNumber
+		public final String fxRate;
+		@ScrapeUtil.AsNumber
+		public final String unitCostJPY;
+		@ScrapeUtil.AsNumber
+		public final String costJPY;
+		@ScrapeUtil.AsNumber
+		public final String valueJPY;
+		@ScrapeUtil.AsNumber
+		public final String retrunsJPY;
 		
-		public ForeignStockInfo(String stockName, String stockCode, String accountType, String units, String currency) {
+		public ForeignStockInfo(
+			String stockName, String stockCode, String accountType, String units, String currency,
+			String price, String fxRate, String unitCostJPY, String costJPY, String valueJPY, String retrunsJPY) {
 			this.stockName   = stockName;
 			this.stockCode   = stockCode;
 			this.accountType = accountType;
 			this.units       = units;
 			this.currency    = currency;
+			this.price       = price;
+			this.fxRate      = fxRate;
+			this.unitCostJPY = unitCostJPY;
+			this.costJPY     = costJPY;
+			this.valueJPY    = valueJPY;
+			this.retrunsJPY  = retrunsJPY;
 		}
 		
 		@Override
@@ -155,31 +189,214 @@ public class UpdateAssetNikko {
 	}
 	
 	
-	private static void update() {
-		{
-			var page = FileUtil.read().file(FILE_BALANCE);
+	// 外貨建てMMF
+	public static class ForeignMMFInfo {
+		public static final Pattern PAT = Pattern.compile(
+				"<tr>\\s+" +
+				"<td .+?><span .+?>(?<name>[^\\s]+)\\s+（(?<currency>...)）<br><div .+?>　</div></span></td>\\s+" +
+				"<td .+?>.+?<a href=\"/FundOrderConfirmation/.+?</a>.+?</td>\\s+" +
+				"<td .+?><span .+?>(?<accountType>.+?)</span></td>\\s+" +			
+				"<td .+?><span .+?>(?<units>.+?)</span></td>\\s+" +
+				"<td .+?><span .+?>\\s+(?<value>.+?)\\s+<br>\\s+(?<unitPrice>.+?)\\s+</span></td>\\s+" +
+				"<td .+?><span .+?>\\s+(?<fxRate>.+?)\\s+<br>(?<fxRateCost>.+?)\\s+</span></td>\\s+" +
+				"<td .+?><span .+?>\\s+(?<valueJPY>.+?)\\s+<br>\\s+<span .+?>\\s+(?<retrunsJPY>.+?)\\s+</span>\\s+</span></td>\\s+" +
+				"</tr>" +
+				"",
+				Pattern.DOTALL
+			);
+			public static List<ForeignMMFInfo> getInstance(String page) {
+				return ScrapeUtil.getList(ForeignMMFInfo.class, PAT, page);
+			}
+			
+			public final String name;
+			public final String currency;
+			public final String accountType;
+			@ScrapeUtil.AsNumber
+			public final String units;
+			@ScrapeUtil.AsNumber
+			public final String value;
+			public final String unitPrice;
+			public final String fxRate;
+			public final String fxRateCost;
+			
+			public ForeignMMFInfo(
+				String name, String currency, String accountType, String units, String value, String unitPrice,
+				String fxRate, String fxRateCost) {
+				this.name        = name;
+				this.currency    = currency;
+				this.accountType = accountType;
+				this.units       = units;
+				this.value       = value;
+				this.unitPrice   = unitPrice;
+				this.fxRate      = fxRate;
+				this.fxRateCost  = fxRateCost;
+			}
+			
+			@Override
+			public String toString() {
+				return StringUtil.toString(this);
+			}
+	}
+	
+	
+	// 外国債券＜転換社債含む＞s
+	public static class ForeignBondInfo {
+		//		<tr>
+		//		<td colspan="3" class="td01_5" index="6"><span class="txt_01_1">ミツイスミトモギンコウ（0855 - 9151）</span></td>
+		//
+		//			<td rowspan="2" align="center" class="td01" index="6"><span class="txt_01_1">特定</span></td>
+		//
+		//		<td rowspan="2" align="right" class="td01" index="6"><span class="txt_01_1">2,000<br>USD</span></td>
+		//
+		//		<td rowspan="2" align="right" class="td01" index="6"><span class="txt_01_1">98.20<br>147.03</span></td>
+		//		<td rowspan="2" align="right" class="td01" index="6"><span class="txt_01_1">288,766<br><span class="txt_fc09">-2,834</span></span></td></tr>
+		//		<tr>
+		//		<td align="center" class="td01_5" index="6"><span class="txt_01_1">2026/09/01</span></td>
+		//		<td align="center" class="td01" index="6"><span class="txt_01_1">03/01</span></td>
+		//		<td align="center" class="td01" index="6"><span class="txt_01_1">5.050%</span></td></tr>
+		
+		public static final Pattern PAT = Pattern.compile(
+				"<tr>\\s+" +
+				"<td colspan=\"3\" .+?><span .+?>(?<name>[^（]+)（(?<code>[0-9]{4} - [0-9]{4})）</span></td>\\s+" +
+				"<td .+?><span .+?>(?<accountType>.+?)</span></td>\\s+" +			
+				"<td .+?><span .+?>(?<units>.+?)<br>(?<currency>[A-Z]{3})</span></td>\\s+" +
+				"<td .+?><span .+?>(?<price>.+?)<br>(?<fxRate>.+?)</span></td>\\s+" +
+				"<td .+?><span .+?>(?<valueJPY>.+?)<br><span .+?>(?<returnsJPY>.+?)</span></span></td></tr>\\s+" +
+				"<tr>\\s+" +
+				"<td .+?><span .+?>(?<redemptionDate>.+?)</span></td>\\s+" +
+				"<td .+?><span .+?>(?<nextDivDate>.+?)</span></td>\\s+" +
+				"<td .+?><span .+?>(?<interestRate>.+?)%</span></td></tr>\\s+" +
+				"",
+				Pattern.DOTALL
+			);
+			public static List<ForeignBondInfo> getInstance(String page) {
+				return ScrapeUtil.getList(ForeignBondInfo.class, PAT, page);
+			}
+			
+			public final String name;
+			public final String code;
+			public final String accountType;
+			@ScrapeUtil.AsNumber
+			public final String units;
+			public final String currency;
+			@ScrapeUtil.AsNumber
+			public final String price;
+			public final String fxRate;
+			@ScrapeUtil.AsNumber
+			public final String valueJPY;
+			@ScrapeUtil.AsNumber
+			public final String returnsJPY;
+			public final String redemptionDate;
+			public final String nextDivDate;
+			public final String interestRate;
+			
+			public ForeignBondInfo(
+					String name, String code, String accountType, String units, String currency,
+					String price, String fxRate, String valueJPY, String returnsJPY,
+					String redemptionDate, String nextDivDate, String interestRate) {
+					this.name           = name;
+					this.code           = code;
+					this.accountType    = accountType;
+					this.units          = units;
+					this.currency       = currency;
+					this.price          = price;
+					this.fxRate         = fxRate;
+					this.valueJPY       = valueJPY;
+					this.returnsJPY     = returnsJPY;
+					this.redemptionDate = redemptionDate;
+					this.nextDivDate    = nextDivDate;
+					this.interestRate   = interestRate;
+				}
+			
+			@Override
+			public String toString() {
+				return StringUtil.toString(this);
+			}
 
-			var mrfInfo = MRFInfo.getInstance(page);
-			logger.info("mrfInfo  {}", mrfInfo);
+		
+	}
+	
+	
+	public static Asset mrfJPY(LocalDateTime dateTime, BigDecimal value) {
+		return Asset.mrfJPY(dateTime, Company.NIKKO, value);
+	}
+	public static Asset fundJP(LocalDateTime dateTime, BigDecimal factor, int units, String code, String name) {
+		return Asset.fundJP(dateTime, Company.NIKKO, factor, units, code, name);
+	}
+	public static Asset stock(LocalDateTime dateTime, Currency currency, int quantity, String code, String name) {
+		return Asset.stock(dateTime, Company.NIKKO, currency, quantity, code, name);
+	}
+	public static Asset mmf(LocalDateTime dateTime, Currency currency, BigDecimal value, String name) {
+		return Asset.mmf(dateTime, Company.NIKKO, currency, value, name);
+	}
+	public static Asset bond(LocalDateTime dateTime, Currency currency, int quantity, String code, String name) {
+		return Asset.bond(dateTime, Company.NIKKO, currency, quantity, code, name);
+	}
+	
+	private static void update() {
+		
+		var list = new ArrayList<Asset>();
+		
+		// build assetList
+		{
+			LocalDateTime dateTime;
+			{
+				var instant = FileUtil.getLastModified(FILE_BALANCE);
+				dateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault()).truncatedTo(ChronoUnit.SECONDS);
+			}
+			logger.info("dateTime  {}", dateTime);
+			
+			var page = FileUtil.read().file(FILE_BALANCE);
+			
+			{
+				var mrfInfo = MRFInfo.getInstance(page);
+//				logger.info("mrfInfo  {}", mrfInfo);
+				list.add(mrfJPY(dateTime, BigDecimal.valueOf(mrfInfo.value)));
+			}
+			
 			
 			var fundInfoList = FundInfo.getInstance(page);
 			for(var e: fundInfoList) {
-				logger.info("fundInfo  {}", e);
+//				logger.info("fundInfo  {}", e);
+				var units = new BigDecimal(e.units);
+				var unitPrice = new BigDecimal(e.unitPrice);
+				var value = new BigDecimal(e.value);
+				var factor = units.multiply(unitPrice).divide(value, 0, RoundingMode.HALF_EVEN);
+				
+				list.add(fundJP(dateTime, factor, units.intValue(), e.fundCode, e.fundName));
 			}
 			
 			var foreignStockInfoList = ForeignStockInfo.getInstance(page);
 			for(var e: foreignStockInfoList) {
-				logger.info("foreginStock  {}", e);
+//				logger.info("foreginStock  {}", e);
+				var quantity = Integer.valueOf(e.units);
+				var currency = Currency.valueOf(e.currency);
+				list.add(stock(dateTime, currency, quantity, e.stockCode, e.stockName));
 			}
-
+			
+			var foreignMMFList = ForeignMMFInfo.getInstance(page);
+			for(var e: foreignMMFList) {
+//				logger.info("foreignMMF  {}", e);
+				list.add(mmf(dateTime, Currency.valueOf(e.currency), new BigDecimal(e.value), e.name));
+			}
+			
+			var foreignBondList = ForeignBondInfo.getInstance(page);
+			for(var e: foreignBondList) {
+//				logger.info("foreignBond  {}", e);
+				var quantity = Integer.valueOf(e.units);
+				list.add(bond(dateTime, Currency.valueOf(e.currency), quantity, e.code, e.name));
+			}
 		}
 		
+		for(var e: list) {
+			logger.info("list {}", e);
+		}
 	}
 	
 	public static void main(String[] args) {
 		logger.info("START");
 		
-//		download();
+		download();
 		update();
 		
 		logger.info("STOP");
