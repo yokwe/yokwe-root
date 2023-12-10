148e3d5728f451b91e9976acf582c648dc8b100d
hasegawa <hasegawa@localhost>
2023-12-10 14:10:45 +0900

    Work as expected.

--- yokwe-finance/src/main/java/yokwe/finance/account/prestia/WebBrowserPrestia.java
+++ yokwe-finance/src/main/java/yokwe/finance/account/prestia/WebBrowserPrestia.java
@@ -11,6 +11,13 @@ public class WebBrowserPrestia extends WebBrowser {
 	private static final Target LOGIN_B = new Target.Click(By.linkText("サインオン"));
 	private static final Target LOGOUT  = new Target.Click(By.linkText("サインオフ"));
 	
+	private static final Target BALANCE_A = new Target.Click(By.id("header-nav-label-0"));
+	private static final Target BALANCE_B = new Target.Click(By.linkText("口座残高"));
+	
+	private static final Target FUND_ENTER_A = new Target.Click(By.id("header-nav-label-3"));
+	private static final Target FUND_ENTER_B = new Target.Click(By.linkText("投資信託サービス"), "インターネットバンキング投資信託");
+	private static final Target FUND_BALANCE = new Target.Click(By.xpath("//*[@id=\"navi02_03\"]/li[2]/a"));
+	private static final Target FUND_EXIT    = new Target.Click(By.xpath("//*[@id=\"header\"]/img[1]"), "プレスティア オンライン");
 	
 	public WebBrowserPrestia() {
 		super();
@@ -29,11 +36,27 @@ public class WebBrowserPrestia extends WebBrowser {
 		LOGIN_B.action(this);
 		wait.untilPageContains("代表口座");
 	}
-	
+	//*[@id="header-nav-menu-0"]/li[1]/a
 	public void logout() {
 		LOGOUT.action(this);
 		wait.untilPageContains("サインオフが完了しました");
 	}
-
-
+	public void balance() {
+		BALANCE_A.action(this);
+		BALANCE_B.action(this);
+	}
+	public void fundEnter() {
+		FUND_ENTER_A.action(this);
+		FUND_ENTER_B.action(this);
+	}
+	public void fundBalance() {
+		// hover mouse to navi02_03_active
+		moveMouse(By.id("navi02_03_active"));
+		
+		// click 
+		FUND_BALANCE.action(this);
+	}
+	public void fundExit() {
+		FUND_EXIT.action(this);
+	}
 }
