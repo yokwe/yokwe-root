27244bb419ccaf9c792e3281ad45cbbc09deecb3
hasegawa <hasegawa@macstudio2023.lan>
2023-12-14 16:51:19 +0900

    Add Currency and FXRate to finance.type.

---
+++ yokwe-finance/src/main/java/yokwe/finance/type/Currency.java
@@ -0,0 +1,5 @@
+package yokwe.finance.type;
+
+public enum Currency {
+	JPY, USD, EUR, GBP, AUD, NZD,
+}
--- yokwe-finance/src/main/java/yokwe/finance/type/FXRate.java
+++ yokwe-finance/src/main/java/yokwe/finance/type/FXRate.java
@@ -1,9 +1,51 @@
-package yokwe.finance.account;
+package yokwe.finance.type;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public class FXRate {
-	public LocalDate  date;
-	public BigDecimal usd;
+import yokwe.util.StringUtil;
+import yokwe.util.UnexpectedException;
+
+public final class FXRate implements Comparable<FXRate> {
+	public final LocalDate  date;
+	public final BigDecimal usd;
+	public final BigDecimal eur;
+	public final BigDecimal gbp;
+	public final BigDecimal aud;
+	public final BigDecimal nzd;
+	
+	public FXRate(LocalDate date, BigDecimal usd, BigDecimal eur, BigDecimal gbp, BigDecimal aud, BigDecimal nzd) {
+		this.date = date;
+		this.usd  = usd;
+		this.eur  = eur;
+		this.gbp  = gbp;
+		this.aud  = aud;
+		this.nzd  = nzd;
+	}
+	
+	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
+
+	public BigDecimal rate(Currency currency) {
+		switch(currency) {
+		case USD: return usd;
+		case EUR: return eur;
+		case GBP: return gbp;
+		case AUD: return aud;
+		case NZD: return nzd;
+		default:
+			logger.error("Unexpected currency");
+			logger.error("  currency {}!", currency);
+			throw new UnexpectedException("Unexpected currency");
+		}
+	}
+	
+	@Override
+	public int compareTo(FXRate that) {
+		return this.date.compareTo(that.date);
+	}
+	
+	@Override
+	public String toString() {
+		return StringUtil.toString(this);
+	}
 }
