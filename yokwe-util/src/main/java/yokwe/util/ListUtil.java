de2f5c76e99b79f9d053a03e335df6a60edeb7a0
hasegawa <hasegawa@localhost>
2023-12-21 17:54:44 +0900

    Add File file version of save/load/getList.

--- yokwe-util/src/main/java/yokwe/util/ListUtil.java
+++ yokwe-util/src/main/java/yokwe/util/ListUtil.java
@@ -1,5 +1,6 @@
 package yokwe.util;
 
+import java.io.File;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -32,6 +33,20 @@ public final class ListUtil {
 		return map;
 	}
 	
+	//
+	// Reader reader
+	//
+	public static <E extends Comparable<E>> List<E> load(Class<E> clazz, Reader reader) {
+		return CSVUtil.read(clazz).file(reader);
+	}
+	public static <E extends Comparable<E>> List<E> getList(Class<E> clazz, Reader reader) {
+		var ret = load(clazz, reader);
+		return ret == null ? new ArrayList<>() : ret;
+	}
+	
+	//
+	// String path
+	//
 	public static <E extends Comparable<E>> void save(Class<E> clazz, String path, Collection<E> collection) {
 		save(clazz, path, new ArrayList<>(collection));
 	}
@@ -43,15 +58,27 @@ public final class ListUtil {
 	public static <E extends Comparable<E>> List<E> load(Class<E> clazz, String path) {
 		return CSVUtil.read(clazz).file(path);
 	}
-	public static <E extends Comparable<E>> List<E> load(Class<E> clazz, Reader reader) {
-		return CSVUtil.read(clazz).file(reader);
-	}
 	public static <E extends Comparable<E>> List<E> getList(Class<E> clazz, String path) {
 		var ret = load(clazz, path);
 		return ret == null ? new ArrayList<>() : ret;
 	}
-	public static <E extends Comparable<E>> List<E> getList(Class<E> clazz, Reader reader) {
-		var ret = load(clazz, reader);
+	
+	//
+	// File file
+	//
+	public static <E extends Comparable<E>> void save(Class<E> clazz, File file, Collection<E> collection) {
+		save(clazz, file, new ArrayList<>(collection));
+	}
+	public static <E extends Comparable<E>> void save(Class<E> clazz, File file, List<E> list) {
+		// Sort before save
+		Collections.sort(list);
+		CSVUtil.write(clazz).file(file, list);
+	}
+	public static <E extends Comparable<E>> List<E> load(Class<E> clazz, File file) {
+		return CSVUtil.read(clazz).file(file);
+	}
+	public static <E extends Comparable<E>> List<E> getList(Class<E> clazz, File file) {
+		var ret = load(clazz, file);
 		return ret == null ? new ArrayList<>() : ret;
 	}
 }
