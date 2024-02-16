df37fbe23683a9ce05526a418af8e7a23b8754e3
hasegawa <hasegawa@localhost>
2024-02-16 21:15:22 +0900

    Delete FundInfo and Add FundReturns

--- yokwe-finance/src/main/java/yokwe/finance/account/prestia/FundPage.java
+++ yokwe-finance/src/main/java/yokwe/finance/account/prestia/FundPage.java
@@ -5,57 +5,70 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 import yokwe.util.ScrapeUtil;
+import yokwe.util.StringUtil;
 
 public class FundPage {
-	public static class FundInfo {
+	public static class FundReturns {
 		public static final Pattern PAT = Pattern.compile(
-			"<td .+?><a href=\"\" onclick=\"MM_openBrWindow\\('https://www.smbctb.co.jp/re/mf_service/D01-10_fund.html\\?fundcode=(?<fundCode>[0-9]+)'.+?>(?<fundName>.+?)</a></td>\\s+" +
-			"<td .+?>(?<accountType>.+?)</td>\\s+" +
-			"<td .+?>\\s+(?<price>[0-9,\\.,]+)<br><br>\\s+(?<priceDate>.+?)\\s+</td>\\s+" +
-			"<td .+?>\\s+(?<units>[0-9\\.,]+)\\s+<br><br>\\s+(?<currency>...)/(?<fxRate>[0-9\\.]+)</td>\\s+" +
-			"<td .+?>\\s+(?<valueJPY>[0-9\\.,]+)<br><br>\\s+(?<value>[0-9\\.,]+)\\s+</td>\\s+" +
-			"<td .+?>.+?</td>\\s+" +
-			"<td .+?>.+?</td>\\s+" +
-			"<td .+?>.+?</td>\\s+" +
-			"<td .+?>.+?</td>\\s+" +
-			"</tr>" +
+			"<tr>\\s+<td colspan=\"7\" class=\"GAI_FUND_NAME .+?><a .+?fundcode=(?<fundCode>.+?)'.+?>(?<fundName>.+?)</a></td>\\s+</tr>\\s+" +
+			"<tr>\\s+" +
+			"<td .+?>(?<accountTypeA>.+?)<br>\\s+(?<accountTypeB>.+?)</td>\\s+" +
+			"<td .+?>(?<units>.+?)</td>\\s+" +
+			"<td .+?>(?<value>.+?)</td>\\s+" +
+			"<td .+?>(?<divTotal>.+?)</td>\\s+" +
+			"<td .+?>(?<soldTotal>.+?)</td>\\s+" +
+			"<td .+?>(?<buyTotal>.+?)</td>\\s+" +
+			"<td .+?>(?<returns>.+?)</td>\\s+" +
+			"</tr>\\s+" +
+			"<tr>\\s+" +
+			"<td .+?>(?<dateStart>.+?)</td>\\s+" +
+			"<td .+?>(?<unitPrice>.+?)</td>\\s+" +
+			"</tr>\\s+" +
 			"",
 			Pattern.DOTALL
 		);
-		public static List<FundInfo> getInstance(String page) {
-			return ScrapeUtil.getList(FundInfo.class, PAT, page);
+		public static List<FundReturns> getInstance(String page) {
+			return ScrapeUtil.getList(FundReturns.class, PAT, page);
 		}
 		
 		public final String     fundCode;
 		public final String     fundName;
-		public final String     accountType;
-		public final BigDecimal price;
-		public final String     priceDate;
-		public final BigDecimal units;
-		public final String     currency;
-		public final BigDecimal fxRate;
-		public final BigDecimal valueJPY;
-		public final BigDecimal value;
+		public final String		accountTypeA;
+		public final String		accountTypeB;
 		
-		public FundInfo(
-			String fundCode, String fundName, String accountType, BigDecimal price, String priceDate,
-			BigDecimal units, String currency, BigDecimal fxRate, BigDecimal valueJPY, BigDecimal value) {
-			this.fundCode    = fundCode;
-			this.fundName    = fundName;
-			this.accountType = accountType;
-			this.price       = price;
-			this.priceDate   = priceDate;
-			this.units       = units;
-			this.currency    = currency;
-			this.fxRate      = fxRate;
-			this.valueJPY    = valueJPY;
-			this.value       = value;
+		public final BigDecimal units;
+		public final BigDecimal value;
+		public final BigDecimal divTotal;
+		public final BigDecimal soldTotal;
+		public final BigDecimal buyTotal;
+		public final BigDecimal returns;
+		
+		public final String     dateStart;
+		public final BigDecimal unitPrice;
+		
+		public FundReturns(
+			String fundCode, String fundName, String accountTypeA, String accountTypeB,
+			BigDecimal units, BigDecimal value, BigDecimal divTotal, BigDecimal soldTotal, BigDecimal buyTotal, BigDecimal returns,
+			String dateStart, BigDecimal unitPrice) {
+			this.fundCode     = fundCode;
+			this.fundName     = fundName;
+			this.accountTypeA = accountTypeA;
+			this.accountTypeB = accountTypeB;
+			
+			this.units        = units;
+			this.value        = value;
+			this.divTotal     = divTotal;
+			this.soldTotal    = soldTotal;
+			this.buyTotal     = buyTotal;
+			this.returns      = returns;
+
+			this.dateStart    = dateStart;
+			this.unitPrice    = unitPrice;
 		}
 		
 		@Override
 		public String toString() {
-			return String.format("{%s  %s  %s  %s  %s  %s}", fundCode, accountType, price.toPlainString(), units.toPlainString(), currency, fundName);
+			return StringUtil.toString(this);
 		}
 	}
-
 }
