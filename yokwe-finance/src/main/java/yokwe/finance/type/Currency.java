18cd3396e0081b474d1d09cf5413d21ea1714edb
hasegawa <hasegawa@localhost>
2024-02-23 10:12:18 +0900

    Add getInstance(String) method.

--- yokwe-finance/src/main/java/yokwe/finance/type/Currency.java
+++ yokwe-finance/src/main/java/yokwe/finance/type/Currency.java
@@ -1,5 +1,7 @@
 package yokwe.finance.type;
 
+import yokwe.util.UnexpectedException;
+
 public enum Currency {
 	JPY("日本円"),
 	USD("米ドル"),
@@ -7,6 +9,17 @@ public enum Currency {
 	GBP("ポンド"),
 	AUD("AUドル"),
 	NZD("NZドル");
+	
+	public static Currency getInstance(String string) {
+		for(var e: values()) {
+			if (string.equals(e.description)) return e;
+			if (string.equals(e.name()))      return e;
+		}
+		org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
+		logger.error("Unexpeced string");
+		logger.error("  string  {}!", string);
+		throw new UnexpectedException("Unexpeced string");
+	}
 	
 	public final String description;
 	private Currency(String nweValue) {
