187e099f9297e2b9d61e4f6e664446cefefdced3
hasegawa <hasegawa@mac.lan>
2025-01-23 21:45:20 +0900

    Change for new data source.

--- yokwe-finance/src/main/java/yokwe/finance/provider/nikko/UpdateFundInfoNikko.java
+++ yokwe-finance/src/main/java/yokwe/finance/provider/nikko/UpdateFundInfoNikko.java
@@ -1,62 +1,31 @@
 package yokwe.finance.provider.nikko;
 
 import java.io.File;
+import java.math.BigDecimal;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import yokwe.finance.Storage;
 import yokwe.finance.fund.StorageFund;
-import yokwe.finance.provider.nikko.FundPage.FundInfo;
 import yokwe.util.FileUtil;
-import yokwe.util.ScrapeUtil;
+import yokwe.util.StringUtil;
 import yokwe.util.UnexpectedException;
 import yokwe.util.http.HttpUtil;
+import yokwe.util.json.JSON;
+import yokwe.util.json.JSON.Name;
 
 public class UpdateFundInfoNikko {
 	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
 	
 	private static final Storage storage = Storage.provider.nikko;
 	
-	private static final File PAGE_DIR = storage.getFile("fund");
+	private static final File FILE_JSON = storage.getFile("trading-fund-nikko.json");
 	
-	private static final String URL_FORMAT = "https://fund2.smbcnikko.co.jp/smbc_nikko_fund/qsearch.exe?F=list_kokunai&GO_BEFORE=&BEFORE=%d&MAXDISP=%d";
-		
-	private static final int MAXDISP = 50;
+	private static final String URL = "https://fund2.smbcnikko.co.jp/smbc_nikko_hp/fund/jcgi/wrapcf/qjsonp.aspx?F=ctl%2Ffnd_list";
+	
 	private static final Charset CHARSET = Charset.forName("Shift_JIS");
 
-	private static String getURL(int pageNo) {
-		var before = pageNo * MAXDISP;
-		return String.format(URL_FORMAT, before, MAXDISP);
-	}
-	private static File getFile(int pageNo) {
-		var child = String.format("%03d.html", pageNo);
-		return new File(PAGE_DIR, child);
-	}
-	
-	// <input type="hidden" name="HIT_COUNT" id="HIT_COUNT" value="1056" />
-	public static class HitCountInfo {
-		public static final Pattern PAT = Pattern.compile(
-			"<input type=\"hidden\" name=\"HIT_COUNT\" id=\"HIT_COUNT\" value=\"(?<hitCount>.+?)\" />"
-		);
-		public static HitCountInfo getInstance(String page) {
-			return ScrapeUtil.get(HitCountInfo.class, PAT, page);
-		}
-		
-		public final int hitCount;
-		
-		public HitCountInfo(int hitCount) {
-			this.hitCount = hitCount;
-		}
-		
-		@Override
-		public String toString() {
-			return String.format("%d", hitCount);
-		}
-	}
-	
 	private static final boolean DEBUG_USE_FILE = false;
 	private static String download(String url, Charset charset, File file, boolean useFile) {
 		final String page;
@@ -70,7 +39,8 @@ public class UpdateFundInfoNikko {
 					logger.error("  result  {}", result);
 					throw new UnexpectedException("Unexpected");
 				}
-				page = result.result.replace("\r","\n").replace("shift_jis", "utf-8"); // cr -> nl  shift_jis -> utf-8
+				// ({ -> {  }) -> }
+				page = result.result.replace("fnd_list({", "{").replace("})", "}");
 				
 				// debug
 				if (DEBUG_USE_FILE) logger.info("save  {}  {}", page.length(), file.getPath());
@@ -80,84 +50,129 @@ public class UpdateFundInfoNikko {
 		return page;
 	}
 	
-	private static void deleteAllFiles(File dir) {
-		File[] files = dir.listFiles();
-		if (files != null) {
-			for(var file: files) {
-				if (file.isFile()) file.delete();
-			}
-		}
+	private static void download() {
+		var page = download(URL, CHARSET, FILE_JSON, DEBUG_USE_FILE);
+		logger.info("page  {}", page.length());
 	}
 	
-	private static void download() {
-		deleteAllFiles(PAGE_DIR);
+	
+	public static class FundList {
+		public static class Section {
+			@Name("currentpage") public int    currentpage;
+			@Name("hitcount")	 public int    hitcount;
+			@Name("pagecount")	 public int    pagecount;
+			@Name("recordcount") public int    recordcount;
+			@Name("status")	     public int    status;
+			@Name("type")	     public String ver;
+			@Name("data")        public Data[] data;
+		}
 		
-		int maxPageNo = 999;
-		for(int pageNo = 0; pageNo <= maxPageNo; pageNo++) {
-			var url  = getURL(pageNo);
-			var file = getFile(pageNo);
+		public static class Data {
+			@Name("Area")                         public String[]   Area;
+			@Name("Categories")                   public String[]   Categories;
+			@Name("ChangeValue")                  public String     ChangeValue;
+			@Name("ChangeValueRaw")               public int        ChangeValueRaw;
+			@Name("ChangeValueUnit")              public String     ChangeValueUnit;
+			@Name("CompanyCode")                  public String     CompanyCode;
+			@Name("CompanyName")                  public String     CompanyName;
+			@Name("CompanySort")                  public int        CompanySort;
+			@Name("Currency")                     public String     Currency;
+			@Name("DividendProfit")               public String     DividendProfit;
+			@Name("DividendProfitRaw")            public int        DividendProfitRaw;
+			@Name("DividendTimes")                public String     DividendTimes;
+			@Name("DividendYears")                public String     DividendYears;
+			@Name("DividendYearsRaw")             public int        DividendYearsRaw;
+			@Name("FundCode")                     public String     FundCode;
+			@Name("FundName")                     public String     FundName;
+			@Name("FundNameRaw")                  public String     FundNameRaw;
+			@Name("FundType")                     public String     FundType;
+			@Name("HasFavoriteButton")            public boolean    HasFavoriteButton;
+			@Name("HasFundCompareButton")         public boolean    HasFundCompareButton;
+			@Name("HasInvestmentReportLink")      public boolean    HasInvestmentReportLink;
+			@Name("HasMokuromiLink")              public boolean    HasMokuromiLink;
+			@Name("HasPurchaseOrderButton")       public boolean    HasPurchaseOrderButton;
+			@Name("HasReserveApplicationButton")  public boolean    HasReserveApplicationButton;
+			@Name("HasReserveSimulationButton")   public boolean    HasReserveSimulationButton;
+			@Name("InvestmentReportUrl")          public String     InvestmentReportUrl;
+			@Name("IsBullBear")                   public boolean    IsBullBear;
+			@Name("IsDirectCourse")               public String     IsDirectCourse;
+			@Name("IsDirectCourseOnly")           public boolean    IsDirectCourseOnly;
+			@Name("IsESG")                        public boolean    IsESG;
+			@Name("IsGeneralCourse")              public String     IsGeneralCourse;
+			@Name("IsIndexType")                  public boolean    IsIndexType;
+			@Name("IsNISA")                       public boolean    IsNISA;
+			@Name("IsNoload")                     public boolean    IsNoload;
+			@Name("IsTsumiNISA")                  public boolean    IsTsumiNISA;
+			@Name("IsTsumitate")                  public boolean    IsTsumitate;
+			@Name("IsiDeCo")                      public boolean    IsiDeCo;
+			@Name("MokuromiUrl")                  public String     MokuromiUrl;
+			@Name("MorningstarRating")            public String     MorningstarRating;
+			@Name("NetAssetValue")                public String     NetAssetValue;
+			@Name("NetAssetValueRaw")             public int        NetAssetValueRaw;
+			@Name("NetAssetValueUnit")            public String     NetAssetValueUnit;
+			@Name("NickNameRaw")                  public String     NickNameRaw;
+			@Name("NikkoCode")                    public String     NikkoCode;
+			@Name("NikkoSort")                    public String     nikkoSort;
+			@Name("QuickFundRisk")                public String     QuickFundRisk;
+			@Name("ReferenceDate")                public String     ReferenceDate;
+			@Name("ReturnMonth1Raw")              public int        ReturnMonth1Raw;
+			@Name("ReturnMonth3")                 public String     ReturnMonth3;
+			@Name("ReturnMonth3Raw")              public int        ReturnMonth3Raw;
+			@Name("ReturnMonth6Raw")              public int        ReturnMonth6Raw;
+			@Name("ReturnSettingRaw")             public int        ReturnSettingRaw;
+			@Name("ReturnYear1")                  public String     ReturnYear1;
+			@Name("ReturnYear10Raw")              public int        ReturnYear10Raw;
+			@Name("ReturnYear1Raw")               public int        ReturnYear1Raw;
+			@Name("ReturnYear3Raw")               public int        ReturnYear3Raw;
+			@Name("ReturnYear5Raw")               public int        ReturnYear5Raw;
+			@Name("SharpeRatioYear1Raw")          public BigDecimal SharpeRatioYear1Raw;
+			@Name("SharpeRatioYear3Raw")          public BigDecimal SharpeRatioYear3Raw;
+			@Name("SharpeRatioYear5Raw")          public BigDecimal SharpeRatioYear5Raw;
+			@Name("TotalAssetValue")              public String     TotalAssetValue;
+			@Name("TotalAssetValueRaw")           public int        TotalAssetValueRaw;
+			@Name("line_no")                      public int        line_no;
 			
-			var page = download(url, CHARSET, file, DEBUG_USE_FILE);
-			FileUtil.write().file(file, page);
-			logger.info("page  {}  /  {}   {}", pageNo, maxPageNo, page.length());
-			
-			if (pageNo == 0) {
-				var hitCountInfo = HitCountInfo.getInstance(page);
-				maxPageNo = hitCountInfo.hitCount / MAXDISP;
-				logger.info("hitCount   {}", hitCountInfo.hitCount);
-				logger.info("maxPageNo  {}", maxPageNo);
-			}
-			
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				//
+			public String toString() {
+				return StringUtil.toString(this);
 			}
 		}
+		
+		@Name("cputime")  public String  cputime;
+		@Name("status")	  public int     status;
+		@Name("ver")	  public String  ver;
+		@Name("section1") public Section section;
 	}
 	
 	private static void update() {
 		var fundCodeMap = StorageFund.FundInfo.getList().stream().collect(Collectors.toMap(o -> o.fundCode, o -> o.isinCode));
-
-		File[] files = PAGE_DIR.listFiles();
-		Arrays.sort(files);
-		var fileLast = files[files.length - 1];
+		
+		var page = FileUtil.read().file(FILE_JSON);
+		var fundList = JSON.unmarshal(FundList.class, page);
+		
+		logger.info("fundList  {}", fundList.section.data.length);
 		
 		var list = new ArrayList<FundInfoNikko>();
 		
-		for(var file: files) {
-			var page = FileUtil.read().file(file);
-			page = page.replaceAll("<!--[\\s\\S]*?-->", "");
+		int countFund = 0;
+		for (var data: fundList.section.data) {
+			if (!data.Currency.equals("--")) continue;
 			
-			var fundInfoList = FundInfo.getInstance(page);
-			logger.info("fundInfoList  {}  {}", fundInfoList.size(), file.getName());
-			
-			int countFund = 0;
-			for (var fundInfo: fundInfoList) {
-//				logger.info("fundInfo  {}", fundInfo);
-				
-				var isinCode   = fundCodeMap.get(fundInfo.fundCode);
-				if (isinCode == null) {
-					// In case fund is redemption, it disappeared from fundList
-					logger.warn("Unpexpected fundCode  {}", fundInfo);
-					continue;
-				}
-				int prospectus = fundInfo.prospectus ? 1 : 0;
-				int sougou     = fundInfo.sougou     ? 1 : 0;
-				int direct     = fundInfo.direct     ? 1 : 0;
-				var name       = fundInfo.fundName;
-				
-				list.add(new FundInfoNikko(isinCode, prospectus, sougou, direct, name));
-				
-				countFund++;
+			var isinCode = fundCodeMap.get(data.FundCode);
+			if (isinCode == null) {
+				// In case fund become redemption, it will disappeared from fundList
+				logger.warn("Unpexpected fundCode  {}", data);
+				continue;
 			}
-//			logger.info("countFund  {}", countFund);
+			int prospectus = data.MokuromiUrl.isEmpty()     ? 0 : 1;
+			int sougou     = data.IsGeneralCourse.isEmpty() ? 0 : 1;
+			int direct     = data.IsDirectCourse.isEmpty()  ? 0 : 1;
+			var name       = data.FundName;
 			
-			if (countFund != MAXDISP && !file.equals(fileLast)) {
-				// somethign goes wrong
-				logger.warn("Uexpected countFund  {}", countFund);
-			}
+			list.add(new FundInfoNikko(isinCode, prospectus, sougou, direct, name));
+			
+			countFund++;
 		}
+		logger.info("countFund  {}", countFund);
 		
 		logger.info("save  {}  {}", list.size(), StorageNikko.FundInfoNikko.getPath());
 		StorageNikko.FundInfoNikko.save(list);
