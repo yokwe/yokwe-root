5008e6b51f191dbf4f7a1c9aee9c5fc906c054d8
hasegawa <hasegawa@localhost>
2025-06-01 14:58:04 +0900

    Add set initial value of fields

--- yokwe-finance/src/main/java/yokwe/finance/trade/AccountReportUSD.java
+++ yokwe-finance/src/main/java/yokwe/finance/trade/AccountReportUSD.java
@@ -3,64 +3,80 @@ package yokwe.finance.trade;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import yokwe.util.ToString;
+import yokwe.util.libreoffice.Sheet;
 import yokwe.util.libreoffice.SpreadSheet;
-import yokwe.util.libreoffice.Sheet.ColumnName;
-import yokwe.util.libreoffice.Sheet.NumberFormat;
 
-public class AccountReportUSD {
+@Sheet.SheetName("account-report-usd")
+@Sheet.HeaderRow(0)
+@Sheet.DataRow(1)
+public class AccountReportUSD extends Sheet {
 	@ColumnName("年月日")
 	@NumberFormat(SpreadSheet.FORMAT_DATE)
 	public LocalDate date;
 	
 	@ColumnName("入金")
 	@NumberFormat(SpreadSheet.FORMAT_USD_BLANK)
-	public BigDecimal deposit;
+	public BigDecimal deposit = BigDecimal.ZERO;
 	
 	@ColumnName("出金")
 	@NumberFormat(SpreadSheet.FORMAT_USD_BLANK)
-	public BigDecimal withdraw;
+	public BigDecimal withdraw = BigDecimal.ZERO;
 	
 	@ColumnName("資金")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	public BigDecimal fund;
+	public BigDecimal fundTotal = BigDecimal.ZERO;
 	
 	@ColumnName("現金")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	public BigDecimal cash;
+	public BigDecimal cashTotal = BigDecimal.ZERO;
 	
 	@ColumnName("証券評価")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	public BigDecimal stockValue;
+	public BigDecimal stockValue = BigDecimal.ZERO;
 	
 	@ColumnName("証券原価")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	public BigDecimal stockCost;
+	public BigDecimal stockCost = BigDecimal.ZERO;
+	
+	@ColumnName("未実現損益")
+	@NumberFormat(SpreadSheet.FORMAT_USD)
+	public BigDecimal unrealizedGain = BigDecimal.ZERO;
 	
 	@ColumnName("実現損益")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	public BigDecimal realizedGain;
+	public BigDecimal realizedGain = BigDecimal.ZERO;
 	
 	@ColumnName("配当")
 	@NumberFormat(SpreadSheet.FORMAT_USD_BLANK)
-	public BigDecimal dividend;
+	public BigDecimal dividend = BigDecimal.ZERO;
 	
 	@ColumnName("購入")
 	@NumberFormat(SpreadSheet.FORMAT_USD_BLANK)
-	public BigDecimal buy;
+	public BigDecimal buy = BigDecimal.ZERO;
 	
 	@ColumnName("売却")
 	@NumberFormat(SpreadSheet.FORMAT_USD_BLANK)
-	public BigDecimal sell;
+	public BigDecimal sell = BigDecimal.ZERO;
 	
 	@ColumnName("売却原価")
 	@NumberFormat(SpreadSheet.FORMAT_USD_BLANK)
-	public BigDecimal sellCost;
+	public BigDecimal sellCost = BigDecimal.ZERO;
 	
 	@ColumnName("売却損益")
 	@NumberFormat(SpreadSheet.FORMAT_USD_BLANK)
-	public BigDecimal sellGain;
+	public BigDecimal sellGain = BigDecimal.ZERO;
 	
 	@ColumnName("銘柄")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	public String symbol;
+	public String code = "";
+	
+	@ColumnName("コメント")
+	@NumberFormat(SpreadSheet.FORMAT_STRING)
+	public String comment = "";
+	
+	@Override
+	public String toString() {
+		return ToString.withFieldName(this);
+	}
 }
