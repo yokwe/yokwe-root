58ddea67f6bc4339f7e81f3bb07a1e01031f9353
hasegawa <hasegawa@localhost>
2025-06-01 14:58:50 +0900

    Temporary commit.

--- yokwe-finance/src/main/java/yokwe/finance/trade/rakuten/UpdateAccountReport.java
+++ yokwe-finance/src/main/java/yokwe/finance/trade/rakuten/UpdateAccountReport.java
@@ -1,7 +1,23 @@
 package yokwe.finance.trade.rakuten;
 
+import java.math.BigDecimal;
+
+import yokwe.util.StringUtil;
+
 public class UpdateAccountReport {
 	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
+	
+	public static int toCentValue(BigDecimal value) {
+		if (value.compareTo(BigDecimal.ZERO) == 0) return 0;
+		return value.movePointRight(2).intValue();
+	}
+	public static BigDecimal toDollarValue(int value) {
+		if (value == 0) return BigDecimal.ZERO;
+		return BigDecimal.valueOf(value).movePointLeft(2);
+	}
+	
+	public static final String URL_TEMPLATE  = StringUtil.toURLString("data/form/ACCOUNT.ods");
+
 
 	public static void main(String[] args) {
 		logger.info("START");
@@ -12,6 +28,7 @@ public class UpdateAccountReport {
 	}
 	
 	private static void update() {
-		UpdateAccountReportJPY.update();
+//		UpdateAccountReportJPY.update(); // FIXME
+		UpdateAccountReportUSD.update();
 	}
 }
