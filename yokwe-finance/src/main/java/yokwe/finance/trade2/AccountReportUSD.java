ddd11c104b6cc0f3987131fae1235a80d880bd33
hasegawa <hasegawa@localhost>
2025-06-08 22:27:52 +0900

    Unify UpdateAccountReportJPY and USD.

--- yokwe-finance/src/main/java/yokwe/finance/trade2/AccountReportUSD.java
+++ yokwe-finance/src/main/java/yokwe/finance/trade2/AccountReportUSD.java
@@ -2,6 +2,8 @@ package yokwe.finance.trade2;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import yokwe.util.ToString;
 import yokwe.util.libreoffice.Sheet;
@@ -17,66 +19,112 @@ public class AccountReportUSD extends Sheet {
 	
 	@ColumnName("入金")
 	@NumberFormat(SpreadSheet.FORMAT_USD_BLANK)
-	public BigDecimal deposit = BigDecimal.ZERO;
+	public BigDecimal deposit;
 	
 	@ColumnName("出金")
 	@NumberFormat(SpreadSheet.FORMAT_USD_BLANK)
-	public BigDecimal withdraw = BigDecimal.ZERO;
+	public BigDecimal withdraw;
 	
 	@ColumnName("資金")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	public BigDecimal fundTotal = BigDecimal.ZERO;
+	public BigDecimal fundTotal;
 	
 	@ColumnName("現金")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	public BigDecimal cashTotal = BigDecimal.ZERO;
+	public BigDecimal cashTotal;
 	
 	@ColumnName("証券評価")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	public BigDecimal stockValue = BigDecimal.ZERO;
+	public BigDecimal stockValue;
 	
 	@ColumnName("証券原価")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	public BigDecimal stockCost = BigDecimal.ZERO;
+	public BigDecimal stockCost;
 	
 	@ColumnName("未実現損益")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	public BigDecimal unrealizedGain = BigDecimal.ZERO;
+	public BigDecimal unrealizedGain;
 	
 	@ColumnName("実現損益")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	public BigDecimal realizedGain = BigDecimal.ZERO;
+	public BigDecimal realizedGain;
 	
 	@ColumnName("配当")
 	@NumberFormat(SpreadSheet.FORMAT_USD_BLANK)
-	public BigDecimal dividend = BigDecimal.ZERO;
+	public BigDecimal dividend;
 	
 	@ColumnName("購入")
 	@NumberFormat(SpreadSheet.FORMAT_USD_BLANK)
-	public BigDecimal buy = BigDecimal.ZERO;
+	public BigDecimal buy;
 	
 	@ColumnName("売却")
 	@NumberFormat(SpreadSheet.FORMAT_USD_BLANK)
-	public BigDecimal sell = BigDecimal.ZERO;
+	public BigDecimal sell;
 	
 	@ColumnName("売却原価")
 	@NumberFormat(SpreadSheet.FORMAT_USD_BLANK)
-	public BigDecimal sellCost = BigDecimal.ZERO;
+	public BigDecimal sellCost;
 	
 	@ColumnName("売却損益")
 	@NumberFormat(SpreadSheet.FORMAT_USD_BLANK)
-	public BigDecimal sellGain = BigDecimal.ZERO;
+	public BigDecimal sellGain;
 	
 	@ColumnName("銘柄")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	public String code = "";
+	public String code;
 	
 	@ColumnName("コメント")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	public String comment = "";
+	public String comment;
+	
+	public AccountReportUSD() {
+		this.date           = null;
+		this.deposit        = BigDecimal.ZERO;
+		this.withdraw       = BigDecimal.ZERO;
+		this.fundTotal      = BigDecimal.ZERO;
+		this.cashTotal      = BigDecimal.ZERO;
+		this.stockValue     = BigDecimal.ZERO;
+		this.stockCost      = BigDecimal.ZERO;
+		this.unrealizedGain = BigDecimal.ZERO;
+		this.realizedGain   = BigDecimal.ZERO;
+		this.dividend       = BigDecimal.ZERO;
+		this.buy            = BigDecimal.ZERO;
+		this.sell           = BigDecimal.ZERO;
+		this.sellCost       = BigDecimal.ZERO;
+		this.sellGain       = BigDecimal.ZERO;
+		this.code           = "";
+		this.comment        = "";
+	}
+	
+	public AccountReportUSD(AccountReport that) {
+		this.date           = that.date;
+		this.deposit        = BigDecimal.valueOf(that.deposit).movePointLeft(2);
+		this.withdraw       = BigDecimal.valueOf(that.withdraw).movePointLeft(2);
+		this.fundTotal      = BigDecimal.valueOf(that.fundTotal).movePointLeft(2);
+		this.cashTotal      = BigDecimal.valueOf(that.cashTotal).movePointLeft(2);
+		this.stockValue     = BigDecimal.valueOf(that.stockValue).movePointLeft(2);
+		this.stockCost      = BigDecimal.valueOf(that.stockCost).movePointLeft(2);
+		this.unrealizedGain = BigDecimal.valueOf(that.unrealizedGain).movePointLeft(2);
+		this.realizedGain   = BigDecimal.valueOf(that.realizedGain).movePointLeft(2);
+		this.dividend       = BigDecimal.valueOf(that.dividend).movePointLeft(2);
+		this.buy            = BigDecimal.valueOf(that.buy).movePointLeft(2);
+		this.sell           = BigDecimal.valueOf(that.sell).movePointLeft(2);
+		this.sellCost       = BigDecimal.valueOf(that.sellCost).movePointLeft(2);
+		this.sellGain       = BigDecimal.valueOf(that.sellGain).movePointLeft(2);
+		this.code           = that.code;
+		this.comment        = that.comment;
+	}
 	
 	@Override
 	public String toString() {
 		return ToString.withFieldName(this);
+	}
+	
+	public static AccountReportUSD toAccountReportUSD(AccountReport that) {
+		return new AccountReportUSD(that);
+	}
+	
+	public static List<AccountReportUSD> toAccountReportUSD(List<AccountReport> list) {
+		return list.stream().map(o -> new AccountReportUSD(o)).collect(Collectors.toList());
 	}
 }
--- yokwe-finance/src/main/java/yokwe/finance/trade2/rakuten/UpdateAccountReport.java
+++ yokwe-finance/src/main/java/yokwe/finance/trade2/rakuten/UpdateAccountReport.java
@@ -5,11 +5,18 @@ import static yokwe.finance.trade.rakuten.UpdateAccountReport.URL_TEMPLATE;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
-import yokwe.finance.trade2.AccountReportJPY;
+import yokwe.finance.trade2.AccountReport;
 import yokwe.finance.trade2.AccountReportUSD;
 import yokwe.finance.trade2.Portfolio;
+import yokwe.finance.trade2.Transaction;
+import yokwe.finance.trade2.Transaction.Currency;
 import yokwe.util.StringUtil;
 import yokwe.util.libreoffice.LibreOffice;
 import yokwe.util.libreoffice.Sheet;
@@ -26,21 +33,21 @@ public class UpdateAccountReport {
 		logger.info("STOP");
 	}
 	
-	public static void update() {
+	private static void update() {
 		var transactionList = StorageRakuten.TransactionList.getList();
 		
-		var reportJPY = UpdateAccountReportJPY.toAccountReportJPY(transactionList);
+		var reportJPY = toAccountReport(transactionList.stream().filter(o -> o.currency == Currency.JPY).toList());
 		logger.info("reportJPY  {}", reportJPY.size());
-		var reportUSD = UpdateAccountReportUSD.toAccountReportUSD(transactionList);
+		var reportUSD = toAccountReport(transactionList.stream().filter(o -> o.currency == Currency.USD).toList());
 		logger.info("reportUSD  {}", reportUSD.size());
 		
 		// remove entry older than 1 year
 		reportJPY.removeIf(o -> o.date.isBefore(LocalDate.now().minusYears(1)));
 		reportUSD.removeIf(o -> o.date.isBefore(LocalDate.now().minusYears(1)));
 		
-		generateReport(reportJPY, reportUSD);
+		generateReport(reportJPY, AccountReportUSD.toAccountReportUSD(reportUSD));
 	}
-	private static void generateReport(List<AccountReportJPY> listJPY, List<AccountReportUSD> listUSD) {
+	private static void generateReport(List<AccountReport> listJPY, List<AccountReportUSD> listUSD) {
 		String urlReport;
 		{
 			String timestamp  = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss").format(LocalDateTime.now());
@@ -60,7 +67,7 @@ public class UpdateAccountReport {
 			SpreadSheet docSave = new SpreadSheet();
 			
 			{
-				String sheetName = Sheet.getSheetName(AccountReportJPY.class);
+				String sheetName = Sheet.getSheetName(AccountReport.class);
 				logger.info("sheet     {}", sheetName);
 				docSave.importSheet(docLoad, sheetName, docSave.getSheetCount());
 				Sheet.fillSheet(docSave, listJPY);
@@ -88,11 +95,302 @@ public class UpdateAccountReport {
 		}
 	}
 	
-	static class Context {
+	private static class Context {
 		Portfolio  portfolio    = new Portfolio();
 		int        fundTotal    = 0;
 		int        cashTotal    = 0;
 		int        stockCost    = 0;
 		int        realizedGain = 0;
 	}
+	
+	private static List<AccountReport> toAccountReport(List<Transaction> transactionList) {
+		var context = new Context();
+		
+		// map needs to use TreeMap for entrySet ordering
+		var map = transactionList.stream().collect(Collectors.groupingBy(o -> o.settlementDate, TreeMap::new, Collectors.toCollection(ArrayList::new)));
+		
+		var ret = new ArrayList<AccountReport>();
+		
+		LocalDate lastDate = null;
+		for(var entry: map.entrySet()) {
+			var date    = entry.getKey();
+			var mapList = entry.getValue();
+			
+			// fill gap
+			for(;;) {
+				if (lastDate == null) break;
+				lastDate = lastDate.plusDays(1);
+				if (!lastDate.isBefore(date)) break;
+				ret.add(reportAsOf(context, lastDate));
+			}
+			lastDate = date;
+			
+			for(var e: mapList) {
+				ret.add(toAccountReportJPY(context, e));
+			}
+		}
+		
+		// fill gap to today
+		{
+			var today = LocalDate.now();
+			for(;;) {
+				if (lastDate == null) break;
+				lastDate = lastDate.plusDays(1);
+				if (lastDate.isAfter(today)) break;
+				ret.add(reportAsOf(context, lastDate));
+			}
+		}
+		
+		return ret;
+	}
+	
+	private static AccountReport getAccountReport(Context context, LocalDate date, String code, String comment) {
+		var ret = new AccountReport();
+		
+		ret.date           = date;
+		ret.fundTotal      = context.fundTotal;
+		ret.cashTotal      = context.cashTotal;
+		ret.stockValue     = context.portfolio.valueAsOf(date);
+		ret.stockCost      = context.stockCost;
+		ret.unrealizedGain = (ret.stockValue <= 0) ? 0 : (ret.stockValue - ret.stockCost);
+		ret.realizedGain   = context.realizedGain;
+		ret.code           = code;
+		ret.comment        = comment;
+
+		return ret;
+	}
+	private static AccountReport getAccountReport(Context context, Transaction transaction) {
+		return getAccountReport(context, transaction.settlementDate, transaction.code, transaction.comment);
+	}
+	
+	private static AccountReport reportAsOf(Context context, LocalDate date) {
+		return getAccountReport(context, date, "", "");
+	}
+	
+	
+	private static final Map<Transaction.Type, Map<Transaction.Asset, BiFunction<Context, Transaction, AccountReport>>> typeMap = Map.ofEntries(
+		Map.entry(Transaction.Type.DEPOSIT, Map.ofEntries(
+			Map.entry(Transaction.Asset.CASH,     new DEPOSIT()),
+			Map.entry(Transaction.Asset.STOCK_JP, new DEPOSTI_STOCK_JP())
+		)),
+		Map.entry(Transaction.Type.WITHDRAW, Map.ofEntries(
+				Map.entry(Transaction.Asset.CASH, new WITHDRAW())
+		)),
+		Map.entry(Transaction.Type.DEPOSIT_TRANSFER, Map.ofEntries(
+			Map.entry(Transaction.Asset.CASH, new DEPOSIT())
+		)),
+		Map.entry(Transaction.Type.WITHDRAW_TRANSFER, Map.ofEntries(
+			Map.entry(Transaction.Asset.CASH, new WITHDRAW())
+		)),
+		Map.entry(Transaction.Type.DIVIDEND, Map.ofEntries(
+			Map.entry(Transaction.Asset.CASH,   new DIVIDEND_CASH()),
+			Map.entry(Transaction.Asset.MMF_US, new DIVIDEND_MMF_US())
+		)),
+		Map.entry(Transaction.Type.TAX, Map.ofEntries(
+			Map.entry(Transaction.Asset.CASH, new TAX())
+		)),
+		Map.entry(Transaction.Type.TAX_REFUND, Map.ofEntries(
+			Map.entry(Transaction.Asset.CASH, new TAX_REFUND())
+		)),
+		Map.entry(Transaction.Type.BUY, Map.ofEntries(
+			Map.entry(Transaction.Asset.STOCK_JP, new BUY()),
+			Map.entry(Transaction.Asset.FUND_JP,  new BUY()),
+			Map.entry(Transaction.Asset.STOCK_US, new BUY()),
+			Map.entry(Transaction.Asset.BOND_US,  new BUY()),
+			Map.entry(Transaction.Asset.MMF_US,   new BUY())
+		)),
+		Map.entry(Transaction.Type.SELL, Map.ofEntries(
+			Map.entry(Transaction.Asset.STOCK_JP, new SELL()),
+			Map.entry(Transaction.Asset.FUND_JP,  new SELL()),
+			Map.entry(Transaction.Asset.STOCK_US, new SELL()),
+			Map.entry(Transaction.Asset.BOND_US,  new SELL()),
+			Map.entry(Transaction.Asset.MMF_US,   new SELL())
+		))
+	);
+	private static AccountReport toAccountReportJPY(Context context, Transaction transaction) {
+		var assetMap = typeMap.get(transaction.type);
+		if (assetMap == null) logger.warn("unexpected  type  {}  asset  {}", transaction.type, transaction.asset);
+		var func = assetMap.get(transaction.asset);
+		if (func == null) logger.warn("unexpected  type  {}  asset  {}", transaction.type, transaction.asset);
+		return func.apply(context, transaction);
+	}
+	//
+	private static class DEPOSIT implements BiFunction<Context, Transaction, AccountReport> {
+		@Override
+		public AccountReport apply(Context context, Transaction transaction) {
+			var amount  = transaction.amount;
+
+			// update context
+			// fundTotal = cashTotal + stockCost
+			context.fundTotal += amount;
+			context.cashTotal += amount;
+			
+			var ret = getAccountReport(context, transaction);
+			
+			ret.deposit = amount;
+			
+			return ret;
+		}
+	}
+	private static class WITHDRAW implements BiFunction<Context, Transaction, AccountReport> {
+		@Override
+		public AccountReport apply(Context context, Transaction transaction) {
+			var amount  = transaction.amount;
+			
+			// update context
+			// fundTotal = cashTotal + stockCost
+			context.fundTotal -= amount;
+			context.cashTotal -= amount;
+			
+			var ret = getAccountReport(context, transaction);
+			
+			ret.withdraw = amount;
+			
+			return ret;
+		}
+	}
+	private static class DEPOSTI_STOCK_JP implements BiFunction<Context, Transaction, AccountReport> {
+		@Override
+		public AccountReport apply(Context context, Transaction transaction) {
+			var amount  = transaction.amount;
+			
+			// update portfolio
+			transaction.amount = 0;
+			context.portfolio.buy(transaction);
+			transaction.amount = amount;
+			
+			// update context
+			// fundTotal = cashTotal + stockCost
+			
+			var ret = getAccountReport(context, transaction);
+			
+			ret.buy  = amount;
+			
+			return ret;
+		}
+	}
+	private static class DIVIDEND_CASH implements BiFunction<Context, Transaction, AccountReport> {
+		@Override
+		public AccountReport apply(Context context, Transaction transaction) {
+			var amount = transaction.amount;
+			
+			// update context
+			// fundTotal = cashTotal + stockCost
+			context.fundTotal    += amount;
+			context.cashTotal    += amount;
+			context.realizedGain += amount;
+			
+			var ret = getAccountReport(context, transaction);
+			
+			ret.dividend = amount;
+			
+			return ret;
+		}
+	}
+	private static class DIVIDEND_MMF_US implements BiFunction<Context, Transaction, AccountReport> {
+		@Override
+		public AccountReport apply(Context context, Transaction transaction) {
+			var amount = transaction.amount;
+			
+			// update portfolio
+			{
+				transaction.amount = 0;
+				context.portfolio.buy(transaction);
+				transaction.amount = amount;
+			}
+			
+			// update context
+			// fundTotal = cashTotal + stockCost
+			context.fundTotal    += amount;
+			context.realizedGain += amount;
+			
+			var ret = getAccountReport(context, transaction);
+			
+			ret.dividend = amount;
+			
+			return ret;
+		}
+	}
+	private static class TAX implements BiFunction<Context, Transaction, AccountReport> {
+		@Override
+		public AccountReport apply(Context context, Transaction transaction) {
+			var amount  = transaction.amount;
+			
+			// update context
+			// fundTotal = cashTotal + stockCost
+			context.fundTotal    -= amount;
+			context.cashTotal    -= amount;
+			context.realizedGain -= amount;
+			
+			var ret = getAccountReport(context, transaction);
+			
+			ret.withdraw = amount;
+			
+			return ret;
+		}
+	}
+	private static class TAX_REFUND implements BiFunction<Context, Transaction, AccountReport> {
+		@Override
+		public AccountReport apply(Context context, Transaction transaction) {
+			var amount  = transaction.amount;
+			
+			// update context
+			// fundTotal = cashTotal + stockCost
+			context.fundTotal    += amount;
+			context.cashTotal    += amount;
+			context.realizedGain += amount;
+			
+			var ret = getAccountReport(context, transaction);
+			
+			ret.deposit = transaction.amount;
+			
+			return ret;
+		}
+	}
+	private static class BUY implements BiFunction<Context, Transaction, AccountReport> {
+		@Override
+		public AccountReport apply(Context context, Transaction transaction) {			
+			// update portfolio
+			context.portfolio.buy(transaction);
+			
+			// update context
+			// fundTotal = cashTotal + stockCost
+			var amount  = transaction.amount;
+			context.cashTotal -= amount;
+			context.stockCost += amount;
+			
+			var ret = getAccountReport(context, transaction);
+			
+			ret.buy = amount;
+			
+			return ret;
+		}
+	}
+	//
+	private static class SELL implements BiFunction<Context, Transaction, AccountReport> {
+		@Override
+		public AccountReport apply(Context context, Transaction transaction) {
+			var amount  = transaction.amount;
+			
+			// update portfolio
+			var sellCost = context.portfolio.sell(transaction);
+			var gain     = amount - sellCost;
+
+			// update context
+			// fundTotal = cashTotal + stockCost
+			context.fundTotal    += amount - sellCost;
+			context.cashTotal    += amount;
+			context.stockCost    -= sellCost;
+			context.realizedGain += gain;
+			
+			var ret = getAccountReport(context, transaction);
+			
+			ret.sell     = amount;
+			ret.sellCost = sellCost;
+			ret.sellGain = gain;
+			
+			return ret;
+		}
+	}
+
 }
