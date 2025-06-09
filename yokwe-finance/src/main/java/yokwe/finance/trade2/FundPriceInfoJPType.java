e904eaef73dca5f8d597a240c0e5ee850f9927f2
hasegawa <hasegawa@localhost>
2025-06-09 11:45:33 +0900

    Add shift field for BigDecimal

--- yokwe-finance/src/main/java/yokwe/finance/trade2/FundPriceInfoJPType.java
+++ yokwe-finance/src/main/java/yokwe/finance/trade2/FundPriceInfoJPType.java
@@ -1,16 +1,37 @@
 package yokwe.finance.trade2;
 
 import yokwe.util.ToString;
+import yokwe.util.UnexpectedException;
 
-public class FundPriceInfoJPType implements Comparable<FundPriceInfoJPType>{
+public class FundPriceInfoJPType implements Comparable<FundPriceInfoJPType> {
+	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
+	
 	public final String code;
 	public final String name;
 	public final int    units; // how many units represents fund price. if value is 10,000, fund price represents 10,000 units of fund
+	public final int    shift; // log10(units)
 	
-	public FundPriceInfoJPType(String code, String name, int units) {
+	public FundPriceInfoJPType(String code, String name, int units, int shift) {
 		this.code  = code;
 		this.name  = name;
 		this.units = units;
+		this.shift = shift;
+		
+		// sanity check
+		{
+			var units2 = (int)Math.pow(10, shift);
+			if (units != units2) {
+				logger.error("Unexpected shift");
+				logger.error("  code    {}", code);
+				logger.error("  units   {}", units);
+				logger.error("  shift   {}", shift);
+				logger.error("  units2  {}", units2);
+				throw new UnexpectedException("Unexpected shift");
+			}
+		}
+	}
+	public FundPriceInfoJPType(String code, String name, int units) {
+		this(code, name, units, (int)Math.log10(units));
 	}
 
 	@Override 
